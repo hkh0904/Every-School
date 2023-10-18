a71e24d2aa6b12579019325ba95c91f072a411f6
임우택 <lyt1228@naver.com>
2023-10-18 14:24:06 +0900

    feat: setup every-school microservice project

---
+++ call-service/src/test/java/com/everyschool/callservice/CallServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everyschool.callservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ consult-service/src/main/java/com/everyschool/consultservice/ConsultServiceApplication.java
@@ -0,0 +1,13 @@
+package com.everyschool.consultservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultServiceApplication.class, args);
+	}
+
+}
