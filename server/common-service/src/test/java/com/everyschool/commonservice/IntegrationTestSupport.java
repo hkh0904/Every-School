0dfdf5988982cad9706f99b6cdd724571cac0e34
Chaos0103 <lyt1228@naver.com>
2023-10-19 08:27:17 +0900

    feat(common): 공통 클래스 생성
    Adds S09P31C108-21

---
+++ server/common-service/src/test/java/com/everyschool/commonservice/IntegrationTestSupport.java
@@ -0,0 +1,10 @@
+package com.everyschool.commonservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public abstract class IntegrationTestSupport {
+}
+
