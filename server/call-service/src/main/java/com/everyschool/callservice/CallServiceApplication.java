f24d64c85e349bc21fd3e1e7e73ba10119af3f41
임우택 <lyt1228@naver.com>
2023-10-18 14:25:36 +0900

    feat: setup every-school microservice project

---
+++ server/call-service/src/main/java/com/everyschool/callservice/CallServiceApplication.java
@@ -0,0 +1,13 @@
+package com.everyschool.callservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallServiceApplication.class, args);
+	}
+
+}
