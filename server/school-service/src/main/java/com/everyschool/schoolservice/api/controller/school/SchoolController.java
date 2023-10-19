add2ae3ca3dd3729bac7a76f6ccef86d7c5da07b
sungjoo <hans52410537@gmail.com>
2023-10-20 00:24:39 +0900

    test(school): 학급 생성 API 테스트 구현

--- server/school-service/src/main/java/com/everyschool/schoolservice/api/controller/school/SchoolController.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/api/controller/school/SchoolController.java
@@ -1,12 +1,13 @@
 package com.everyschool.schoolservice.api.controller.school;
 
 import com.everyschool.schoolservice.api.ApiResponse;
+import com.everyschool.schoolservice.api.controller.school.request.ClassroomRequest;
+import com.everyschool.schoolservice.api.controller.school.response.ClassroomResponse;
 import com.everyschool.schoolservice.api.controller.school.response.SchoolResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +44,43 @@ public class SchoolController {
         schools.add(response2);
 
         return ApiResponse.ok(schools);
+    }
+
+    /**
+     * 학교 단건 조회 API
+     *
+     * @return 학교 정보
+     */
+    @GetMapping("/school/{schoolId}")
+    public ApiResponse<SchoolResponse> searchSchool(@PathVariable String schoolId) {
+        SchoolResponse response = SchoolResponse.builder()
+                .name("광주싸피초등학교")
+                .address("광주광역시 광산구 하남삼단로")
+                .tel("062-1111-2222")
+                .url("https://www.ssafy.com/")
+                .build();
+        
+        return ApiResponse.ok(response);
+    }
+
+    /**
+     * 학급 생성 API
+     *
+     * @return 생성한 학급 정보
+     */
+    @PostMapping("/school/{schoolId}/classroom")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<ClassroomResponse> createClassroom(@PathVariable String schoolId,
+                                                       @RequestBody ClassroomRequest request) {
+
+        ClassroomResponse res = ClassroomResponse.builder()
+                .teacherName("임우택")
+                .year(2023)
+                .grade(1)
+                .name("동팔이")
+                .build();
+
+        return ApiResponse.ok(res);
     }
 
 }
--- server/school-service/src/test/java/com/everyschool/schoolservice/docs/school/SchoolControllerDocsTest.java
+++ server/school-service/src/test/java/com/everyschool/schoolservice/docs/school/SchoolControllerDocsTest.java
@@ -1,17 +1,17 @@
 package com.everyschool.schoolservice.docs.school;
 
 import com.everyschool.schoolservice.api.controller.school.SchoolController;
+import com.everyschool.schoolservice.api.controller.school.request.ClassroomRequest;
 import com.everyschool.schoolservice.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,7 +21,7 @@ public class SchoolControllerDocsTest extends RestDocsSupport {
         return new SchoolController();
     }
 
-    @DisplayName("학교 조회 API")
+    @DisplayName("학교 리스트 조회 API")
     @Test
     void searchSchools() throws Exception {
         mockMvc.perform(
@@ -29,7 +29,7 @@ public class SchoolControllerDocsTest extends RestDocsSupport {
             )
             .andDo(print())
             .andExpect(status().isOk())
-            .andDo(document("search-school",
+            .andDo(document("search-schools",
                 preprocessResponse(prettyPrint()),
                 responseFields(
                     fieldWithPath("code").type(JsonFieldType.NUMBER)
@@ -50,5 +50,97 @@ public class SchoolControllerDocsTest extends RestDocsSupport {
                             .description("확교 전화번호")
                 )
             ));
+    }
+
+    @DisplayName("학교 단건 조회 API")
+    @Test
+    void searchSchool() throws Exception {
+        String schoolId = "1";
+
+        mockMvc.perform(
+                        get("/school/{schoolId}", schoolId)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("search-one-school",
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("응답 데이터"),
+                                fieldWithPath("data.name").type(JsonFieldType.STRING)
+                                        .description("학교 이름"),
+                                fieldWithPath("data.address").type(JsonFieldType.STRING)
+                                        .description("학교 주소"),
+                                fieldWithPath("data.url").type(JsonFieldType.STRING)
+                                        .description("학교 홈페이지 주소"),
+                                fieldWithPath("data.tel").type(JsonFieldType.STRING)
+                                        .description("확교 전화번호")
+                        )
+                ));
+    }
+
+    @DisplayName("학급 생성 API")
+    @Test
+    void createClassroom() throws Exception {
+        String schoolId = "1";
+        Long teacherId = Long.parseLong("123123123123");
+
+        ClassroomRequest request = ClassroomRequest.builder()
+                .teacherId(teacherId)
+                .year(2023)
+                .grade(1)
+                .name("동팔이")
+                .build();
+
+        mockMvc.perform(
+                post("/school/{schoolId}/classroom", schoolId)
+                    .header("Authorization", "Bearer Token")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andDo(document("create-classroom",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("teacherId").type(JsonFieldType.NUMBER)
+                                        .optional()
+                                        .description("교사 ID"),
+                                fieldWithPath("year").type(JsonFieldType.NUMBER)
+                                        .optional()
+                                        .description("현재 년도"),
+                                fieldWithPath("grade").type(JsonFieldType.NUMBER)
+                                        .optional()
+                                        .description("학년"),
+                                fieldWithPath("name").type(JsonFieldType.STRING)
+                                        .optional()
+                                        .description("학급 이름")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("응답 데이터"),
+                                fieldWithPath("data.teacherName").type(JsonFieldType.STRING)
+                                        .description("교사 이름"),
+                                fieldWithPath("data.year").type(JsonFieldType.NUMBER)
+                                        .description("년도"),
+                                fieldWithPath("data.grade").type(JsonFieldType.NUMBER)
+                                        .description("학년"),
+                                fieldWithPath("data.name").type(JsonFieldType.STRING)
+                                        .description("학급 이름")
+                        )
+                ));
     }
 }
