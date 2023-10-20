d56c5c91b20ddee72c728b8d0bc79027316865c7
이지혁 <illu140411@gmail.com>
2023-10-20 17:22:00 +0900

    feat: 웹 초기 설정
    Closes #S09P31C108-31

--- web/src/index.js
+++ web/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// [1]라우터 [2]스토어 제공
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        {/* <PersistGate loading={null} persistor={persistor}> */}
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
