58deb759b1fc25b0f2616a6e909e130c8c4a60bc
Chaos0103 <lyt1228@naver.com>
2023-10-22 20:33:55 +0900

    feat(user): 이메일 인증 번호 전송 API 컨트롤러 구현
    Adds S09P31C108-20

--- server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/AuthController.java
+++ server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/AuthController.java
@@ -1,10 +1,13 @@
 package com.everyschool.userservice.api.controller.user;
 
 import com.everyschool.userservice.api.ApiResponse;
+import com.everyschool.userservice.api.client.mail.dto.EmailMessage;
 import com.everyschool.userservice.api.controller.user.request.AuthEmailCheckRequest;
 import com.everyschool.userservice.api.controller.user.request.AuthEmailRequest;
+import com.everyschool.userservice.api.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -13,9 +16,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping("/email")
     public ApiResponse<String> authEmail(@RequestBody AuthEmailRequest request) {
-        return ApiResponse.ok(null);
+        EmailMessage message = request.toMessage();
+
+        authService.sendEmail(message);
+
+        return ApiResponse.of(HttpStatus.OK, "인증 번호 발송", null);
     }
 
     @PostMapping("/email/check")
--- server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/request/AuthEmailRequest.java
+++ server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/request/AuthEmailRequest.java
@@ -1,17 +1,30 @@
 package com.everyschool.userservice.api.controller.user.request;
 
+import com.everyschool.userservice.api.client.mail.dto.EmailMessage;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 public class AuthEmailRequest {
 
+    @NotBlank
+    @Size(max = 100)
     private String email;
 
     @Builder
     private AuthEmailRequest(String email) {
         this.email = email;
+    }
+
+    public EmailMessage toMessage() {
+        return EmailMessage.builder()
+            .to(this.email)
+            .subject("[everySCHOOL] 이메일 인증을 위한 인증 코드 발송")
+            .build();
     }
 }
