833d9f6d1a5eea4509b2deae572fc522ffd1d8e4
Chaos0103 <lyt1228@naver.com>
2023-10-22 15:09:09 +0900

    feat(user): 회원가입 request 제약 조건 추가
    Adds S09P31C108-20

--- server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/UserController.java
+++ server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/UserController.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
@@ -23,7 +24,7 @@ public class UserController {
 
     @PostMapping("/join")
     @ResponseStatus(HttpStatus.CREATED)
-    public ApiResponse<UserResponse> join(@RequestBody JoinUserRequest request) {
+    public ApiResponse<UserResponse> join(@Valid @RequestBody JoinUserRequest request) {
         UserResponse response = UserResponse.builder()
             .email("ssafy@ssafy.com")
             .name("김싸피")
--- server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/request/JoinUserRequest.java
+++ server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/request/JoinUserRequest.java
@@ -4,18 +4,35 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 public class JoinUserRequest {
 
+    @NotNull
     private Integer userCode;
+
+    @NotEmpty
+    @Size(max = 100)
     private String email;
+
+    @NotEmpty
+    @Size(max = 20)
     private String password;
+
+    @NotEmpty
+    @Size(max = 20)
     private String name;
+
+    @NotEmpty
+    @Size(min = 10, max = 10)
     private String birth;
 
     @Builder
-    public JoinUserRequest(Integer userCode, String email, String password, String name, String birth) {
+    private JoinUserRequest(Integer userCode, String email, String password, String name, String birth) {
         this.userCode = userCode;
         this.email = email;
         this.password = password;
