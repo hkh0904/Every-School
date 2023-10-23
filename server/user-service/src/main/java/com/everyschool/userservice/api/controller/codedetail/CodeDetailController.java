70d143bb273c1b9110408be1e54358e2aea11281
Chaos0103 <lyt1228@naver.com>
2023-10-23 20:24:51 +0900

    feat(user): 공통 상세 코드 등록 서비스 구현
    Adds S09P31C108-134

---
+++ server/user-service/src/main/java/com/everyschool/userservice/api/controller/codedetail/CodeDetailController.java
@@ -0,0 +1,13 @@
+package com.everyschool.userservice.api.controller.codedetail;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+@RequestMapping
+public class CodeDetailController {
+}
--- server/user-service/src/main/java/com/everyschool/userservice/domain/codedetail/repository/CodeDetailQueryRepository.java
+++ server/user-service/src/main/java/com/everyschool/userservice/domain/codedetail/repository/CodeDetailQueryRepository.java
@@ -1,9 +1,12 @@
 package com.everyschool.userservice.domain.codedetail.repository;
 
+import com.everyschool.userservice.domain.codedetail.QCodeDetail;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+
+import static com.everyschool.userservice.domain.codedetail.QCodeDetail.codeDetail;
 
 @Repository
 public class CodeDetailQueryRepository {
@@ -12,5 +15,17 @@ public class CodeDetailQueryRepository {
 
     public CodeDetailQueryRepository(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public boolean existCodeName(int groupId, String codeName) {
+        Integer result = queryFactory
+            .select(codeDetail.id)
+            .from(codeDetail)
+            .where(
+                codeDetail.group.id.eq(groupId),
+                codeDetail.codeName.eq(codeName)
+            )
+            .fetchFirst();
+        return result != null;
     }
 }
