9023b8ac7740c8ac0e37c3496d3c77fdf2fc8fd5
오연주 <siunaus224@gmail.com>
2023-10-23 17:50:53 +0900

    feat: 상담 확인 페이지 메뉴 상단 구현 Adds #S09P31C108-158

--- web/src/pages/ConsultApprovePage/ConsultApprovePage.jsx
+++ web/src/pages/ConsultApprovePage/ConsultApprovePage.jsx
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+import styles from './ConsultApprovePage.module.css';
+
 export default function ConsultApprovePage() {
+  const [message, setMessage] = useState();
+
+  const csltList = [
+    {
+      studentName: '강OO',
+      parentName: '김OO',
+      parentSex: 'F',
+      date: [2023, 10, 23, 16],
+      reason: '자녀의진로상담어쩌고저쩌고'
+    },
+    {
+      studentName: '강OO',
+      parentName: '김OO',
+      parentSex: 'F',
+      date: [2023, 10, 23, 13],
+      reason: '자녀의진로상담어쩌고저쩌고'
+    },
+    {
+      studentName: '강OO',
+      parentName: '김OO',
+      parentSex: 'F',
+      date: [2023, 11, 23, 16],
+      reason: '자녀의진로상담어쩌고저쩌고'
+    }
+  ];
+
   return (
-    <div>
-      <p>오!</p>
+    <div className={styles.ConsultApprove}>
+      <div className={styles.title}>
+        <p>상담 확인</p>
+        <p>대기 중 : {csltList.length}건</p>
+        <p>상담 설명 메세지 : </p>
+      </div>
     </div>
   );
 }
