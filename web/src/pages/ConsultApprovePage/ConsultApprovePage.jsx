ef0fcb48734b2e80d2a0d882e21029a51747f9f4
Yeoni <siunaus224@gmail.com>
2023-10-23 22:10:48 +0900

    feat: 상담 확인 카드 부분 구현 Adds #feature/S09P31C108-158

--- web/src/pages/ConsultApprovePage/ConsultApprovePage.jsx
+++ web/src/pages/ConsultApprovePage/ConsultApprovePage.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import styles from './ConsultApprovePage.module.css';
+import ConsultCheckList from './ConsultCheckList';
 
 export default function ConsultApprovePage() {
-  const [message, setMessage] = useState();
+  const [message, setMessage] = useState('상담 메세지를 설정하세요');
+  const [isCorrect, setIsCorrect] = useState(false);
 
   const csltList = [
     {
@@ -25,6 +27,20 @@ export default function ConsultApprovePage() {
       parentSex: 'F',
       date: [2023, 11, 23, 16],
       reason: '자녀의진로상담어쩌고저쩌고'
+    },
+    {
+      studentName: '강OO',
+      parentName: '김OO',
+      parentSex: 'F',
+      date: [2024, 1, 23, 16],
+      reason: '자녀의진로상담어쩌고저쩌고'
+    },
+    {
+      studentName: '강OO',
+      parentName: '김OO',
+      parentSex: 'F',
+      date: [2024, 1, 23, 16],
+      reason: '자녀의진로상담어쩌고저쩌고'
     }
   ];
 
@@ -33,8 +49,26 @@ export default function ConsultApprovePage() {
       <div className={styles.title}>
         <p>상담 확인</p>
         <p>대기 중 : {csltList.length}건</p>
-        <p>상담 설명 메세지 : </p>
+        <div className={styles.csltMessage}>
+          <p>상담 설명 메세지 :&nbsp;</p>
+          {isCorrect ? (
+            <div className={styles.correctMsg}>
+              <input type='text' value={message} onChange={(e) => setMessage(e.target.value)} />
+              <p className={styles.msgBtn} onClick={() => setIsCorrect(false)}>
+                완료
+              </p>
+            </div>
+          ) : (
+            <div className={styles.setMsg}>
+              <p>{message}</p>
+              <p className={styles.msgBtn} onClick={() => setIsCorrect(true)}>
+                수정
+              </p>
+            </div>
+          )}
+        </div>
       </div>
+      <ConsultCheckList csltList={csltList} />
     </div>
   );
 }
---
+++ web/src/pages/ConsultApprovePage/ConsultCheckList.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import styles from './ConsultCheckList.module.css';
+import { useEffect } from 'react';
+
+export default function ConsultCheckList({ csltList }) {
+  const [groupCslt, setGroupCslt] = useState([]);
+
+  function groupByYearMonth(list) {
+    const grouped = {};
+
+    list.forEach((item) => {
+      const year = item.date[0];
+      const month = item.date[1];
+
+      if (!grouped[year]) {
+        grouped[year] = {};
+      }
+      if (!grouped[year][month]) {
+        grouped[year][month] = [];
+      }
+      grouped[year][month].push(item);
+    });
+    return grouped;
+  }
+
+  useEffect(() => {
+    const groupedByYearMonth = groupByYearMonth(csltList);
+    setGroupCslt(groupedByYearMonth);
+  }, []);
+
+  return (
+    <div className={styles.ConsultCheckList}>
+      <div className={styles.checkList}>
+        {Object.keys(groupCslt).map((year) => (
+          <div key={year}>
+            {Object.keys(groupCslt[year]).map((month) => (
+              <div key={month} className={styles.listMonth}>
+                <p className={styles.yearNmonth}>
+                  {year}년 {month}월
+                </p>
+                <div>
+                  {groupCslt[year][month].map((item, index) => (
+                    <div key={index} className={styles.csltCard}>
+                      <div className={styles.cardInfo}>
+                        <div>
+                          <img
+                            className={styles.cardImage}
+                            src={process.env.PUBLIC_URL + 'assets/consult/consultcheck.png'}
+                            alt=''
+                          />
+                          <p className={styles.childName}>{item.studentName} 학생</p>
+                          <p className={styles.parentName}>
+                            {item.parentName}
+                            {item.parentSex === 'F' ? <span>(모)</span> : <span>(부)</span>}
+                          </p>
+                        </div>
+                        <div className={styles.reasonBox}>
+                          <p className={styles.reasonBoxP1}>상담 일자</p>
+                          <p className={styles.reasonBoxP2}>
+                            {item.date.slice(0, 3).join('.')} {item.date[3]}:00
+                          </p>
+                          <p className={styles.reasonBoxP3}>신청 사유</p>
+                          <p className={styles.reasonBoxP4}>{item.reason}</p>
+                        </div>
+                      </div>
+                      <div className={styles.checkBtn}>
+                        <p>승인</p>
+                        <p>거절</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
