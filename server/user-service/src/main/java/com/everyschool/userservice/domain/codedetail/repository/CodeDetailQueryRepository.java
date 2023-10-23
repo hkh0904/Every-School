ac4a749a3947c2ee0458fe753dc85b47b9b42c8c
Chaos0103 <lyt1228@naver.com>
2023-10-23 23:31:39 +0900

    feat(user): 공통 상세 코드 조회 쿼리 구현
    Adds S09P31C108-135

--- server/user-service/src/main/java/com/everyschool/userservice/domain/codedetail/repository/CodeDetailQueryRepository.java
+++ server/user-service/src/main/java/com/everyschool/userservice/domain/codedetail/repository/CodeDetailQueryRepository.java
@@ -1,10 +1,14 @@
 package com.everyschool.userservice.domain.codedetail.repository;
 
+import com.everyschool.userservice.api.controller.codedetail.respnse.CodeDetailResponse;
 import com.everyschool.userservice.domain.codedetail.QCodeDetail;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+
+import java.util.List;
 
 import static com.everyschool.userservice.domain.codedetail.QCodeDetail.codeDetail;
 
@@ -27,5 +31,20 @@ public class CodeDetailQueryRepository {
             )
             .fetchFirst();
         return result != null;
+    }
+
+    public List<CodeDetailResponse> findByGroupId(int groupId) {
+        return queryFactory
+            .select(Projections.constructor(CodeDetailResponse.class,
+                codeDetail.id,
+                codeDetail.codeName,
+                codeDetail.isDeleted
+
+            ))
+            .from(codeDetail)
+            .where(
+                codeDetail.group.id.eq(groupId)
+            )
+            .fetch();
     }
 }
