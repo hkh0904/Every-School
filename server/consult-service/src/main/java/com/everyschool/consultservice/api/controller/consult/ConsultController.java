eac4d875ac8c6eafbb77c3030cb4d7d560ad46a4
이예리 <leeyr0412@naver.com>
2023-10-24 11:21:55 +0900

    test(consult): 상담 Rest Api Docs
    상담 신청 Rest Api Docs Test 작성
    
    Adds #S09P31C108-80

--- server/consult-service/src/main/java/com/everyschool/consultservice/api/controller/consult/ConsultController.java
+++ server/consult-service/src/main/java/com/everyschool/consultservice/api/controller/consult/ConsultController.java
@@ -1,4 +1,34 @@
 package com.everyschool.consultservice.api.controller.consult;
 
+import com.everyschool.consultservice.api.ApiResponse;
+import com.everyschool.consultservice.api.controller.consult.request.CreateConsultRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/consult-service")
 public class ConsultController {
+
+    @PostMapping("/consult")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<Long> createConsult(@RequestBody @Valid CreateConsultRequest request) {
+        // TODO: 2023-10-24 상담 신청
+        return ApiResponse.created(1L);
+    }
+
+    // TODO: 2023-10-24 상담 일정 등록
+
+    // TODO: 2023-10-24 상담 가능 일정 조회
+
+    // TODO: 2023-10-24 상담 상태 변경
+
+    // TODO: 2023-10-24 상담 삭제
+
+    // TODO: 2023-10-24 상담 결과 기록
 }
--- server/consult-service/src/test/java/com/everyschool/consultservice/docs/consult/ConsultControllerDocsTest.java
+++ server/consult-service/src/test/java/com/everyschool/consultservice/docs/consult/ConsultControllerDocsTest.java
@@ -1,105 +1,73 @@
 package com.everyschool.consultservice.docs.consult;
 
 import com.everyschool.consultservice.api.controller.consult.ConsultController;
+import com.everyschool.consultservice.api.controller.consult.request.CreateConsultRequest;
 import com.everyschool.consultservice.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
-import java.util.ArrayList;
-import java.util.UUID;
+import java.time.LocalDateTime;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class ConsultControllerDocsTest extends RestDocsSupport {
 
-//    @DisplayName("게시글 작성 API")
-//    @Test
-//    void createBoard() throws Exception {
-//        CreateBoardRequest request = CreateBoardRequest.builder()
-//            .title("개교 기념일 안내")
-//            .content("학교 쉬어요~")
-//            .categoryId(1L)
-//            .schoolId(1L)
-//            .classId(1L)
-//            .useComment(false)
-//            .uploadFiles(new ArrayList<>())
-//            .build();
-//
-//        mockMvc.perform(
-//                post("/board-service/boards/{userKey}", UUID.randomUUID().toString())
-//                    .content(objectMapper.writeValueAsString(request))
-//                    .contentType(MediaType.MULTIPART_FORM_DATA)
-//            )
-//            .andDo(print())
-//            .andExpect(status().isCreated())
-//            .andDo(document("create-board",
-//                preprocessRequest(prettyPrint()),
-//                preprocessResponse(prettyPrint()),
-//                requestFields(
-//                    fieldWithPath("title").type(JsonFieldType.STRING)
-//                        .optional()
-//                        .description("게시글 제목"),
-//                    fieldWithPath("content").type(JsonFieldType.STRING)
-//                        .optional()
-//                        .description("게시글 내용"),
-//                    fieldWithPath("categoryId").type(JsonFieldType.NUMBER)
-//                        .optional()
-//                        .description("카테고리 코드"),
-//                    fieldWithPath("schoolId").type(JsonFieldType.NUMBER)
-//                        .optional()
-//                        .description("학교 코드"),
-//                    fieldWithPath("classId").type(JsonFieldType.NUMBER)
-//                        .description("학급 코드"),
-//                    fieldWithPath("useComment").type(JsonFieldType.BOOLEAN)
-//                        .optional()
-//                        .description("댓글 사용 여부"),
-//                    fieldWithPath("uploadFiles").type(JsonFieldType.ARRAY)
-//                        .description("이미지나 파일")
-//                ),
-//                responseFields(
-//                    fieldWithPath("code").type(JsonFieldType.NUMBER)
-//                        .description("코드"),
-//                    fieldWithPath("status").type(JsonFieldType.STRING)
-//                        .description("상태"),
-//                    fieldWithPath("message").type(JsonFieldType.STRING)
-//                        .description("메시지"),
-//                    fieldWithPath("data").type(JsonFieldType.OBJECT)
-//                        .description("응답 데이터"),
-//                    fieldWithPath("data.userName").type(JsonFieldType.STRING)
-//                        .description("작성자 이름"),
-//                    fieldWithPath("data.title").type(JsonFieldType.STRING)
-//                        .description("게시글 제목"),
-//                    fieldWithPath("data.content").type(JsonFieldType.STRING)
-//                        .description("게시글 내용"),
-//                    fieldWithPath("data.hit").type(JsonFieldType.NUMBER)
-//                        .description("조회수"),
-//                    fieldWithPath("data.categoryName").type(JsonFieldType.STRING)
-//                        .description("카테고리 이름"),
-//                    fieldWithPath("data.categoryId").type(JsonFieldType.NUMBER)
-//                        .description("카테고리 코드"),
-//                    fieldWithPath("data.createdDate").type(JsonFieldType.ARRAY)
-//                        .description("게시글 작성일시"),
-//                    fieldWithPath("data.uploadFileNum").type(JsonFieldType.NUMBER)
-//                        .description("이미지 수")
-//                )
-//            ));
-//    }
-//
+    @DisplayName("상담 예약 API")
+    @Test
+    void createConsult() throws Exception {
+        CreateConsultRequest request = CreateConsultRequest.builder()
+            .startTime(LocalDateTime.of(2023, 10, 15, 10, 30))
+            .content("우리아이 진로 상담 요청 드려요")
+            .codeId(1)
+            .build();
+
+        mockMvc.perform(
+                post("/consult-service/consult")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andDo(document("create-consult",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestFields(
+                    fieldWithPath("startTime").type(JsonFieldType.ARRAY)
+                        .optional()
+                        .description("상담 신청 일시"),
+                    fieldWithPath("content").type(JsonFieldType.STRING)
+                        .optional()
+                        .description("상담 내용"),
+                    fieldWithPath("codeId").type(JsonFieldType.NUMBER)
+                        .optional()
+                        .description("진행상태 코드")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.NUMBER)
+                        .description("응답 데이터(상담 PK)")
+                )
+            ));
+    }
+
 //    @DisplayName("교내 공지 목록 조회 API")
 //    @Test
 //    void searchBoards() throws Exception {
 //
 //        mockMvc.perform(
-//                get("/board-service/boards/{schoolId}/{userKey}", 1L, UUID.randomUUID().toString())
+//                get("/consult-service/boards/{schoolId}/{userKey}", 1L, UUID.randomUUID().toString())
 //                    .param("limit", "4")
 //                    .param("categoryId", "1")
 //            )
@@ -137,7 +105,7 @@ public class ConsultControllerDocsTest extends RestDocsSupport {
 //    void searchBoard() throws Exception {
 //
 //        mockMvc.perform(
-//                get("/board-service/boards/{schoolId}/{userKey}/{boardId}", 1L, UUID.randomUUID().toString(), 2L)
+//                get("/consult-service/boards/{schoolId}/{userKey}/{boardId}", 1L, UUID.randomUUID().toString(), 2L)
 //            )
 //            .andDo(print())
 //            .andExpect(status().isOk())
@@ -179,7 +147,7 @@ public class ConsultControllerDocsTest extends RestDocsSupport {
 //            .uploadFiles(new ArrayList<>())
 //            .build();
 //        mockMvc.perform(
-//                patch("/board-service/boards/{schoolId}/{userKey}/{boardId}", 1L, UUID.randomUUID().toString(), 2L)
+//                patch("/consult-service/boards/{schoolId}/{userKey}/{boardId}", 1L, UUID.randomUUID().toString(), 2L)
 //                    .content(objectMapper.writeValueAsString(request))
 //                    .contentType(MediaType.MULTIPART_FORM_DATA)
 //            )
@@ -231,7 +199,7 @@ public class ConsultControllerDocsTest extends RestDocsSupport {
 //    void deleteBoard() throws Exception {
 //
 //        mockMvc.perform(
-//                delete("/board-service/boards/{schoolId}/{userKey}/{boardId}", 1L, UUID.randomUUID().toString(), 2L)
+//                delete("/consult-service/boards/{schoolId}/{userKey}/{boardId}", 1L, UUID.randomUUID().toString(), 2L)
 //            )
 //            .andDo(print())
 //            .andExpect(status().isOk())
