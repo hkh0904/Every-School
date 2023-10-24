d8929fdaae64c332fa9ca1973e45bb01b616afae
신성주 <hans52410537@gmail.com>
2023-10-24 17:44:52 +0900

    test(report): 신고 등록/조회 API 테스트 구현

---
+++ server/report-service/src/main/java/com/everyschool/reportservice/api/controller/school/response/UserResponse.java
@@ -0,0 +1,20 @@
+package com.everyschool.reportservice.api.controller.school.response;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserResponse {
+    private int grade;
+    private int classNum;
+    private String name;
+
+    @Builder
+    public UserResponse(int grade, int classNum, String name) {
+        this.grade = grade;
+        this.classNum = classNum;
+        this.name = name;
+    }
+}
