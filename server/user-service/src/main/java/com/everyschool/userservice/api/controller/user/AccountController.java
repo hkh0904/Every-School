eeb6b77dd2e6699800fa793b72b4dc9281f681b3
임우택 <lyt1228@naver.com>
2023-10-25 14:01:49 +0900

    feat(user): 회원 이메일 조회 API 컨트롤러 구현
    Adds S09P31C108-128

---
+++ server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/AccountController.java
@@ -0,0 +1,33 @@
+package com.everyschool.userservice.api.controller.user;
+
+import com.everyschool.userservice.api.ApiResponse;
+import com.everyschool.userservice.api.controller.user.request.ForgotEmailRequest;
+import com.everyschool.userservice.api.service.user.UserQueryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+@RequestMapping("/forgot")
+public class AccountController {
+
+    private final UserQueryService userQueryService;
+
+    @GetMapping("/email")
+    public ApiResponse<String> forgotEmail(@Valid @RequestBody ForgotEmailRequest request) {
+        log.debug("call AccountController#forgotEmail");
+        log.debug("ForgotEmailRequest={}", request);
+
+        String maskingEmail = userQueryService.searchEmail(request.getName(), request.getBirth());
+        log.debug("maskingEmail={}", maskingEmail);
+
+        return ApiResponse.ok(maskingEmail);
+    }
+}
