e9fc8177a50517a9233a4d3108e058069ebb5b87
이지혁 <illu140411@gmail.com>
2023-10-25 14:23:48 +0900

    feat(user) : 로그인 화면 이동 로직 추가 adds #S09P31C108-187

--- app/everyschool/lib/page/home/home_page.dart
+++ app/everyschool/lib/page/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:everyschool/page/home/home_body.dart';
+import 'package:everyschool/page/login/login_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,21 +18,32 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
         centerTitle: true,
         title: Image.asset('assets/images/home/title.png', height: 50),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
-            child: Icon(Icons.notifications_none),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(5, 0, 10, 0),
-            child: Icon(Icons.settings),
-          )
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.notifications_none),
+              visualDensity:
+                  const VisualDensity(horizontal: -4.0, vertical: -4.0),
+              padding: const EdgeInsets.all(0), // 패딩을 조절합니다.
+              alignment: Alignment.center, // 아이콘을 가운데 정렬합니다.
+              splashRadius: 24.0, // 클릭 시 스플래시 효과의 반지름을 조절합니다.
+              onPressed: () {}),
+          IconButton(
+              icon: const Icon(Icons.settings),
+              visualDensity:
+                  const VisualDensity(horizontal: -2.0, vertical: -4.0),
+              padding: const EdgeInsets.all(0), // 패딩을 조절합니다.
+              alignment: Alignment.center, // 아이콘을 가운데 정렬합니다.
+              splashRadius: 24.0, // 클릭 시 스플래시 효과의 반지름을 조절합니다.
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()));
+              })
         ],
-        actionsIconTheme: IconThemeData(
+        actionsIconTheme: const IconThemeData(
           color: Colors.black,
         ),
       ),
-      body: HomeBody(),
+      body: const HomeBody(),
     );
   }
 }
