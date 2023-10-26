181822af271731bfc4f28051df9c129d6f4e5082
신성주 <hans52410537@gmail.com>
2023-10-26 15:44:19 +0900

    feat(school): school entity 생성
    Adds #S09P31C108-242

---
+++ server/school-service/src/main/java/com/everyschool/schoolservice/domain/school/School.java
@@ -0,0 +1,56 @@
+package com.everyschool.schoolservice.domain.school;
+
+import com.everyschool.schoolservice.domain.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "`school`")
+public class School extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "school_id")
+    private Long id;
+
+    @Column(nullable = false, updatable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false, length = 6)
+    private String zipcode;
+
+    @Column(nullable = false, length = 100)
+    private String address;
+
+    private String url;
+
+    @Column(unique = true, length = 13)
+    private String tel;
+
+    @Column(nullable = false)
+    private LocalDateTime openDate;
+
+    @Column(nullable = false)
+    private Integer codeId;
+
+    protected School() {
+        super();
+    }
+
+    @Builder
+    public School(String name, String zipcode, String address, String url, String tel, LocalDateTime openDate,
+                  Integer codeId) {
+        this();
+        this.name = name;
+        this.zipcode = zipcode;
+        this.address = address;
+        this.url = url;
+        this.tel = tel;
+        this.openDate = openDate;
+        this.codeId = codeId;
+    }
+}
