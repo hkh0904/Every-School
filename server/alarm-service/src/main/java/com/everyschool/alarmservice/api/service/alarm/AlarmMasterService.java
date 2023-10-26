e7a09ac7d44382522130c25e72845cec617456fb
임우택 <lyt1228@naver.com>
2023-10-26 15:35:38 +0900

    feat(alarm): 알림 삭제 서비스 구현
    Adds S09P31C108-144

--- server/alarm-service/src/main/java/com/everyschool/alarmservice/api/service/alarm/AlarmMasterService.java
+++ server/alarm-service/src/main/java/com/everyschool/alarmservice/api/service/alarm/AlarmMasterService.java
@@ -2,6 +2,7 @@ package com.everyschool.alarmservice.api.service.alarm;
 
 import com.everyschool.alarmservice.api.client.user.UserServiceClient;
 import com.everyschool.alarmservice.api.client.user.resquest.UserIdRequest;
+import com.everyschool.alarmservice.api.controller.alarm.response.RemoveAlarmResponse;
 import com.everyschool.alarmservice.api.controller.alarm.response.SendAlarmResponse;
 import com.everyschool.alarmservice.api.service.alarm.dto.CreateAlarmDto;
 import com.everyschool.alarmservice.domain.alarm.AlarmMaster;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -35,4 +38,15 @@ public class AlarmMasterService {
         return SendAlarmResponse.of(savedAlarmMaster);
     }
 
+    public RemoveAlarmResponse removeAlarmMaster(Long alarmMasterId) {
+        Optional<AlarmMaster> findAlarmMaster = alarmMasterRepository.findById(alarmMasterId);
+        if (findAlarmMaster.isEmpty()) {
+            throw new NoSuchElementException("등록되지 않은 알림입니다.");
+        }
+        AlarmMaster alarmMaster = findAlarmMaster.get();
+
+        alarmMaster.remove();
+
+        return RemoveAlarmResponse.of(alarmMaster);
+    }
 }
