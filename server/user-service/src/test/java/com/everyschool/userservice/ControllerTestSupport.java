03518460238de6a7024ca44b86bb0e57f0fc6bb7
임우택 <lyt1228@naver.com>
2023-10-26 09:54:00 +0900

    test(user): 상세 코드 API 컨트롤러 테스트 코드 구현
    Adds S09P31C108-133

--- server/user-service/src/test/java/com/everyschool/userservice/ControllerTestSupport.java
+++ server/user-service/src/test/java/com/everyschool/userservice/ControllerTestSupport.java
@@ -1,8 +1,12 @@
 package com.everyschool.userservice;
 
+import com.everyschool.userservice.api.controller.codedetail.CodeDetailController;
+import com.everyschool.userservice.api.controller.codedetail.CodeDetailQueryController;
 import com.everyschool.userservice.api.controller.codegroup.CodeGroupController;
 import com.everyschool.userservice.api.controller.codegroup.CodeGroupQueryController;
 import com.everyschool.userservice.api.controller.user.AccountController;
+import com.everyschool.userservice.api.service.codedetail.CodeDetailQueryService;
+import com.everyschool.userservice.api.service.codedetail.CodeDetailService;
 import com.everyschool.userservice.api.service.codegroup.CodeGroupQueryService;
 import com.everyschool.userservice.api.service.codegroup.CodeGroupService;
 import com.everyschool.userservice.api.service.user.UserQueryService;
@@ -11,9 +15,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {AccountController.class, CodeGroupController.class, CodeGroupQueryController.class})
+@WebMvcTest(controllers = {AccountController.class,
+    CodeGroupController.class, CodeGroupQueryController.class,
+    CodeDetailController.class, CodeDetailQueryController.class
+})
+@WithMockUser
 public abstract class ControllerTestSupport {
 
     @Autowired
@@ -33,4 +42,10 @@ public abstract class ControllerTestSupport {
 
     @MockBean
     private CodeGroupQueryService codeGroupQueryService;
+
+    @MockBean
+    private CodeDetailService codeDetailService;
+
+    @MockBean
+    private CodeDetailQueryService codeDetailQueryService;
 }
--- server/user-service/src/test/java/com/everyschool/userservice/api/controller/codedetail/CodeDetailQueryControllerTest.java
+++ server/user-service/src/test/java/com/everyschool/userservice/api/controller/codedetail/CodeDetailQueryControllerTest.java
@@ -1,9 +1,8 @@
-package com.everyschool.userservice.api.controller.codegroup;
+package com.everyschool.userservice.api.controller.codedetail;
 
 import com.everyschool.userservice.ControllerTestSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -11,24 +10,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WithMockUser
-class CodeGroupQueryControllerTest extends ControllerTestSupport {
+class CodeDetailQueryControllerTest extends ControllerTestSupport {
 
-    @DisplayName("코드 그룹 목록을 조회한다.")
+    @DisplayName("상세 코드 목록을 조회한다.")
     @Test
-    void searchCodeGroups() throws Exception {
+    void searchCodeDetails() throws Exception {
         //given
 
         //when //then
         mockMvc.perform(
-                get("/v1/code-groups")
+                get("/v1/code-groups/{groupId}/code-details", 1L)
                     .with(csrf())
             )
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.code").value("200"))
             .andExpect(jsonPath("$.status").value("OK"))
-            .andExpect(jsonPath("$.message").value("SUCCESS"))
-            .andExpect(jsonPath("$.data").isArray());
+            .andExpect(jsonPath("$.message").value("SUCCESS"));
     }
 }
