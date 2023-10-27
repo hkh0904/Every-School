71384a56e03e18a0cb425a4af2718c319429cb03
신성주 <hans52410537@gmail.com>
2023-10-27 15:07:24 +0900

    feat(call): call entity 생성
    Adds #S09P31C108-245

---
+++ server/call-service/src/main/java/com/everyschool/callservice/domain/call/Call.java
@@ -0,0 +1,61 @@
+package com.everyschool.callservice.domain.call;
+
+import com.everyschool.callservice.domain.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "call")
+public class Call extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "call_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Long teacherId;
+
+    @Column(nullable = false)
+    private Long otherUserId;
+
+    @Column(nullable = false, updatable = false, length = 1)
+    private String sender;
+
+    @Column(nullable = false)
+    private LocalDateTime startDateTime;
+
+    @Column(nullable = false)
+    private LocalDateTime endDateTime;
+
+    @Column(nullable = false, updatable = false, length = 200)
+    private String uploadFileName;
+
+    @Column(nullable = false, updatable = false, length = 200)
+    private String storeFileName;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private Boolean isBad;
+
+    protected Call() { super(); }
+
+    @Builder
+    private Call(Long teacherId, Long otherUserId, String sender, LocalDateTime startDateTime, LocalDateTime endDateTime,
+                String uploadFileName, String storeFileName, Boolean isBad) {
+        this();
+        this.teacherId = teacherId;
+        this.otherUserId = otherUserId;
+        this.sender = sender;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+        this.isBad = isBad;
+    }
+}
