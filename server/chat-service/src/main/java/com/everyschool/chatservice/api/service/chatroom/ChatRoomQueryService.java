2ca73d70c1bc265616d409b627347f42c555c742
이예리 <leeyr0412@naver.com>
2023-10-27 17:29:46 +0900

    feat(chat): 채팅방 조회 골격 생성
    Adds #S09P31C108-194

---
+++ server/chat-service/src/main/java/com/everyschool/chatservice/api/service/chatroom/ChatRoomQueryService.java
@@ -0,0 +1,32 @@
+package com.everyschool.chatservice.api.service.chatroom;
+
+import com.everyschool.chatservice.api.client.UserServiceClient;
+import com.everyschool.chatservice.api.client.response.UserInfo;
+import com.everyschool.chatservice.api.controller.chat.response.ChatRoomListResponse;
+import com.everyschool.chatservice.domain.chatroom.repository.ChatRoomQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ChatRoomQueryService {
+
+    private final ChatRoomQueryRepository chatRoomQueryRepository;
+    private final UserServiceClient userServiceClient;
+
+    /**
+     * 생성된 채팅방 목록 불러오기
+     *
+     * @param token
+     * @return
+     */
+    public List<ChatRoomListResponse> searchChatRooms(String token) {
+
+        UserInfo loginUser = userServiceClient.searchUserInfo(token);
+        return null;
+    }
+}
