d3f24e8757aa944192baba19526ee913cf8a0956
임우택 <lyt1228@naver.com>
2023-10-27 13:18:15 +0900

    refactor(school): 응답 데이터 수정
    Adds S09P31C108-246

--- server/school-service/src/main/java/com/everyschool/schoolservice/api/controller/school/response/SchoolResponse.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/api/controller/school/response/SchoolResponse.java
@@ -1,26 +1,19 @@
 package com.everyschool.schoolservice.api.controller.school.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SchoolResponse {
-    private Long id;
+
+    private Long schoolId;
     private String name;
     private String address;
-    private String url;
-    private String tel;
 
     @Builder
-    public SchoolResponse(Long id, String name, String address, String url, String tel) {
-        this.id = id;
+    public SchoolResponse(Long schoolId, String name, String address) {
+        this.schoolId = schoolId;
         this.name = name;
         this.address = address;
-        this.url = url;
-        this.tel = tel;
     }
 }
