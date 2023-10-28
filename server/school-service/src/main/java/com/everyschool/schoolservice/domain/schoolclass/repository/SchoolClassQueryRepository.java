18d640b98c1a2cc0965d2de5166fabf6c86a3251
Chaos0103 <lyt1228@naver.com>
2023-10-28 11:25:55 +0900

    feat(school): 담임 등록 여부 쿼리 구현
    Adds S09P31C108-240

--- server/school-service/src/main/java/com/everyschool/schoolservice/domain/schoolclass/repository/SchoolClassQueryRepository.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/domain/schoolclass/repository/SchoolClassQueryRepository.java
@@ -1,9 +1,12 @@
 package com.everyschool.schoolservice.domain.schoolclass.repository;
 
+import com.everyschool.schoolservice.domain.schoolclass.QSchoolClass;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+
+import static com.everyschool.schoolservice.domain.schoolclass.QSchoolClass.schoolClass;
 
 @Repository
 public class SchoolClassQueryRepository {
@@ -14,7 +17,15 @@ public class SchoolClassQueryRepository {
         this.queryFactory = new JPAQueryFactory(em);
     }
 
-    public boolean existByTeacherIdAndSchoolYear(Long teacherId, int year) {
-        return false;
+    public boolean existByTeacherIdAndSchoolYear(Long teacherId, int schoolYear) {
+        Long result = queryFactory
+            .select(schoolClass.id)
+            .from(schoolClass)
+            .where(
+                schoolClass.teacherId.eq(teacherId),
+                schoolClass.schoolYear.eq(schoolYear)
+            )
+            .fetchFirst();
+        return result != null;
     }
 }
