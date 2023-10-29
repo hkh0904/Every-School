e3bfd1acff22c0bbc218eb76e33ad5148a617c8f
Chaos0103 <lyt1228@naver.com>
2023-10-29 18:28:42 +0900

    feat(school): 회원 서비스 통신 API 구현
    Adds S09P31C108-263

---
+++ server/school-service/src/main/java/com/everyschool/schoolservice/api/client/response/StudentResponse.java
@@ -0,0 +1,19 @@
+package com.everyschool.schoolservice.api.client.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class StudentResponse {
+
+    private Long studentId;
+    private String name;
+    private String birth;
+
+    @Builder
+    public StudentResponse(Long studentId, String name, String birth) {
+        this.studentId = studentId;
+        this.name = name;
+        this.birth = birth;
+    }
+}
