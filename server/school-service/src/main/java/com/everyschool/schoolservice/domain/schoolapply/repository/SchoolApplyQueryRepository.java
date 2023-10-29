d806a9436971a36192731393dd3859c2581a4755
Chaos0103 <lyt1228@naver.com>
2023-10-30 08:18:11 +0900

    refactor(school): 쿼리 수정
    Adds S09P31C108-261

--- server/school-service/src/main/java/com/everyschool/schoolservice/domain/schoolapply/repository/SchoolApplyQueryRepository.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/domain/schoolapply/repository/SchoolApplyQueryRepository.java
@@ -1,9 +1,15 @@
 package com.everyschool.schoolservice.domain.schoolapply.repository;
 
+import com.everyschool.schoolservice.domain.schoolapply.SchoolApply;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.everyschool.schoolservice.domain.schoolapply.QSchoolApply.schoolApply;
+import static com.everyschool.schoolservice.domain.schoolclass.QSchoolClass.schoolClass;
 
 @Repository
 public class SchoolApplyQueryRepository {
@@ -12,5 +18,25 @@ public class SchoolApplyQueryRepository {
 
     public SchoolApplyQueryRepository(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<SchoolApply> findByTeacherId(Long teacherId, String status) {
+        return queryFactory
+            .select(schoolApply)
+            .from(schoolApply)
+            .join(schoolApply.schoolClass, schoolClass).fetchJoin()
+            .where(
+                schoolApply.schoolClass.teacherId.eq(teacherId),
+                statusCond(status)
+            )
+            .orderBy(schoolApply.createdDate.desc())
+            .fetch();
+    }
+
+    private BooleanExpression statusCond(String status) {
+        if (status.equals("wait")) {
+            return schoolApply.isApproved.isFalse().and(schoolApply.rejectedReason.isNull());
+        }
+        return schoolApply.isApproved.isTrue();
     }
 }
