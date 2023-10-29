e7643dc776200318adf03c489682f40efc45738f
Chaos0103 <lyt1228@naver.com>
2023-10-29 20:57:11 +0900

    refactor(school): 학급 등록 신청 엔티티 수정
    Adds S09P31C108-262

--- server/school-service/src/main/java/com/everyschool/schoolservice/domain/application/SchoolApply.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/domain/application/SchoolApply.java
@@ -1,60 +1,51 @@
 package com.everyschool.schoolservice.domain.application;
 
-import com.everyschool.schoolservice.domain.school.School;
+import com.everyschool.schoolservice.domain.BaseEntity;
 import com.everyschool.schoolservice.domain.schoolclass.SchoolClass;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
-@Table(name = "`school_apply`")
-public class SchoolApply {
+public class SchoolApply extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "school_apply_id")
     private Long id;
 
+    @Column(nullable = false, updatable = false)
+    private Integer schoolYear;
+
+    @Column(nullable = false)
+    private Boolean isApproved; //false
+
+    @Column(insertable = false, length = 50)
+    private String rejectedReason;
+
+    @Column(nullable = false)
+    private Integer codeId;
+
+    private Long studentId;
+
+    private Long parentId;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "school_class_id")
     private SchoolClass schoolClass;
 
-    @Column(nullable = false)
-    private Long studentId;
-
-    private int parentId;
-
-    @Column(nullable = false)
-    private int schoolYear;
-
-    @Column(nullable = false)
-    @ColumnDefault("false")
-    private boolean isApproved;
-
-    @Column(length = 50)
-    private String rejectedReason;
-
-    @Column(nullable = false)
-    private int codeId;
-
     protected SchoolApply() {
         super();
+        this.isApproved = false;
     }
 
     @Builder
-
-    public SchoolApply(SchoolClass schoolClass, Long studentId, int parentId, int schoolYear, boolean isApproved,
-                       String rejectedReason, int codeId) {
-        this();
-        this.schoolClass = schoolClass;
+    private SchoolApply(Integer schoolYear, Integer codeId, Long studentId, Long parentId, SchoolClass schoolClass) {
+        this.schoolYear = schoolYear;
+        this.codeId = codeId;
         this.studentId = studentId;
         this.parentId = parentId;
-        this.schoolYear = schoolYear;
-        this.isApproved = isApproved;
-        this.rejectedReason = rejectedReason;
-        this.codeId = codeId;
+        this.schoolClass = schoolClass;
     }
 }
