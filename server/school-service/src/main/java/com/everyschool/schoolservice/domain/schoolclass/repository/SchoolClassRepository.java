61660d78951f4564e6f33d0b0e53f697b63471b5
Chaos0103 <lyt1228@naver.com>
2023-10-29 18:29:34 +0900

    feat(school): 학급 조회 쿼리 구현
    Adds S09P31C108-263

--- server/school-service/src/main/java/com/everyschool/schoolservice/domain/schoolclass/repository/SchoolClassRepository.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/domain/schoolclass/repository/SchoolClassRepository.java
@@ -4,6 +4,10 @@ import com.everyschool.schoolservice.domain.schoolclass.SchoolClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SchoolClassRepository extends JpaRepository<SchoolClass, Long> {
+
+    Optional<SchoolClass> findByTeacherIdAndSchoolYear(Long teacherId, Integer schoolYear);
 }
