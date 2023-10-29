a0bab981168f24241b3a31a5747689098284503e
Chaos0103 <lyt1228@naver.com>
2023-10-29 19:03:58 +0900

    feat(school): 토큰 정보 추출 클래스 구현
    Adds S09P31C108-263

---
+++ server/school-service/src/main/java/com/everyschool/schoolservice/utils/TokenUtils.java
@@ -0,0 +1,39 @@
+package com.everyschool.schoolservice.utils;
+
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
+@Component
+public class TokenUtils {
+
+    private final Environment env;
+
+    public String getUserKey() {
+        String token = getTokenByHeader();
+
+        return getSubject(token);
+    }
+
+    private String getTokenByHeader() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        return authorizationHeader.replace("Bearer", "");
+    }
+
+    private String getSubject(String token) {
+        return Jwts.parserBuilder()
+            .setSigningKey(env.getProperty("token.secret"))
+            .build().parseClaimsJws(token).getBody()
+            .getSubject();
+    }
+}
