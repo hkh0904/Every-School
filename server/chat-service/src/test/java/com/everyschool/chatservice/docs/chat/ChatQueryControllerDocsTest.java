5358c14d7a21e6784ab7eb92a61191910d43cbaf
이예리 <leeyr0412@naver.com>
2023-10-29 17:54:41 +0900

    test(chat): 채팅방 목록 조회 Controller Test 구현
    Adds #S09P31C108-194

--- server/chat-service/src/test/java/com/everyschool/chatservice/docs/chat/ChatQueryControllerDocsTest.java
+++ server/chat-service/src/test/java/com/everyschool/chatservice/docs/chat/ChatQueryControllerDocsTest.java
@@ -1,73 +1,51 @@
 package com.everyschool.chatservice.docs.chat;
 
-import com.everyschool.chatservice.api.controller.chat.ChatController;
-import com.everyschool.chatservice.api.controller.chat.request.CreateChatRoomRequest;
-import com.everyschool.chatservice.api.controller.chat.response.CreateChatRoomResponse;
-import com.everyschool.chatservice.api.service.chatroom.ChatRoomService;
-import com.everyschool.chatservice.api.service.chatroom.dto.CreateChatRoomDto;
+import com.everyschool.chatservice.api.controller.chat.ChatQueryController;
+import com.everyschool.chatservice.api.controller.chat.response.ChatRoomListResponse;
+import com.everyschool.chatservice.api.service.chatroom.ChatRoomQueryService;
 import com.everyschool.chatservice.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
-import static org.mockito.ArgumentMatchers.any;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class ChatControllerDocsTest extends RestDocsSupport {
+public class ChatQueryControllerDocsTest extends RestDocsSupport {
 
-    @DisplayName("채팅방 생성 API")
+    @DisplayName("채팅방 목록 조회 API")
     @Test
-    void createChatRoom() throws Exception {
-        CreateChatRoomRequest request = CreateChatRoomRequest.builder()
-                .opponentUserKey("opponentUserKey")
-                .loginUserType('M')
-                .schoolClassId(1L)
-                .relation("1학년 1반 임우택(모)")
-                .build();
+    void searchChatRoomList() throws Exception {
 
-        CreateChatRoomResponse response = CreateChatRoomResponse.builder()
-                .roomId(1L)
-                .roomTitle("1학년 1반 임우택(모)")
-                .userName("신성주")
-                .build();
+        ChatRoomListResponse response1 = createChatRoomListResponse(1L, "1학년 2반 하현상 선생님", "아이패드 에어4", LocalDateTime.of(2023, 10, 10, 10, 10), 3);
 
-        BDDMockito.given(chatRoomService.createChatRoom(any(CreateChatRoomDto.class)))
-                .willReturn(response);
+        ChatRoomListResponse response2 = createChatRoomListResponse(2L, "1학년 5반 임동규 선생님", "아이패드 프로", LocalDateTime.of(2023, 10, 10, 12, 10), 0);
+
+        List<ChatRoomListResponse> responses = List.of(response1, response2);
+
+        BDDMockito.given(chatRoomQueryService.searchChatRooms(anyString()))
+                .willReturn(responses);
 
         mockMvc.perform(
-                        post("/chat-service/v1/chat-room")
-                                .content(objectMapper.writeValueAsString(request))
-                                .contentType(MediaType.APPLICATION_JSON)
+                        get("/chat-service/v1/chat-room")
                                 .header("Authorization", "jwt")
                 )
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andDo(document("create-chat-room",
-                        preprocessRequest(prettyPrint()),
+                .andExpect(status().isOk())
+                .andDo(document("search-chat-room-list",
                         preprocessResponse(prettyPrint()),
-                        requestFields(
-                                fieldWithPath("opponentUserKey").type(JsonFieldType.STRING)
-                                        .optional()
-                                        .description("상대방 유저 키"),
-                                fieldWithPath("loginUserType").type(JsonFieldType.STRING)
-                                        .optional()
-                                        .description("로그인 유저 타입"),
-                                fieldWithPath("schoolClassId").type(JsonFieldType.NUMBER)
-                                        .optional()
-                                        .description("학급 ID"),
-                                fieldWithPath("relation").type(JsonFieldType.STRING)
-                                        .optional()
-                                        .description("상대유저 관계(학생, 누구의 부/모)")
-                        ),
                         responseFields(
                                 fieldWithPath("code").type(JsonFieldType.NUMBER)
                                         .description("코드"),
@@ -75,14 +53,18 @@ public class ChatControllerDocsTest extends RestDocsSupport {
                                         .description("상태"),
                                 fieldWithPath("message").type(JsonFieldType.STRING)
                                         .description("메시지"),
-                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                fieldWithPath("data").type(JsonFieldType.ARRAY)
                                         .description("응답 데이터"),
-                                fieldWithPath("data.roomId").type(JsonFieldType.NUMBER)
-                                        .description("채팅방 ID"),
-                                fieldWithPath("data.roomTitle").type(JsonFieldType.STRING)
+                                fieldWithPath("data[].roomId").type(JsonFieldType.NUMBER)
+                                        .description("채팅방 PK"),
+                                fieldWithPath("data[].roomTitle").type(JsonFieldType.STRING)
                                         .description("채팅방 제목"),
-                                fieldWithPath("data.userName").type(JsonFieldType.STRING)
-                                        .description("상대방 이름")
+                                fieldWithPath("data[].lastMessage").type(JsonFieldType.STRING)
+                                        .description("채팅방 마지막 메세지"),
+                                fieldWithPath("data[].updateTime").type(JsonFieldType.ARRAY)
+                                        .description("채팅방 마지막 대화 시간"),
+                                fieldWithPath("data[].unreadMessageNum").type(JsonFieldType.NUMBER)
+                                        .description("읽지 않은 메세지 수")
                         )
                 ));
     }
@@ -144,10 +126,20 @@ public class ChatControllerDocsTest extends RestDocsSupport {
                 ));
     }
 
-    private final ChatRoomService chatRoomService = mock(ChatRoomService.class);
+    private static ChatRoomListResponse createChatRoomListResponse(long roomId, String roomTitle, String lastMessage, LocalDateTime updateTime, int unreadMessageNum) {
+        return ChatRoomListResponse.builder()
+                .roomId(roomId)
+                .roomTitle(roomTitle)
+                .lastMessage(lastMessage)
+                .updateTime(updateTime)
+                .unreadMessageNum(unreadMessageNum)
+                .build();
+    }
+
+    private final ChatRoomQueryService chatRoomQueryService = mock(ChatRoomQueryService.class);
 
     @Override
     protected Object initController() {
-        return new ChatController(chatRoomService);
+        return new ChatQueryController(chatRoomQueryService);
     }
 }
