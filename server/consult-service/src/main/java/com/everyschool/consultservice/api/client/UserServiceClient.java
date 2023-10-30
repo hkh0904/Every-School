53bffb6b139f911363c23db9aaaf04f32ec34e11
임우택 <lyt1228@naver.com>
2023-10-30 15:08:06 +0900

    feat(consult): 회원 서비스 통신 구현
    Adds S09P31C108-298

---
+++ server/consult-service/src/main/java/com/everyschool/consultservice/api/client/UserServiceClient.java
@@ -0,0 +1,20 @@
+package com.everyschool.consultservice.api.client;
+
+import com.everyschool.consultservice.api.client.response.TeacherInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient("user-service")
+public interface UserServiceClient {
+
+    // TODO: 2023-10-30 상대 구현
+    @PostMapping
+    Long searchByUserKey(@RequestBody String userKey);
+
+    // TODO: 2023-10-30 상대 구현
+    @PostMapping
+    List<TeacherInfo> searchTeacherByIdIn(@RequestBody List<Long> teacherIds);
+}
