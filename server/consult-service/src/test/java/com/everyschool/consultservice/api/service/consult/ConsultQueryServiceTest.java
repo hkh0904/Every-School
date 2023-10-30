68588f3caea83886c2689e167a4017553244c778
임우택 <lyt1228@naver.com>
2023-10-30 15:59:10 +0900

    test(consult): 상담 내역 상세 조회 서비스 테스트 코드 구현
    Adds S09P31C108-298

--- server/consult-service/src/test/java/com/everyschool/consultservice/api/service/consult/ConsultQueryServiceTest.java
+++ server/consult-service/src/test/java/com/everyschool/consultservice/api/service/consult/ConsultQueryServiceTest.java
@@ -5,6 +5,7 @@ import com.everyschool.consultservice.api.client.SchoolServiceClient;
 import com.everyschool.consultservice.api.client.UserServiceClient;
 import com.everyschool.consultservice.api.client.response.SchoolClassInfo;
 import com.everyschool.consultservice.api.client.response.TeacherInfo;
+import com.everyschool.consultservice.api.controller.consult.response.ConsultDetailResponse;
 import com.everyschool.consultservice.api.controller.consult.response.ConsultResponse;
 import com.everyschool.consultservice.domain.consult.Consult;
 import com.everyschool.consultservice.domain.consult.repository.ConsultRepository;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.anyList;
 import static org.mockito.BDDMockito.given;
@@ -71,6 +73,38 @@ class ConsultQueryServiceTest extends IntegrationTestSupport {
 
         //then
         assertThat(responses).hasSize(3);
+    }
+
+    @DisplayName("상담 PK로 상담 상세 내역을 조회할 수 있다.")
+    @Test
+    void searchConsult() {
+        //given
+        Consult consult = saveConsult("우리집 고양이 이름은 리온이에요!", 3003, 2001);
+
+        TeacherInfo teacherInfo = TeacherInfo.builder()
+            .userId(100L)
+            .name("이예리")
+            .schoolClassId(1000L)
+            .userKey(UUID.randomUUID().toString())
+            .build();
+
+        given(userServiceClient.searchTeacherById(anyLong()))
+            .willReturn(teacherInfo);
+
+        SchoolClassInfo schoolClassInfo = SchoolClassInfo.builder()
+            .teacherId(100L)
+            .grade(1)
+            .classNum(3)
+            .build();
+
+        given(schoolServiceClient.searchSchoolClassByTeacherId(anyLong()))
+            .willReturn(schoolClassInfo);
+
+        //when
+        ConsultDetailResponse response = consultQueryService.searchConsult(consult.getId());
+
+        //then
+        assertThat(response).isNotNull();
     }
 
     public Consult saveConsult(String message, int progressStatusId, int typeId) {
