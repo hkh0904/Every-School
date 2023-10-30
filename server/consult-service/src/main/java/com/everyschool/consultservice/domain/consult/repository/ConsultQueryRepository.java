46f49586b97cd80766858ae9c35cb16d4ce54384
임우택 <lyt1228@naver.com>
2023-10-30 15:07:15 +0900

    feat(consult): 상담 조회 쿼리 구현
    Adds S09P31C108-298

--- server/consult-service/src/main/java/com/everyschool/consultservice/domain/consult/repository/ConsultQueryRepository.java
+++ server/consult-service/src/main/java/com/everyschool/consultservice/domain/consult/repository/ConsultQueryRepository.java
@@ -1,9 +1,13 @@
 package com.everyschool.consultservice.domain.consult.repository;
 
+import com.everyschool.consultservice.domain.consult.Consult;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.everyschool.consultservice.domain.consult.QConsult.consult;
 
 @Repository
 public class ConsultQueryRepository {
@@ -12,5 +16,13 @@ public class ConsultQueryRepository {
 
     public ConsultQueryRepository(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<Consult> findByParentId(Long parentId) {
+        return queryFactory
+            .select(consult)
+            .from(consult)
+            .where(consult.parentId.eq(parentId))
+            .fetch();
     }
 }
