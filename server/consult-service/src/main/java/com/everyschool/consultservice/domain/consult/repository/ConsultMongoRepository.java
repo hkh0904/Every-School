b71300022396fa22e619c9e5a9826c58bfb71cd3
Chaos0103 <lyt1228@naver.com>
2023-10-30 19:36:33 +0900

    refactor(consult): 미사용 파일 제거
    Adds S09P31C108-298

--- server/consult-service/src/main/java/com/everyschool/consultservice/domain/consult/repository/ConsultMongoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.everyschool.consultservice.domain.consult.repository;
-
-import com.everyschool.consultservice.domain.consult.ConsultParentDocs;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConsultMongoRepository extends MongoRepository<ConsultParentDocs, Long> {
-}
