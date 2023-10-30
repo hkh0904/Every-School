f45d40c20046c014903623c5ecf621afeb7fd86b
임우택 <lyt1228@naver.com>
2023-10-30 11:07:37 +0900

    test(user): 회원 정보 조회 API docs 테스트 코드 구현
    Adds S09P31C108-261

--- server/user-service/src/test/java/com/everyschool/userservice/docs/client/UserClientControllerDocsTest.java
+++ server/user-service/src/test/java/com/everyschool/userservice/docs/client/UserClientControllerDocsTest.java
@@ -1,10 +1,12 @@
 package com.everyschool.userservice.docs.client;
 
 import com.everyschool.userservice.api.controller.client.UserClientController;
+import com.everyschool.userservice.api.controller.client.response.UserInfo;
 import com.everyschool.userservice.api.controller.user.request.UserIdRequest;
 import com.everyschool.userservice.api.controller.user.response.UserClientResponse;
 import com.everyschool.userservice.api.service.user.UserQueryService;
 import com.everyschool.userservice.docs.RestDocsSupport;
+import com.everyschool.userservice.utils.TokenUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -16,6 +18,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
@@ -27,10 +30,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UserClientControllerDocsTest extends RestDocsSupport {
 
     private final UserQueryService userQueryService = mock(UserQueryService.class);
+    private final TokenUtils tokenUtils = mock(TokenUtils.class);
 
     @Override
     protected Object initController() {
-        return new UserClientController(userQueryService);
+        return new UserClientController(userQueryService, tokenUtils);
     }
 
     @DisplayName("회원 PK 단건 조회 API")
@@ -48,10 +52,10 @@ public class UserClientControllerDocsTest extends RestDocsSupport {
             .willReturn(response);
 
         mockMvc.perform(
-            post("/client/v1/user-id")
-                .content(objectMapper.writeValueAsString(request))
-                .contentType(MediaType.APPLICATION_JSON)
-        )
+                post("/client/v1/user-id")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
             .andDo(print())
             .andExpect(status().isOk())
             .andDo(document("client-search-user-id",
@@ -68,4 +72,44 @@ public class UserClientControllerDocsTest extends RestDocsSupport {
                 )
             ));
     }
+
+    @DisplayName("회원 정보 조회 API")
+    @Test
+    void searchUserInfo() throws Exception {
+        given(tokenUtils.getUserKey())
+            .willReturn(UUID.randomUUID().toString());
+
+        UserInfo userInfo = UserInfo.builder()
+            .userId(1L)
+            .userType('S')
+            .userName("이예리")
+            .schoolClassId(100L)
+            .build();
+
+        given(userQueryService.searchUserInfo(anyString()))
+            .willReturn(userInfo);
+
+        mockMvc.perform(
+                get("/client/v1/user-info")
+                    .header("Authorization", "Bearer Access Token")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("client-search-user-info",
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("userId").type(JsonFieldType.NUMBER)
+                        .description("회원 id"),
+                    fieldWithPath("userType").type(JsonFieldType.STRING)
+                        .description("회원 타입"),
+                    fieldWithPath("userName").type(JsonFieldType.STRING)
+                        .description("회원 이름"),
+                    fieldWithPath("schoolClassId").type(JsonFieldType.NUMBER)
+                        .description("학급 id")
+                )
+            ));
+
+
+    }
+
 }
