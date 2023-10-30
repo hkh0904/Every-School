90c08e1dcbcea16bcba458e3cfc7ece1b13a3c28
임우택 <lyt1228@naver.com>
2023-10-30 15:07:27 +0900

    feat(consult): 학교 서비스 통신 구현
    Adds S09P31C108-298

---
+++ server/consult-service/src/main/java/com/everyschool/consultservice/api/client/SchoolServiceClient.java
@@ -0,0 +1,17 @@
+package com.everyschool.consultservice.api.client;
+
+import com.everyschool.consultservice.api.client.response.SchoolClassInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient("school-service")
+public interface SchoolServiceClient {
+
+    // TODO: 2023-10-30 상대 구현
+    @PostMapping
+    List<SchoolClassInfo> searchSchoolClassByTeacherIdIn(@RequestBody List<Long> teacherIds);
+
+}
