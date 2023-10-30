0f7d63de856f297f17daaad595859e120e8c2985
Chaos0103 <lyt1228@naver.com>
2023-10-30 23:14:46 +0900

    refactor(report): 신고 등록 서비스 골격 수정
    Adds S09P31C108-299

--- server/report-service/src/main/java/com/everyschool/reportservice/api/service/report/ReportService.java
+++ server/report-service/src/main/java/com/everyschool/reportservice/api/service/report/ReportService.java
@@ -1,10 +1,13 @@
 package com.everyschool.reportservice.api.service.report;
 
 import com.everyschool.reportservice.api.service.report.dto.CreateReportDto;
+import com.everyschool.reportservice.domain.report.UploadFile;
 import com.everyschool.reportservice.domain.report.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,7 +16,7 @@ public class ReportService {
 
     private final ReportRepository reportRepository;
 
-    public void createReport(String userKey, CreateReportDto dto) {
+    public void createReport(String userKey, CreateReportDto dto, List<UploadFile> uploadFiles) {
 
     }
 }
