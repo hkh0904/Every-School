aa1ad51ccaeb0fa31bbd15f62c39acea43f216a8
임우택 <lyt1228@naver.com>
2023-10-30 11:06:43 +0900

    feat(user): 회원 정보 객체 정보 생성
    Adds S09P31C108-261

---
+++ server/user-service/src/main/java/com/everyschool/userservice/api/controller/client/response/UserInfo.java
@@ -0,0 +1,42 @@
+package com.everyschool.userservice.api.controller.client.response;
+
+import com.everyschool.userservice.domain.user.Parent;
+import com.everyschool.userservice.domain.user.Student;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class UserInfo {
+
+    private Long userId;
+    private char userType;
+    private String userName;
+    private Long schoolClassId;
+
+    @Builder
+    private UserInfo(Long userId, char userType, String userName, Long schoolClassId) {
+        this.userId = userId;
+        this.userType = userType;
+        this.userName = userName;
+        this.schoolClassId = schoolClassId;
+    }
+
+    public static UserInfo of(Student student) {
+        return UserInfo.builder()
+            .userId(student.getId())
+            .userType('S')
+            .userName(student.getName())
+            .schoolClassId(student.getSchoolClassId())
+            .build();
+    }
+
+    public static UserInfo of(Parent parent) {
+        return UserInfo.builder()
+            .userId(parent.getId())
+            .userType(parent.getParentType().charAt(0))
+            .userName(parent.getName())
+            .schoolClassId(null)
+            .build();
+    }
+
+}
