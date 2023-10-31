21b17495c52209b96f70ad5bad7214df3ece110b
임우택 <lyt1228@naver.com>
2023-10-31 17:42:51 +0900

    feat(report): 편의 메서드 생성
    Adds S09P31C108-299

--- server/report-service/src/main/java/com/everyschool/reportservice/domain/report/ProgressStatus.java
+++ server/report-service/src/main/java/com/everyschool/reportservice/domain/report/ProgressStatus.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ProgressStatus {
 
-    Register(4001, "접수 완료"),
-    Process(4002, "처리중"),
-    finish(4003, "처리 완료");
+    REGISTER(4001, "접수 완료"),
+    PROCESS(4002, "처리중"),
+    FINISH(4003, "처리 완료");
 
     private final int code;
     private final String text;
@@ -15,5 +15,21 @@ public enum ProgressStatus {
     ProgressStatus(int code, String text) {
         this.code = code;
         this.text = text;
+    }
+
+    public static String getText(int code) {
+        if (code == 4001) {
+            return REGISTER.getText();
+        }
+
+        if (code == 4002) {
+            return PROCESS.getText();
+        }
+
+        if (code == 4003) {
+            return FINISH.getText();
+        }
+
+        throw new IllegalArgumentException();
     }
 }
