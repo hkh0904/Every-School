39632c2f3b286a9b5acfde50918e8d8541fac767
임우택 <lyt1228@naver.com>
2023-10-31 17:43:07 +0900

    feat(report): 조회 쿼리 구현
    Adds S09P31C108-299

--- server/report-service/src/main/java/com/everyschool/reportservice/domain/report/repository/ReportQueryRepository.java
+++ server/report-service/src/main/java/com/everyschool/reportservice/domain/report/repository/ReportQueryRepository.java
@@ -1,9 +1,15 @@
 package com.everyschool.reportservice.domain.report.repository;
 
+import com.everyschool.reportservice.api.controller.report.response.vo.ReceivedReportVo;
+import com.everyschool.reportservice.domain.report.ProgressStatus;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.everyschool.reportservice.domain.report.QReport.report;
 
 @Repository
 public class ReportQueryRepository {
@@ -12,5 +18,34 @@ public class ReportQueryRepository {
 
     public ReportQueryRepository(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<ReceivedReportVo> searchReports(Long schoolId, ProgressStatus status) {
+        return queryFactory
+            .select(Projections.constructor(
+                ReceivedReportVo.class,
+                report.id,
+                report.typeId,
+                report.lastModifiedDate
+            ))
+            .from(report)
+            .where(
+                report.schoolId.eq(schoolId),
+                report.progressStatusId.eq(status.getCode())
+            )
+            .orderBy(report.lastModifiedDate.desc())
+            .fetch();
+    }
+
+    public int searchCountReports(Long schoolId, ProgressStatus status) {
+        return queryFactory
+            .select(report.id)
+            .from(report)
+            .where(
+                report.schoolId.eq(schoolId),
+                report.progressStatusId.eq(status.getCode())
+            )
+            .fetch()
+            .size();
     }
 }
