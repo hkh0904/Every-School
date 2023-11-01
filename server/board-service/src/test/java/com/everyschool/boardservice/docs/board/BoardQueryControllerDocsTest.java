ec959bc8e55ad17fea773b850ae0471019a1f397
임우택 <lyt1228@naver.com>
2023-11-01 15:32:50 +0900

    test(board): 가정통신문 새 글 조회 API docs 테스트 코드 구현
    Adds S09P31C108-309

--- server/board-service/src/test/java/com/everyschool/boardservice/docs/board/BoardQueryControllerDocsTest.java
+++ server/board-service/src/test/java/com/everyschool/boardservice/docs/board/BoardQueryControllerDocsTest.java
@@ -1,6 +1,7 @@
 package com.everyschool.boardservice.docs.board;
 
 import com.everyschool.boardservice.api.controller.board.BoardQueryController;
+import com.everyschool.boardservice.api.controller.board.response.NewCommunicationResponse;
 import com.everyschool.boardservice.api.controller.board.response.NewFreeBoardResponse;
 import com.everyschool.boardservice.api.controller.board.response.NewNoticeResponse;
 import com.everyschool.boardservice.api.service.board.BoardQueryService;
@@ -72,10 +73,10 @@ public class BoardQueryControllerDocsTest extends RestDocsSupport {
     @DisplayName("공지사항 새 글 조회 API")
     @Test
     void searchNewNoticeBoards() throws Exception {
-        NewNoticeResponse response4 = createNewNoticeResponse(4L, "가정통신문 제목 4");
-        NewNoticeResponse response3 = createNewNoticeResponse(3L, "가정통신문 제목 3");
-        NewNoticeResponse response2 = createNewNoticeResponse(2L, "가정통신문 제목 2");
-        NewNoticeResponse response1 = createNewNoticeResponse(1L, "가정통신문 제목 1");
+        NewNoticeResponse response4 = createNewNoticeResponse(4L, "공지사항 제목 4");
+        NewNoticeResponse response3 = createNewNoticeResponse(3L, "공지사항 제목 3");
+        NewNoticeResponse response2 = createNewNoticeResponse(2L, "공지사항 제목 2");
+        NewNoticeResponse response1 = createNewNoticeResponse(1L, "공지사항 제목 1");
 
         given(boardQueryService.searchNewNoticeBoards(anyLong()))
             .willReturn(List.of(response4, response3, response2, response1));
@@ -104,6 +105,50 @@ public class BoardQueryControllerDocsTest extends RestDocsSupport {
                         .description("작성일")
                 )
             ));
+    }
+
+    @DisplayName("가정통신문 새 글 조회 API")
+    @Test
+    void searchNewCommunicationBoards() throws Exception {
+        NewCommunicationResponse response3 = createNewCommunicationResponse(3L, "가정통신문 제목 3");
+        NewCommunicationResponse response2 = createNewCommunicationResponse(2L, "가정통신문 제목 2");
+        NewCommunicationResponse response1 = createNewCommunicationResponse(1L, "가정통신문 제목 1");
+
+        given(boardQueryService.searchNewCommunicationBoards(anyLong()))
+            .willReturn(List.of(response3, response2, response1));
+
+        mockMvc.perform(
+                get("/board-service/v1/schools/{schoolId}/boards/new-communication", 1L)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("search-new-communication-boards",
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.ARRAY)
+                        .description("응답 데이터"),
+                    fieldWithPath("data[].boardId").type(JsonFieldType.NUMBER)
+                        .description("게시물 id"),
+                    fieldWithPath("data[].title").type(JsonFieldType.STRING)
+                        .description("게시물 제목"),
+                    fieldWithPath("data[].createdDate").type(JsonFieldType.ARRAY)
+                        .description("작성일")
+                )
+            ));
+    }
+
+    private NewCommunicationResponse createNewCommunicationResponse(long boardId, String title) {
+        return NewCommunicationResponse.builder()
+            .boardId(boardId)
+            .title(title)
+            .createdDate(LocalDateTime.now())
+            .build();
     }
 
     private NewNoticeResponse createNewNoticeResponse(long boardId, String title) {
