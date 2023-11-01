f33aa818eb0bc8e609ec8c0f18ecef79b71f2e5d
임우택 <lyt1228@naver.com>
2023-11-01 09:19:44 +0900

    refactor(report): 응답 객체 수정
    Adds S09P31C108-294

--- server/report-service/src/main/java/com/everyschool/reportservice/api/controller/report/response/ReceivedReportResponse.java
+++ server/report-service/src/main/java/com/everyschool/reportservice/api/controller/report/response/ReceivedReportResponse.java
@@ -1,20 +1,37 @@
 package com.everyschool.reportservice.api.controller.report.response;
 
-import com.everyschool.reportservice.api.controller.report.response.vo.ReceivedReportVo;
+import com.everyschool.reportservice.domain.report.ReportType;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class ReceivedReportResponse {
 
     private int count;
-    private List<ReceivedReportVo> reports;
+    private List<ReportVo> reports;
 
     @Builder
-    public ReceivedReportResponse(int count, List<ReceivedReportVo> reports) {
+    public ReceivedReportResponse(int count, List<ReportVo> reports) {
         this.count = count;
         this.reports = reports;
+    }
+
+    @Data
+    public static class ReportVo {
+
+        private int no;
+        private Long reportId;
+        private String type;
+        private LocalDateTime date;
+
+        @Builder
+        public ReportVo(Long reportId, int typeId, LocalDateTime date) {
+            this.reportId = reportId;
+            this.type = ReportType.getText(typeId);
+            this.date = date;
+        }
     }
 }
