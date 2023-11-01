af5084f285a9e6762fa0eeeeda8a81d59b22fe46
이예리 <leeyr0412@naver.com>
2023-11-01 17:10:28 +0900

    feat(chat): 채팅 필터링 Service 구현
    Adds #S09P31C108-315

--- server/chat-service/src/main/java/com/everyschool/chatservice/api/service/filterword/FilterWordService.java
+++ server/chat-service/src/main/java/com/everyschool/chatservice/api/service/filterword/FilterWordService.java
@@ -2,12 +2,22 @@ package com.everyschool.chatservice.api.service.filterword;
 
 import com.everyschool.chatservice.api.client.UserServiceClient;
 import com.everyschool.chatservice.api.client.response.UserInfo;
+import com.everyschool.chatservice.api.controller.chat.request.ChatMessage;
+import com.everyschool.chatservice.api.controller.filterword.response.ChatFilterResponse;
+import com.everyschool.chatservice.api.service.SequenceGeneratorService;
 import com.everyschool.chatservice.api.service.filterword.dto.CreateFilterWordDto;
+import com.everyschool.chatservice.domain.chat.Chat;
+import com.everyschool.chatservice.domain.chat.repository.ChatRepository;
 import com.everyschool.chatservice.domain.filterword.FilterWord;
+import com.everyschool.chatservice.domain.filterword.Reason;
 import com.everyschool.chatservice.domain.filterword.repository.FilterWordRepository;
+import com.everyschool.chatservice.domain.filterword.repository.ReasonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -15,16 +25,93 @@ import org.springframework.transaction.annotation.Transactional;
 public class FilterWordService {
 
     private final FilterWordRepository filterWordRepository;
+    private final ChatRepository chatRepository;
+    private final ReasonRepository reasonRepository;
+
+    private final SequenceGeneratorService sequenceGeneratorService;
 
     private final UserServiceClient userServiceClient;
 
+    /**
+     * 필터 단어 등록
+     *
+     * @param dto
+     * @return
+     */
     public Long createFilterWord(CreateFilterWordDto dto) {
         UserInfo loginUser = userServiceClient.searchUserInfo(dto.getLoginUserToken());
         if (loginUser.getUserType() != 'A') {
             throw new IllegalArgumentException("관리자 계정만 접근 가능합니다.");
         }
 
-        FilterWord saved = filterWordRepository.save(dto.toEntity());
+        FilterWord saved = saveFilterWord(dto);
         return saved.getId();
+    }
+
+    /**
+     * 메세지 보낼 때 채팅 등록하고 필터 적용하여 전송 가능 여부 출력
+     *
+     * @param message
+     * @return
+     */
+    public ChatFilterResponse sendMessage(ChatMessage message) {
+        UserInfo senderUserInfo = userServiceClient.searchUserInfoByUserKey(message.getSenderUserKey());
+        List<String> reasons = new ArrayList<>();
+        boolean isBad = isBadChat(message, reasons);
+
+        Chat chat = saveChat(message, senderUserInfo, isBad);
+
+        ChatFilterResponse response = ChatFilterResponse.builder()
+                .isBad(false)
+                .build();
+        getFilterResultResponse(reasons, isBad, chat, response);
+        return response;
+    }
+
+    private Chat saveChat(ChatMessage message, UserInfo senderUserInfo, boolean isBad) {
+        Chat chat = Chat.builder()
+                .id(sequenceGeneratorService.generateSequence(Chat.SEQUENCE_NAME))
+                .userId(senderUserInfo.getUserId())
+                .content(message.getMessage())
+                .isBad(isBad)
+                .chatRoomId(message.getChatRoomId())
+                .build();
+        return chatRepository.save(chat);
+    }
+
+    private void getFilterResultResponse(List<String> reasons, boolean isBad, Chat chat, ChatFilterResponse response) {
+        if (isBad) {
+            for (String reason : reasons) {
+                saveReason(chat, reason);
+            }
+            response.setReason("비속어가 포함되어 있습니다.");
+            response.setIsBad(true);
+        }
+    }
+
+    private void saveReason(Chat chat, String reason) {
+        Reason filterReason = Reason.builder()
+                .chatId(chat.getId())
+                .filterReason(reason)
+                .build();
+        reasonRepository.save(filterReason);
+    }
+
+    private boolean isBadChat(ChatMessage message, List<String> reasons) {
+        boolean isBad = false;
+        List<FilterWord> filters = filterWordRepository.findAll();
+        for (FilterWord filter : filters) {
+            if (message.getMessage().contains(filter.getWord())) {
+                reasons.add(filter.getWord());
+                isBad = true;
+            }
+        }
+        return isBad;
+    }
+
+    private FilterWord saveFilterWord(CreateFilterWordDto dto) {
+        FilterWord filterWord = dto.toEntity();
+        filterWord.setId(sequenceGeneratorService.generateSequence(FilterWord.SEQUENCE_NAME));
+        return filterWordRepository.save(filterWord);
     }
 }
