7541776628b52a7c5afd43d3d4692482d5180626
임우택 <lyt1228@naver.com>
2023-11-01 11:46:28 +0900

    feat(board): 게시판 엔티티 생성
    Adds S09P31C108-309

---
+++ server/board-service/src/main/java/com/everyschool/boardservice/domain/board/Board.java
@@ -0,0 +1,64 @@
+package com.everyschool.boardservice.domain.board;
+
+
+import com.everyschool.boardservice.domain.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private Integer codeId;
+
+    @Column(nullable = false, updatable = false)
+    private Long schoolId;
+
+    @Column(updatable = false)
+    private Long schoolClassId;
+
+    @Column(nullable = false, updatable = false)
+    private Long userId;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer hit;
+
+    @Column(nullable = false)
+    private Boolean isUsedComment;
+
+    @Column(nullable = false)
+    private Integer commentPeopleNum;
+
+    protected Board() {
+        super();
+        this.hit = 0;
+        this.commentPeopleNum = 0;
+    }
+
+    @Builder
+    private Board(Integer codeId, Long schoolId, Long schoolClassId, Long userId, String title, String content, Boolean isUsedComment) {
+        this();
+        this.codeId = codeId;
+        this.schoolId = schoolId;
+        this.schoolClassId = schoolClassId;
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+        this.isUsedComment = isUsedComment;
+    }
+}
