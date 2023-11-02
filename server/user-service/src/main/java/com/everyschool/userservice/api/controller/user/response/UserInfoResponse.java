a00607d29579d3f917635102739c9a0076e5f839
임우택 <lyt1228@naver.com>
2023-11-02 17:34:29 +0900

    feat(user): 회원 정보 객체 수정
    Adds S09P31C108-293

--- server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/response/UserInfoResponse.java
+++ server/user-service/src/main/java/com/everyschool/userservice/api/controller/user/response/UserInfoResponse.java
@@ -5,6 +5,8 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+import static com.everyschool.userservice.domain.user.UserType.getText;
+
 @Data
 public class UserInfoResponse {
 
@@ -12,14 +14,18 @@ public class UserInfoResponse {
     private String email;
     private String name;
     private String birth;
+    private Long schoolId;
+    private Long schoolClassId;
     private LocalDateTime joinDate;
 
     @Builder
-    public UserInfoResponse(String type, String email, String name, String birth, LocalDateTime joinDate) {
-        this.type = type;
+    public UserInfoResponse(int userTypeId, String email, String name, String birth, Long schoolId, Long schoolClassId, LocalDateTime joinDate) {
+        this.type = getText(userTypeId);
         this.email = email;
         this.name = name;
         this.birth = birth;
+        this.schoolId = schoolId;
+        this.schoolClassId = schoolClassId;
         this.joinDate = joinDate;
     }
 }
