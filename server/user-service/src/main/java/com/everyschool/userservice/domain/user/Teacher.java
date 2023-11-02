3bebfddec95edf129210a061678b185c8d619fe8
임우택 <lyt1228@naver.com>
2023-11-02 12:02:55 +0900

    refactor(user): 생성자 수정
    Adds S09P31C108-293

--- server/user-service/src/main/java/com/everyschool/userservice/domain/user/Teacher.java
+++ server/user-service/src/main/java/com/everyschool/userservice/domain/user/Teacher.java
@@ -1,6 +1,7 @@
 package com.everyschool.userservice.domain.user;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,8 +17,9 @@ public class Teacher extends User {
     private Long schoolId;
     private Long schoolClassId;
 
-    public Teacher(Long schoolId, Long schoolClassId) {
-        super();
+    @Builder
+    private Teacher(String email, String pwd, String name, String birth, String userKey, int userCodeId, Long schoolId, Long schoolClassId) {
+        super(email, pwd, name, birth, userKey, userCodeId);
         this.schoolId = schoolId;
         this.schoolClassId = schoolClassId;
     }
