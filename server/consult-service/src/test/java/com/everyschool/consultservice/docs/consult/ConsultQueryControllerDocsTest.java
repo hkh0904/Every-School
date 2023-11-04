82684548fe182c4c0f81014d00f05f97d563b8ba
Chaos0103 <lyt1228@naver.com>
2023-11-04 17:38:27 +0900

    test(consult): 상담 상세 조회 API docs 테스트 코드 구현
    Adds S09P31C108-293

--- server/consult-service/src/test/java/com/everyschool/consultservice/docs/consult/ConsultQueryControllerDocsTest.java
+++ server/consult-service/src/test/java/com/everyschool/consultservice/docs/consult/ConsultQueryControllerDocsTest.java
@@ -1,6 +1,7 @@
 package com.everyschool.consultservice.docs.consult;
 
 import com.everyschool.consultservice.api.controller.consult.ConsultQueryController;
+import com.everyschool.consultservice.api.controller.consult.response.ConsultDetailResponse;
 import com.everyschool.consultservice.api.controller.consult.response.WebConsultResponse;
 import com.everyschool.consultservice.api.service.consult.ConsultQueryService;
 import com.everyschool.consultservice.docs.RestDocsSupport;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static com.everyschool.consultservice.domain.consult.ConsultType.*;
+import static com.everyschool.consultservice.domain.consult.ProgressStatus.FINISH;
 import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -37,7 +39,7 @@ public class ConsultQueryControllerDocsTest extends RestDocsSupport {
         return new ConsultQueryController(consultQueryService, tokenUtils);
     }
 
-    @DisplayName("[교직원] 상담 조회 API")
+    @DisplayName("[교직원] 상담 목록 조회 API")
     @Test
     void searchConsults() throws Exception {
         given(tokenUtils.getUserKey())
@@ -82,6 +84,66 @@ public class ConsultQueryControllerDocsTest extends RestDocsSupport {
                         .description("상담 학부모 정보"),
                     fieldWithPath("data.content[].consultDate").type(JsonFieldType.ARRAY)
                         .description("상담 일시")
+                )
+            ));
+    }
+
+    @DisplayName("[교직원] 상담 상세 조회 API")
+    @Test
+    void searchConsult() throws Exception {
+        ConsultDetailResponse response = ConsultDetailResponse.builder()
+            .consultId(1L)
+            .schoolYear(2023)
+            .typeId(CALL.getCode())
+            .statusId(FINISH.getCode())
+            .title("10301 하예솔 학생 박연진 어머님")
+            .message("우리 예솔이가 아나운서 되고 싶다고 하네요.")
+            .resultContent("어머님이 아나운서 선배로서 조언을 많이 해주시면 될 것 같습니다.")
+            .rejectedReason("")
+            .consultDateTime(LocalDateTime.of(2023, 10, 12, 14, 0))
+            .createdDate(LocalDateTime.of(2023, 10, 1, 13, 27))
+            .build();
+
+        given(consultQueryService.searchConsult(anyLong()))
+            .willReturn(response);
+
+        mockMvc.perform(
+            get("/consult-service/v1/web/{schoolYear}/schools/{schoolId}/consults/{consultId}", 2023, 21617, 1)
+                .header("Authorization", "Bearer Access Token")
+        )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("search-consult",
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.consultId").type(JsonFieldType.NUMBER)
+                        .description("상담 id"),
+                    fieldWithPath("data.schoolYear").type(JsonFieldType.NUMBER)
+                        .description("상담 학년도"),
+                    fieldWithPath("data.type").type(JsonFieldType.STRING)
+                        .description("상담 유형"),
+                    fieldWithPath("data.status").type(JsonFieldType.STRING)
+                        .description("상담 상태"),
+                    fieldWithPath("data.title").type(JsonFieldType.STRING)
+                        .description("상담 제목"),
+                    fieldWithPath("data.message").type(JsonFieldType.STRING)
+                        .description("상담 메세지"),
+                    fieldWithPath("data.resultContent").type(JsonFieldType.STRING)
+                        .description("상담 결과 내용"),
+                    fieldWithPath("data.rejectedReason").type(JsonFieldType.STRING)
+                        .description("상담 거절 사유"),
+                    fieldWithPath("data.consultDateTime").type(JsonFieldType.ARRAY)
+                        .description("상담 일시"),
+                    fieldWithPath("data.createdDate").type(JsonFieldType.ARRAY)
+                        .description("상담 등록 일시")
                 )
             ));
     }
