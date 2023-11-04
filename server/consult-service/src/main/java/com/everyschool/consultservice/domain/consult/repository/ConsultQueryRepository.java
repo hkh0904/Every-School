8c28fd94266e1ca65efaa3b2450fcaabe14327bb
Chaos0103 <lyt1228@naver.com>
2023-11-04 12:26:04 +0900

    feat(consult): 승인 전 상담 내역 조회 쿼리 구현
    Adds S09P31C108-298

--- server/consult-service/src/main/java/com/everyschool/consultservice/domain/consult/repository/ConsultQueryRepository.java
+++ server/consult-service/src/main/java/com/everyschool/consultservice/domain/consult/repository/ConsultQueryRepository.java
@@ -1,6 +1,7 @@
 package com.everyschool.consultservice.domain.consult.repository;
 
 import com.everyschool.consultservice.domain.consult.Consult;
+import com.everyschool.consultservice.domain.consult.ProgressStatus;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
@@ -17,6 +18,20 @@ public class ConsultQueryRepository {
 
     public ConsultQueryRepository(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<Consult> findByTeacherIdAndSchoolYear(Long teacherId, int schoolYear) {
+        return queryFactory
+            .select(consult)
+            .from(consult)
+            .where(
+                consult.isDeleted.isFalse(),
+                consult.teacherId.eq(teacherId),
+                consult.schoolYear.eq(schoolYear),
+                consult.progressStatusId.eq(ProgressStatus.WAIT.getCode())
+            )
+            .orderBy(consult.consultDateTime.desc())
+            .fetch();
     }
 
     public List<Consult> findByParentId(Long userId, char userType) {
