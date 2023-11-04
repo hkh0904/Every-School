1a252d5c5c15649c991005dc074546c0aa3081f8
Chaos0103 <lyt1228@naver.com>
2023-11-04 18:15:53 +0900

    refactor(consult): 이름 변경
    Adds S09P31C108-293

--- server/consult-service/src/main/java/com/everyschool/consultservice/domain/consult/ProgressStatus.java
+++ server/consult-service/src/main/java/com/everyschool/consultservice/domain/consult/ProgressStatus.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum ProgressStatus {
 
     WAIT(5001, "승인 대기중"),
-    RESERVATION(5002, "예약 완료"),
+    RESERVATION(5002, "승인 완료"),
     FINISH(5003, "상담 완료"),
     REJECT(5004, "상담 불가");
 
