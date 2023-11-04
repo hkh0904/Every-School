d1e6c01e9540ba2234553c8b6aa187b7df200a1a
Chaos0103 <lyt1228@naver.com>
2023-11-04 22:27:12 +0900

    feat(report): 예외 메세지 공통 관리
    Adds S09P31C108-388

---
+++ server/report-service/src/main/java/com/everyschool/reportservice/error/ErrorMessage.java
@@ -0,0 +1,16 @@
+package com.everyschool.reportservice.error;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorMessage {
+
+    UNREGISTERED_PROGRESS_STATUS("등록이 되지 않은 신고 처리 상태입니다."),
+    UNREGISTERED_REPORT_TYPE("등록이 되지 않은 신고 유형입니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+}
