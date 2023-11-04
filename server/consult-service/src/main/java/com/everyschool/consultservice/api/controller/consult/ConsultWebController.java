b6ca3bc1664f276681fcbb885bb5fa99a712bb21
Chaos0103 <lyt1228@naver.com>
2023-11-04 18:15:31 +0900

    feat(consult): 컨트롤러 분리
    Adds S09P31C108-293

--- server/consult-service/src/main/java/com/everyschool/consultservice/api/controller/consult/ConsultWebController.java
+++ server/consult-service/src/main/java/com/everyschool/consultservice/api/controller/consult/ConsultWebController.java
@@ -1,16 +1,15 @@
 package com.everyschool.consultservice.api.controller.consult;
 
 import com.everyschool.consultservice.api.ApiResponse;
-import com.everyschool.consultservice.api.controller.consult.request.CreateConsultRequest;
+import com.everyschool.consultservice.api.controller.consult.request.FinishConsultRequest;
 import com.everyschool.consultservice.api.controller.consult.request.RejectConsultRequest;
 import com.everyschool.consultservice.api.controller.consult.response.ApproveConsultResponse;
-import com.everyschool.consultservice.api.controller.consult.response.CreateConsultResponse;
+import com.everyschool.consultservice.api.controller.consult.response.FinishConsultResponse;
 import com.everyschool.consultservice.api.controller.consult.response.RejectConsultResponse;
 import com.everyschool.consultservice.api.service.consult.ConsultService;
 import com.everyschool.consultservice.utils.TokenUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,34 +17,19 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/consult-service/v1/schools/{schoolId}/consults/{schoolYear}")
-public class ConsultController {
+@RequestMapping("/consult-service/v1/web/{schoolYear}/schools/{schoolId}/consults")
+public class ConsultWebController {
 
     private final ConsultService consultService;
     private final TokenUtils tokenUtils;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ApiResponse<CreateConsultResponse> createConsult(@Valid @RequestBody CreateConsultRequest request, @PathVariable Long schoolId) {
-        log.debug("call ConsultController#createConsult");
-        log.debug("CreateConsultRequest={}", request);
-
-        String userKey = tokenUtils.getUserKey();
-        log.debug("userKey={}", userKey);
-
-        CreateConsultResponse response = consultService.createConsult(userKey, schoolId, request.toDto());
-        log.debug("result={}", response);
-
-        return ApiResponse.created(response);
-    }
-
-    @PatchMapping("/approve/{consultId}")
+    @PatchMapping("/{consultId}/approve")
     public ApiResponse<ApproveConsultResponse> approveConsult(
         @PathVariable Long schoolId,
         @PathVariable Integer schoolYear,
         @PathVariable Long consultId
     ) {
-        log.debug("call ConsultController#approveConsult");
+        log.debug("call ConsultWebController#approveConsult");
 
         ApproveConsultResponse response = consultService.approveConsult(consultId);
         log.debug("ApproveConsultResponse={}", response);
@@ -53,14 +37,29 @@ public class ConsultController {
         return ApiResponse.ok(response);
     }
 
-    @PatchMapping("/reject/{consultId}")
+    @PatchMapping("/{consultId}/finish")
+    public ApiResponse<FinishConsultResponse> finishConsult(
+        @PathVariable Long schoolId,
+        @PathVariable Integer schoolYear,
+        @PathVariable Long consultId,
+        @Valid @RequestBody FinishConsultRequest request
+    ) {
+        log.debug("call ConsultWebController#finishConsult");
+
+        FinishConsultResponse response = consultService.finishConsult(consultId, request.getResultContent());
+        log.debug("FinishConsultResponse={}", response);
+
+        return ApiResponse.ok(response);
+    }
+
+    @PatchMapping("/{consultId}/reject")
     public ApiResponse<RejectConsultResponse> rejectConsult(
         @PathVariable Long schoolId,
         @PathVariable Integer schoolYear,
         @PathVariable Long consultId,
         @Valid @RequestBody RejectConsultRequest request
     ) {
-        log.debug("call ConsultController#rejectConsult");
+        log.debug("call ConsultWebController#rejectConsult");
 
         RejectConsultResponse response = consultService.rejectConsult(consultId, request.getRejectedReason());
         log.debug("ApproveConsultResponse={}", response);
