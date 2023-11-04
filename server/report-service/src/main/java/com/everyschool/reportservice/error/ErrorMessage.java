a05be549d72bd6c9c53996cdbc314f565d675338
Chaos0103 <lyt1228@naver.com>
2023-11-04 22:49:10 +0900

    feat(report): 에러 메세지 추가
    Adds S09P31C108-388

--- server/report-service/src/main/java/com/everyschool/reportservice/error/ErrorMessage.java
+++ server/report-service/src/main/java/com/everyschool/reportservice/error/ErrorMessage.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum ErrorMessage {
 
     UNREGISTERED_PROGRESS_STATUS("등록이 되지 않은 신고 처리 상태입니다."),
-    UNREGISTERED_REPORT_TYPE("등록이 되지 않은 신고 유형입니다.");
+    UNREGISTERED_REPORT_TYPE("등록이 되지 않은 신고 유형입니다."),
+    UNREGISTERED_REPORT("등록이 되지 않은 신고입니다.");
 
     private final String message;
 
