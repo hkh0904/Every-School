cf9728591ebd0d3ab171c93ae8b784edf2a3d42a
Chaos0103 <lyt1228@naver.com>
2023-11-05 00:15:34 +0900

    test(report): 신고 처리 결과 수정 API docs 테스트 코드 구현
    Adds S09P31C108-388

--- server/report-service/src/test/java/com/everyschool/reportservice/docs/web/report/ReportWebControllerDocsTest.java
+++ server/report-service/src/test/java/com/everyschool/reportservice/docs/web/report/ReportWebControllerDocsTest.java
@@ -1,13 +1,13 @@
 package com.everyschool.reportservice.docs.web.report;
 
 import com.everyschool.reportservice.api.web.controller.report.ReportWebController;
+import com.everyschool.reportservice.api.web.controller.report.request.EditResultRequest;
 import com.everyschool.reportservice.api.web.controller.report.response.EditReportResponse;
 import com.everyschool.reportservice.api.web.service.report.ReportWebService;
 import com.everyschool.reportservice.docs.RestDocsSupport;
-import com.everyschool.reportservice.domain.report.ProgressStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.time.LocalDateTime;
@@ -19,10 +19,8 @@ import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -44,6 +42,7 @@ public class ReportWebControllerDocsTest extends RestDocsSupport {
         EditReportResponse response = EditReportResponse.builder()
             .reportId(1L)
             .statusId(PROCESS.getCode())
+            .result("")
             .lastModifiedDate(LocalDateTime.now())
             .build();
 
@@ -51,10 +50,10 @@ public class ReportWebControllerDocsTest extends RestDocsSupport {
             .willReturn(response);
 
         mockMvc.perform(
-            patch(URL + "/{reportId}", 2023, 21617, 1)
-                .header("Authorization", "Bearer Access Token")
-                .param("status", "7002")
-        )
+                patch(URL + "/{reportId}", 2023, 21617, 1)
+                    .header("Authorization", "Bearer Access Token")
+                    .param("status", "7002")
+            )
             .andDo(print())
             .andExpect(status().isOk())
             .andDo(document("edit-status",
@@ -76,6 +75,61 @@ public class ReportWebControllerDocsTest extends RestDocsSupport {
                         .description("신고 id"),
                     fieldWithPath("data.status").type(JsonFieldType.STRING)
                         .description("신고 처리 상태"),
+                    fieldWithPath("data.result").type(JsonFieldType.STRING)
+                        .description("신고 처리 결과"),
+                    fieldWithPath("data.lastModifiedDate").type(JsonFieldType.ARRAY)
+                        .description("수정 일시")
+                )
+            ));
+    }
+
+    @DisplayName("신고 처리 결과 작성 API")
+    @Test
+    void editResult() throws Exception {
+        EditResultRequest request = EditResultRequest.builder()
+            .result("징계 위원회를 소집하겠습니다.")
+            .build();
+
+        EditReportResponse response = EditReportResponse.builder()
+            .reportId(1L)
+            .statusId(FINISH.getCode())
+            .result("징계 위원회를 소집하겠습니다.")
+            .lastModifiedDate(LocalDateTime.now())
+            .build();
+
+        given(reportWebService.editResult(anyLong(), anyString()))
+            .willReturn(response);
+
+        mockMvc.perform(
+            patch(URL + "/{reportId}/result", 2023, 21617, 1)
+                .header("Authorization", "Bearer Access Token")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("edit-result",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestFields(
+                    fieldWithPath("result").type(JsonFieldType.STRING)
+                        .description("신고 처리 결과")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.reportId").type(JsonFieldType.NUMBER)
+                        .description("신고 id"),
+                    fieldWithPath("data.status").type(JsonFieldType.STRING)
+                        .description("신고 처리 상태"),
+                    fieldWithPath("data.result").type(JsonFieldType.STRING)
+                        .description("신고 처리 결과"),
                     fieldWithPath("data.lastModifiedDate").type(JsonFieldType.ARRAY)
                         .description("수정 일시")
                 )
