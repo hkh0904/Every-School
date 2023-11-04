4a180c805991a4abe0e7189ca2aaa908bbc7898c
Chaos0103 <lyt1228@naver.com>
2023-11-04 23:08:17 +0900

    test(report): 신고 상세 조회 API docs 테스트 코드 구현
    Adds S09P31C108-388

--- server/report-service/src/test/java/com/everyschool/reportservice/docs/web/report/ReportWebQueryControllerDocsTest.java
+++ server/report-service/src/test/java/com/everyschool/reportservice/docs/web/report/ReportWebQueryControllerDocsTest.java
@@ -1,9 +1,11 @@
 package com.everyschool.reportservice.docs.web.report;
 
 import com.everyschool.reportservice.api.web.controller.report.ReportWebQueryController;
+import com.everyschool.reportservice.api.web.controller.report.response.ReportDetailResponse;
 import com.everyschool.reportservice.api.web.controller.report.response.ReportResponse;
 import com.everyschool.reportservice.api.web.service.report.ReportWebQueryService;
 import com.everyschool.reportservice.docs.RestDocsSupport;
+import com.everyschool.reportservice.domain.report.ReportType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -79,6 +81,74 @@ public class ReportWebQueryControllerDocsTest extends RestDocsSupport {
                         .description("신고 처리 상태"),
                     fieldWithPath("data.content[].date").type(JsonFieldType.ARRAY)
                         .description("신고 일시")
+                )
+            ));
+    }
+
+    @DisplayName("신고 상세 조회 API")
+    @Test
+    void searchReport() throws Exception {
+        ReportDetailResponse response = ReportDetailResponse.builder()
+            .reportId(1L)
+            .schoolYear(2023)
+            .typeId(VIOLENCE.getCode())
+            .statusId(REGISTER.getCode())
+            .witness("10301 문동은")
+            .who("박연진")
+            .when("2023-11-04 11시경")
+            .where("학교 체육관")
+            .what("고데기로 화상을 입혔습니다.")
+            .how("")
+            .why("")
+            .description("박연진이 고데기 열을 체크해달라며 제 팔에 댔습니다. 현재 화상을 입은 상태입니다.")
+            .files(List.of("file_url1", "file_url2"))
+            .build();
+
+        given(reportWebQueryService.searchReport(anyLong()))
+            .willReturn(response);
+
+        mockMvc.perform(
+                get(URL + "/{reportId}", 2023, 21617, 1)
+                    .header("Authorization", "Bearer Access Token")
+            ).andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("search-report",
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.reportId").type(JsonFieldType.NUMBER)
+                        .description("신고 id"),
+                    fieldWithPath("data.schoolYear").type(JsonFieldType.NUMBER)
+                        .description("신고 학년도"),
+                    fieldWithPath("data.type").type(JsonFieldType.STRING)
+                        .description("신고 유형"),
+                    fieldWithPath("data.witness").type(JsonFieldType.STRING)
+                        .description("목격자"),
+                    fieldWithPath("data.status").type(JsonFieldType.STRING)
+                        .description("신고 처리 상태"),
+                    fieldWithPath("data.who").type(JsonFieldType.STRING)
+                        .description("누가"),
+                    fieldWithPath("data.when").type(JsonFieldType.STRING)
+                        .description("언제"),
+                    fieldWithPath("data.where").type(JsonFieldType.STRING)
+                        .description("어디서"),
+                    fieldWithPath("data.what").type(JsonFieldType.STRING)
+                        .description("무엇을"),
+                    fieldWithPath("data.how").type(JsonFieldType.STRING)
+                        .description("어떻게"),
+                    fieldWithPath("data.why").type(JsonFieldType.STRING)
+                        .description("왜"),
+                    fieldWithPath("data.description").type(JsonFieldType.STRING)
+                        .description("신고 설명"),
+                    fieldWithPath("data.files").type(JsonFieldType.ARRAY)
+                        .description("첨부 파일")
                 )
             ));
     }
