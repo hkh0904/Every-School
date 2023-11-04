691fa66ae53d61bcc3bc3ed63c0b7bfb36220183
Chaos0103 <lyt1228@naver.com>
2023-11-04 18:15:42 +0900

    feat(consult): 상담 완료 객체 생성
    Adds S09P31C108-293

---
+++ server/consult-service/src/main/java/com/everyschool/consultservice/api/controller/consult/response/FinishConsultResponse.java
@@ -0,0 +1,38 @@
+package com.everyschool.consultservice.api.controller.consult.response;
+
+import com.everyschool.consultservice.domain.consult.Consult;
+import com.everyschool.consultservice.domain.consult.ConsultType;
+import com.everyschool.consultservice.domain.consult.ProgressStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class FinishConsultResponse {
+
+    private Long consultId;
+    private LocalDateTime consultDateTime;
+    private String progressStatus;
+    private String type;
+    private String resultContent;
+
+    @Builder
+    private FinishConsultResponse(Long consultId, LocalDateTime consultDateTime, int progressStatusId, int typeId, String resultContent) {
+        this.consultId = consultId;
+        this.consultDateTime = consultDateTime;
+        this.progressStatus = ProgressStatus.getText(progressStatusId);
+        this.type = ConsultType.getText(typeId);
+        this.resultContent = resultContent;
+    }
+
+    public static FinishConsultResponse of(Consult consult) {
+        return FinishConsultResponse.builder()
+            .consultId(consult.getId())
+            .consultDateTime(consult.getConsultDateTime())
+            .progressStatusId(consult.getProgressStatusId())
+            .typeId(consult.getTypeId())
+            .resultContent(consult.getResultContent())
+            .build();
+    }
+}
