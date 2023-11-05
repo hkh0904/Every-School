a08e2801a33fa0e614cb2b3d5af500e9207ac653
Chaos0103 <lyt1228@naver.com>
2023-11-05 23:23:22 +0900

    feat(consult): 조회 쿼리 구현
    Adds S09P31C108-388

--- server/consult-service/src/main/java/com/everyschool/consultservice/domain/consultschedule/repository/ConsultScheduleRepository.java
+++ server/consult-service/src/main/java/com/everyschool/consultservice/domain/consultschedule/repository/ConsultScheduleRepository.java
@@ -4,6 +4,10 @@ import com.everyschool.consultservice.domain.consultschedule.ConsultSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConsultScheduleRepository extends JpaRepository<ConsultSchedule, Long> {
+
+    Optional<ConsultSchedule> findByTeacherId(Long teacherId);
 }
