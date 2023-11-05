391c403af60c7bdbf68404495c3fd162d96e5632
Chaos0103 <lyt1228@naver.com>
2023-11-05 10:56:32 +0900

    feat(user): 회원 정보 조회 API 컨트롤러 구현
    Adds S09P31C108-388

---
+++ server/user-service/src/main/java/com/everyschool/userservice/api/web/controller/user/response/UserInfoResponse.java
@@ -0,0 +1,55 @@
+package com.everyschool.userservice.api.web.controller.user.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UserInfoResponse {
+
+    private String email;
+    private String name;
+    private String birth;
+    private School school;
+    private SchoolClass schoolClass;
+    private LocalDateTime joinDate;
+
+    @Builder
+    private UserInfoResponse(String email, String name, String birth, School school, SchoolClass schoolClass, LocalDateTime joinDate) {
+        this.email = email;
+        this.name = name;
+        this.birth = birth;
+        this.school = school;
+        this.schoolClass = schoolClass;
+        this.joinDate = joinDate;
+    }
+
+    @Data
+    public static class School {
+
+        private Long schoolId;
+        private String name;
+
+        @Builder
+        private School(Long schoolId, String name) {
+            this.schoolId = schoolId;
+            this.name = name;
+        }
+    }
+
+    @Data
+    public static class SchoolClass {
+
+        private Long schoolClassId;
+        private Integer grade;
+        private Integer classNum;
+
+        @Builder
+        private SchoolClass(Long schoolClassId, Integer grade, Integer classNum) {
+            this.schoolClassId = schoolClassId;
+            this.grade = grade;
+            this.classNum = classNum;
+        }
+    }
+}
