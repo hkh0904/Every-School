67f4d903139192a7d7a5f6d59cbfc478c7d309a7
이예리 <leeyr0412@naver.com>
2023-11-06 16:09:42 +0900

    build(openai): openai 서버 업로드
    Adds #S09P31C108-406

---
+++ server/openai-service/src/main/java/com/everyschool/openaiservice/OpenaiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.everyschool.openaiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaiServiceApplication.class, args);
+	}
+
+}
