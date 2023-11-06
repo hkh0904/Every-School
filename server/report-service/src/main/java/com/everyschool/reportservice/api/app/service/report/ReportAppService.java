2d163fb5611102c0c7de06bc42c067a87f116461
임우택 <lyt1228@naver.com>
2023-11-06 13:44:35 +0900

    feat(report): 신고 등록 서비스 구현
    Adds S09P31C108-388

---
+++ server/report-service/src/main/java/com/everyschool/reportservice/api/app/service/report/ReportAppService.java
@@ -0,0 +1,36 @@
+package com.everyschool.reportservice.api.app.service.report;
+
+import com.everyschool.reportservice.api.app.controller.report.response.CreateReportResponse;
+import com.everyschool.reportservice.api.client.UserServiceClient;
+import com.everyschool.reportservice.api.client.response.UserInfo;
+import com.everyschool.reportservice.api.service.report.dto.CreateReportDto;
+import com.everyschool.reportservice.domain.report.Report;
+import com.everyschool.reportservice.domain.report.ReportContent;
+import com.everyschool.reportservice.domain.report.UploadFile;
+import com.everyschool.reportservice.domain.report.repository.ReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class ReportAppService {
+
+    private final ReportRepository reportRepository;
+    private final UserServiceClient userServiceClient;
+
+    public CreateReportResponse createReport(String userKey, Long schoolId, Integer schoolYear, CreateReportDto dto, List<UploadFile> uploadFiles) {
+        UserInfo userInfo = userServiceClient.searchByUserKey(userKey);
+
+        ReportContent content = dto.toContent();
+
+        Report report = Report.createReport(dto.getTitle(), dto.getDescription(), content, schoolYear, dto.getTypeId(), schoolId, userInfo.getUserId(), uploadFiles);
+
+        Report savedReport = reportRepository.save(report);
+
+        return CreateReportResponse.of(savedReport);
+    }
+}
---
+++ server/report-service/src/main/java/com/everyschool/reportservice/api/app/service/report/dto/CreateReportDto.java
@@ -0,0 +1,43 @@
+package com.everyschool.reportservice.api.app.service.report.dto;
+
+import com.everyschool.reportservice.domain.report.ReportContent;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class CreateReportDto {
+
+    private Integer typeId;
+    private String title;
+    private String description;
+    private String who;
+    private String when;
+    private String where;
+    private String what;
+    private String how;
+    private String why;
+
+    @Builder
+    private CreateReportDto(Integer typeId, String title, String description, String who, String when, String where, String what, String how, String why) {
+        this.typeId = typeId;
+        this.title = title;
+        this.description = description;
+        this.who = who;
+        this.when = when;
+        this.where = where;
+        this.what = what;
+        this.how = how;
+        this.why = why;
+    }
+
+    public ReportContent toContent() {
+        return ReportContent.builder()
+            .reportWho(this.who)
+            .reportWhen(this.when)
+            .reportWhere(this.where)
+            .reportWhat(this.what)
+            .reportHow(this.how)
+            .reportWhy(this.why)
+            .build();
+    }
+}
