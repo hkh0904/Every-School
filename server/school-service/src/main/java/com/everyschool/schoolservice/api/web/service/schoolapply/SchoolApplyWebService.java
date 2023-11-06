c43b2085c217d05d52cbe4d94efc50ea623c3438
임우택 <lyt1228@naver.com>
2023-11-06 10:21:49 +0900

    feat(school): 등록 신청 승인, 거절 서비스 구현
    Adds S09P31C108-454

---
+++ server/school-service/src/main/java/com/everyschool/schoolservice/api/web/service/schoolapply/SchoolApplyWebService.java
@@ -0,0 +1,45 @@
+package com.everyschool.schoolservice.api.web.service.schoolapply;
+
+import com.everyschool.schoolservice.api.web.controller.schoolapply.response.EditSchoolApplyResponse;
+import com.everyschool.schoolservice.domain.schoolapply.SchoolApply;
+import com.everyschool.schoolservice.domain.schoolapply.repository.SchoolApplyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static com.everyschool.schoolservice.error.ErrorMessage.UNREGISTERED_SCHOOL_APPLY;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class SchoolApplyWebService {
+
+    private final SchoolApplyRepository schoolApplyRepository;
+
+    public EditSchoolApplyResponse approveSchoolApply(Long schoolApplyId) {
+        SchoolApply schoolApply = getSchoolApplyEntity(schoolApplyId);
+
+        SchoolApply approvedschoolApply = schoolApply.approve();
+
+        return EditSchoolApplyResponse.of(approvedschoolApply);
+    }
+
+    public EditSchoolApplyResponse rejectSchoolApply(Long schoolApplyId, String rejectedReason) {
+        SchoolApply schoolApply = getSchoolApplyEntity(schoolApplyId);
+
+        SchoolApply rejectedschoolApply = schoolApply.reject(rejectedReason);
+
+        return EditSchoolApplyResponse.of(rejectedschoolApply);
+    }
+
+    private SchoolApply getSchoolApplyEntity(Long schoolApplyId) {
+        Optional<SchoolApply> findSchoolApply = schoolApplyRepository.findById(schoolApplyId);
+        if (findSchoolApply.isEmpty()) {
+            throw new NoSuchElementException(UNREGISTERED_SCHOOL_APPLY.getMessage());
+        }
+        return findSchoolApply.get();
+    }
+}
--- server/school-service/src/main/java/com/everyschool/schoolservice/error/ErrorMessage.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/error/ErrorMessage.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum ErrorMessage {
 
     NOT_EXIST_MY_SCHOOL_CLASS("담당을 하는 학급이 존재하지 않습니다."),
-    UNREGISTERED_SCHOOL_CLASS("등록이 되지 않은 학급입니다.");
+    UNREGISTERED_SCHOOL_CLASS("등록이 되지 않은 학급입니다."),
+    UNREGISTERED_SCHOOL_APPLY("등록이 되지 않은 신청입니다.");
 
     private final String message;
 
