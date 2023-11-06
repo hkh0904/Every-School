0f484fdb9a02e0ec09d2cde2c96438d12eb09fb4
신성주 <hans52410537@gmail.com>
2023-11-06 15:19:15 +0900

    test(call): 통화 녹음 시작,종료 Test
    Adds #S09P31C108-286

--- server/call-service/src/main/java/com/everyschool/callservice/domain/callrecord/CallRecord.java
+++ server/call-service/src/main/java/com/everyschool/callservice/domain/callrecord/CallRecord.java
@@ -22,22 +22,36 @@ public class CallRecord extends BaseEntity {
     @JoinColumn(name = "call_id")
     private Call call;
 
-    @Column(nullable = false, updatable = false, length = 50)
+//    @Column(nullable = false, updatable = false, length = 50)
+//    private String content;
+//
+//    @Column(nullable = false, updatable = false)
+//    private LocalDateTime startDate;
+//
+//    @Column(nullable = false, updatable = false)
+//    private LocalDateTime endDate;
+
     private String content;
-
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime startDate;
-
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime endDate;
+    private int start;
+    private int length;
+    private String sentiment;
+    private Float neutral;
+    private Float positive;
+    private Float negative;
 
     protected CallRecord() { super(); }
 
     @Builder
-    private CallRecord(Call call, String content, LocalDateTime startDate, LocalDateTime endDate) {
+    public CallRecord(Call call, String content, int start, int length, String sentiment, Float neutral,
+                      Float positive, Float negative) {
+        this();
         this.call = call;
         this.content = content;
-        this.startDate = startDate;
-        this.endDate = endDate;
+        this.start = start;
+        this.length = length;
+        this.sentiment = sentiment;
+        this.neutral = neutral;
+        this.positive = positive;
+        this.negative = negative;
     }
 }
