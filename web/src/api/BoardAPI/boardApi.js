1e76eab7c6ea716e47cb4e11f4cd9520ad0879d9
오연주 <siunaus224@gmail.com>
2023-11-06 10:12:52 +0900

    feat: 가정통신문 api 연결 Adds #S09P31C108-396

--- web/src/api/BoardAPI/boardApi.js
+++ web/src/api/BoardAPI/boardApi.js
@@ -2,17 +2,21 @@ import { baseAxios } from '../Axios.jsx';
 
 export const clickNotiRegister = async (e, data) => {
   e.preventDefault();
-  const notiInfo = {
-    title: data.title,
-    content: data.content,
-    isUsedComment: false,
-    files: data.files
-  };
+  const formData = new FormData();
+  formData.append('title', data.title);
+  formData.append('content', data.content);
+  formData.append('isUsedComment', false);
+  // formData.append('files', data.fileName);
+
+  data.fileName.forEach((image) => {
+    formData.append('files', image);
+  });
 
   try {
-    const response = await baseAxios.post(`/board-service/v1/schools/1/boards/communications`, notiInfo, {
+    const response = await baseAxios.post(`/board-service/v1/web/2023/schools/100000/boards/communications`, formData, {
       headers: {
-        'Content-Type': 'multipart/form-data'
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${sessionStorage.getItem('token')}`
       }
     });
     console.log(response.data);
