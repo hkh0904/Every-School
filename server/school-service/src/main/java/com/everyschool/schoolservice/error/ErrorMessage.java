91f24dd8d07d820620871bc2844b15a78b57e067
임우택 <lyt1228@naver.com>
2023-11-06 09:34:01 +0900

    feat(school): 학생 오류 수정
    Adds S09P31C108-454

---
+++ server/school-service/src/main/java/com/everyschool/schoolservice/error/ErrorMessage.java
@@ -0,0 +1,16 @@
+package com.everyschool.schoolservice.error;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorMessage {
+
+    NOT_EXIST_MY_SCHOOL_CLASS("담당을 하는 학급이 존재하지 않습니다."),
+    UNREGISTERED_SCHOOL_CLASS("등록이 되지 않은 학급입니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+}
