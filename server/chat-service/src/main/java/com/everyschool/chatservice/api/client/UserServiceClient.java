c7b0426c137f2e8fb72f4ae8c42b824db29563d7
이예리 <leeyr0412@naver.com>
2023-11-06 17:55:47 +0900

    feat(chat): user client url 작성
    Adds #S09P31C108-406

--- server/chat-service/src/main/java/com/everyschool/chatservice/api/client/UserServiceClient.java
+++ server/chat-service/src/main/java/com/everyschool/chatservice/api/client/UserServiceClient.java
@@ -3,17 +3,18 @@ package com.everyschool.chatservice.api.client;
 import com.everyschool.chatservice.api.client.response.UserInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("user-service")
+@FeignClient(name = "user-service", url = "https://every-school.com/api")
 public interface UserServiceClient {
 
-    @GetMapping
+    @GetMapping("/user-service/client/v1/user-info")
     UserInfo searchUserInfo(@RequestHeader("Authorization") String accessToken);
 
-    @GetMapping
-    UserInfo searchUserInfoByUserKey(@RequestParam(name = "userKey") String userKey);
+    @GetMapping("/user-service/client/v1/user-info/{userKey}")
+    UserInfo searchUserInfoByUserKey(@PathVariable(name = "userKey") String userKey);
 
     @GetMapping
     String searchChildName(@RequestParam(name = "userId") Long userId, @RequestParam(name = "schoolClassId") Long schoolClassId);
