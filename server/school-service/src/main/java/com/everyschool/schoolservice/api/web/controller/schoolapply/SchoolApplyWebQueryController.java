150b181a89eb457422d975320475b46694009fd2
임우택 <lyt1228@naver.com>
2023-11-06 11:13:51 +0900

    feat(school): 신청 목록 조회 API 컨트롤러 구현
    Adds S09P31C108-454

---
+++ server/school-service/src/main/java/com/everyschool/schoolservice/api/web/controller/schoolapply/SchoolApplyWebQueryController.java
@@ -0,0 +1,49 @@
+package com.everyschool.schoolservice.api.web.controller.schoolapply;
+
+import com.everyschool.schoolservice.api.ApiResponse;
+import com.everyschool.schoolservice.api.Result;
+import com.everyschool.schoolservice.api.web.controller.schoolapply.response.SchoolApplyResponse;
+import com.everyschool.schoolservice.api.web.service.schoolapply.SchoolApplyWebQueryService;
+import com.everyschool.schoolservice.utils.TokenUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+@RequestMapping("/school-service/v1/web/{schoolYear}/schools/{schoolId}")
+public class SchoolApplyWebQueryController {
+
+    private final SchoolApplyWebQueryService schoolApplyWebQueryService;
+    private final TokenUtils tokenUtils;
+
+    @GetMapping("/wait-apply")
+    public ApiResponse<Result<SchoolApplyResponse>> searchWaitSchoolApply(
+        @PathVariable Integer schoolYear,
+        @PathVariable Long schoolId
+    ) {
+        String userKey = tokenUtils.getUserKey();
+
+        List<SchoolApplyResponse> response = schoolApplyWebQueryService.searchWaitSchoolApply(userKey, schoolYear);
+
+        return ApiResponse.ok(Result.of(response));
+    }
+
+    @GetMapping("/approve-apply")
+    public ApiResponse<Result<SchoolApplyResponse>> searchApproveSchoolApply(
+        @PathVariable Integer schoolYear,
+        @PathVariable Long schoolId
+    ) {
+        String userKey = tokenUtils.getUserKey();
+
+        List<SchoolApplyResponse> response = schoolApplyWebQueryService.searchApproveSchoolApply(userKey, schoolYear);
+
+        return ApiResponse.ok(Result.of(response));
+    }
+}
