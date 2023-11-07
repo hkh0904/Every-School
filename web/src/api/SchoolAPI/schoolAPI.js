9e375b84222bbfc632a8922bcb94a6a42b992441
hkh0904 <hkh0904@naver.com>
2023-11-07 12:01:38 +0900

    feat: Web 학급 API연결 Closes S09P31C108-351 Adds S09P31C108-456

--- web/src/api/SchoolAPI/schoolAPI.js
+++ web/src/api/SchoolAPI/schoolAPI.js
@@ -8,7 +8,21 @@ export const getParentList = async () => {
       }
     });
     console.log(response.data);
-    return response.data;
+    return response.data.data;
+  } catch (error) {
+    return 0;
+  }
+};
+
+export const getStudentList = async () => {
+  try {
+    const response = await baseAxios.get(`/school-service/v1/schools/1/classes/2023/students`, {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem('token')}`
+      }
+    });
+    // console.log(response.data);
+    return response.data.data;
   } catch (error) {
     return 0;
   }
