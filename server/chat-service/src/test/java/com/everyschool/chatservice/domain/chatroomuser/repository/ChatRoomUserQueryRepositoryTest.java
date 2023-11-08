ac325bf056ed5afa335e3b2e826dc87a1bf46b37
이예리 <leeyr0412@naver.com>
2023-11-08 23:49:36 +0900

    test(chat): user 2명으로 채팅방 찾기 Test 구현
    Adds #S09P31C108-409

--- server/chat-service/src/test/java/com/everyschool/chatservice/domain/chatroomuser/repository/ChatRoomUserQueryRepositoryTest.java
+++ server/chat-service/src/test/java/com/everyschool/chatservice/domain/chatroomuser/repository/ChatRoomUserQueryRepositoryTest.java
@@ -4,6 +4,7 @@ import com.everyschool.chatservice.IntegrationTestSupport;
 import com.everyschool.chatservice.domain.chatroom.ChatRoom;
 import com.everyschool.chatservice.domain.chatroom.repository.ChatRoomRepository;
 import com.everyschool.chatservice.domain.chatroomuser.ChatRoomUser;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,14 +28,7 @@ class ChatRoomUserQueryRepositoryTest extends IntegrationTestSupport {
     void findChatRoomUserByRoomIdAndUserId() {
         //given
         ChatRoom chatRoom = saveChatRoom();
-        chatRoomUserRepository.save(ChatRoomUser.builder()
-                .chatRoomTitle("1학년 2반 홍경환(부)")
-                .socketTopic(String.valueOf(chatRoom.getId()))
-                .userId(1L)
-                .isAlarm(true)
-                .unreadCount(0)
-                .chatRoom(chatRoom)
-                .build());
+        saveChatRoomUser(chatRoom, 1L);
 
         //when
         Optional<ChatRoomUser> chatRoomUser1 = chatRoomUserQueryRepository.findChatRoomUserByRoomIdAndUserId(chatRoom.getId(), 1L);
@@ -45,6 +39,33 @@ class ChatRoomUserQueryRepositoryTest extends IntegrationTestSupport {
         assertThat(chatRoomUser1).isPresent();
         assertThat(chatRoomUser1.get().getChatRoomTitle()).isEqualTo("1학년 2반 홍경환(부)");
         assertThat(chatRoomUser2).isEmpty();
+    }
+
+    @DisplayName("유저 2명으로 채팅방 찾기")
+    @Test
+    void findChatRoomIdByTwoUserId() {
+        //given
+        ChatRoom chatRoom = saveChatRoom();
+        ChatRoomUser chatRoomUser1 = saveChatRoomUser(chatRoom, 1L);
+        ChatRoomUser chatRoomUser2 = saveChatRoomUser(chatRoom, 2L);
+
+        //when
+        Optional<Long> find = chatRoomUserQueryRepository.findChatRoomIdByTwoUserId(1L, 2L);
+
+        //then
+        assertThat(find).isPresent();
+    }
+
+    @NotNull
+    private ChatRoomUser saveChatRoomUser(ChatRoom chatRoom, long userId) {
+        return chatRoomUserRepository.save(ChatRoomUser.builder()
+                .chatRoomTitle("1학년 2반 홍경환(부)")
+                .socketTopic(String.valueOf(chatRoom.getId()))
+                .userId(userId)
+                .isAlarm(true)
+                .unreadCount(0)
+                .chatRoom(chatRoom)
+                .build());
     }
 
     private ChatRoom saveChatRoom() {
