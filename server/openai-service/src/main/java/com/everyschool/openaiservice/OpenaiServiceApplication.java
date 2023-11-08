1b45c446cf8cb26aa642f8a873ce5415a9adc798
이예리 <leeyr0412@naver.com>
2023-11-08 09:20:06 +0900

    feat(chat): FeignClients 어노테이션 작성
    Adds #S09P31C108-406

--- server/openai-service/src/main/java/com/everyschool/openaiservice/OpenaiServiceApplication.java
+++ server/openai-service/src/main/java/com/everyschool/openaiservice/OpenaiServiceApplication.java
@@ -2,12 +2,14 @@ package com.everyschool.openaiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OpenaiServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OpenaiServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OpenaiServiceApplication.class, args);
+    }
 
 }
