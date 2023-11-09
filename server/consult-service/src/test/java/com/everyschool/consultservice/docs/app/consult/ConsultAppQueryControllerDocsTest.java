a0390f58d5a0bd818c9f4150442dc4a89b05985a
Chaos0103 <lyt1228@naver.com>
2023-11-09 19:41:17 +0900

    test(consult): 교직원용 상담 목록 조회 API docs 테스트 코드 구현
    Adds S09P31C108-481

--- server/consult-service/src/test/java/com/everyschool/consultservice/docs/app/consult/ConsultAppQueryControllerDocsTest.java
+++ server/consult-service/src/test/java/com/everyschool/consultservice/docs/app/consult/ConsultAppQueryControllerDocsTest.java
@@ -43,11 +43,11 @@ public class ConsultAppQueryControllerDocsTest extends RestDocsSupport {
         given(tokenUtils.getUserKey())
             .willReturn(UUID.randomUUID().toString());
 
-        ConsultResponse response3 = createConsultResponse(3L, FINISH.getCode());
-        ConsultResponse response2 = createConsultResponse(2L, REJECT.getCode());
-        ConsultResponse response1 = createConsultResponse(1L, WAIT.getCode());
+        ConsultResponse response3 = createConsultResponse(3L, FINISH.getCode(), "2학년 3반 이예리 선생님");
+        ConsultResponse response2 = createConsultResponse(2L, REJECT.getCode(), "2학년 3반 이예리 선생님");
+        ConsultResponse response1 = createConsultResponse(1L, WAIT.getCode(), "2학년 3반 이예리 선생님");
 
-        given(consultAppQueryService.searchConsults(anyString(), anyInt()))
+        given(consultAppQueryService.searchConsultsByParent(anyString(), anyInt()))
             .willReturn(List.of(response1, response2, response3));
 
         mockMvc.perform(
@@ -81,12 +81,56 @@ public class ConsultAppQueryControllerDocsTest extends RestDocsSupport {
             ));
     }
 
-    private ConsultResponse createConsultResponse(Long consultId, int status) {
+    @DisplayName("교직원 상담 내역 조회 API")
+    @Test
+    void searchConsultsByTeacher() throws Exception {
+        given(tokenUtils.getUserKey())
+            .willReturn(UUID.randomUUID().toString());
+
+        ConsultResponse response3 = createConsultResponse(3L, FINISH.getCode(), "2학년 3반 임우택 어머님");
+        ConsultResponse response2 = createConsultResponse(2L, REJECT.getCode(), "2학년 3반 이예리 어머님");
+        ConsultResponse response1 = createConsultResponse(1L, WAIT.getCode(), "2학년 3반 신성주 아버님");
+
+        given(consultAppQueryService.searchConsultsByTeacher(anyString(), anyInt()))
+            .willReturn(List.of(response1, response2, response3));
+
+        mockMvc.perform(
+                get(BASE_URL + "/teacher", 2023, 100000)
+                    .header("Authorization", "Bearer Access Token")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("search-consults-by-teacher",
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.ARRAY)
+                        .description("응답 데이터"),
+                    fieldWithPath("data[].consultId").type(JsonFieldType.NUMBER)
+                        .description("상담 아이디"),
+                    fieldWithPath("data[].type").type(JsonFieldType.STRING)
+                        .description("상담 유형"),
+                    fieldWithPath("data[].status").type(JsonFieldType.STRING)
+                        .description("상담 진행 상태"),
+                    fieldWithPath("data[].info").type(JsonFieldType.STRING)
+                        .description("상담 교직원 정보"),
+                    fieldWithPath("data[].consultDateTime").type(JsonFieldType.ARRAY)
+                        .description("상담 일시")
+                )
+            ));
+    }
+
+    private ConsultResponse createConsultResponse(Long consultId, int status, String info) {
         return ConsultResponse.builder()
             .consultId(consultId)
             .type(ConsultType.VISIT.getCode())
             .status(status)
-            .info("2학년 3반 이예리 선생님")
+            .info(info)
             .consultDateTime(LocalDateTime.now())
             .build();
     }
