e95c8c8889576517a3dd96439f17275bd4dd3c62
Chaos0103 <lyt1228@naver.com>
2023-11-09 19:22:12 +0900

    feat(consult): 학부모용 상담 목록 조회 쿼리 구현
    Adds S09P31C108-481

---
+++ server/consult-service/src/main/java/com/everyschool/consultservice/domain/consult/repository/ConsultAppQueryRepository.java
@@ -0,0 +1,46 @@
+package com.everyschool.consultservice.domain.consult.repository;
+
+import com.everyschool.consultservice.api.app.controller.consult.response.ConsultResponse;
+import com.everyschool.consultservice.domain.consult.Consult;
+import com.everyschool.consultservice.domain.consult.QConsult;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static com.everyschool.consultservice.domain.consult.QConsult.*;
+
+@Repository
+public class ConsultAppQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public ConsultAppQueryRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<ConsultResponse> findByParentIdAndSchoolYear(Long parentId, int schoolYear) {
+        return queryFactory
+            .select(
+                Projections.constructor(
+                    ConsultResponse.class,
+                    consult.id,
+                    consult.typeId,
+                    consult.progressStatusId,
+                    consult.title.teacherTitle,
+                    consult.consultDateTime
+                )
+            )
+            .from(consult)
+            .where(
+                consult.isDeleted.isFalse(),
+                consult.schoolYear.eq(schoolYear),
+                consult.parentId.eq(parentId)
+            )
+            .orderBy(consult.lastModifiedDate.desc())
+            .fetch();
+    }
+}
