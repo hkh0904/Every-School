e7e024fd32c2c94c373fb32d932c11b14e399ac5
이예리 <leeyr0412@naver.com>
2023-11-09 14:54:03 +0900

    feat(chat): 채팅 전송 시 알림 전송 부분 구현
    Adds #S09P31C108-409

--- server/chat-service/src/main/java/com/everyschool/chatservice/api/service/chat/dto/ChatService.java
+++ server/chat-service/src/main/java/com/everyschool/chatservice/api/service/chat/dto/ChatService.java
@@ -5,7 +5,12 @@ import com.everyschool.chatservice.api.client.response.UserInfo;
 import com.everyschool.chatservice.api.service.util.RedisUtils;
 import com.everyschool.chatservice.domain.chat.Chat;
 import com.everyschool.chatservice.domain.chat.repository.ChatRepository;
+import com.everyschool.chatservice.domain.chatroomuser.ChatRoomUser;
 import com.everyschool.chatservice.domain.chatroomuser.repository.ChatRoomUserQueryRepository;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +32,7 @@ public class ChatService {
     private final ChatRoomUserQueryRepository chatRoomUserQueryRepository;
 
     private final UserServiceClient userServiceClient;
+    private final FirebaseMessaging firebaseMessaging;
 
     /**
      * date에 채팅 있던 채팅방 목록 반환하기
@@ -47,8 +53,8 @@ public class ChatService {
         return responses;
     }
 
-
-    public void sendMessageProcessing(SendMessageDto dto) {
+    // TODO: 2023-11-09 리팩토링 하기 
+    public void sendMessageProcessing(SendMessageDto dto) throws FirebaseMessagingException {
         // 채팅방 인원수 확인
         String chatRoomUserCountKey = "CHAR_ROOM_USER_COUNT_" + dto.getChatRoomId();
         String roomUserCount = redisUtil.getString(chatRoomUserCountKey);
@@ -59,8 +65,34 @@ public class ChatService {
             // 채팅방 다른 유저 가져오기
             Long opponentUserId = chatRoomUserQueryRepository.findOpponentUserId(dto.getChatRoomId(), senderUser.getUserId())
                     .orElseThrow(() -> new NoSuchElementException("상대 유저 정보를 찾을 수 없습니다."));
-            // TODO: 2023-11-02 해당 유저한테 알림 보내기
+            String notificationMessage = dto.getMessage();
+            if (notificationMessage.length() > 50) {
+                notificationMessage = notificationMessage.substring(0, 50) + "...";
+            }
+            Notification notification = Notification.builder()
+                    .setTitle(senderUser.getUserName())
+                    .setBody(notificationMessage)
+                    .build();
 
+            String childName = "";
+
+            if (senderUser.getUserType() == 'M' || senderUser.getUserType() == 'F') {
+                ChatRoomUser senderChatRoomUser = chatRoomUserQueryRepository.findChatRoomUserByRoomIdAndUserId(dto.getChatRoomId(), senderUser.getUserId())
+                        .orElseThrow(() -> new NoSuchElementException("상대 유저를 찾을 수 없습니다."));
+                childName = senderChatRoomUser.getChildName();
+            }
+
+            String fcmToken = userServiceClient.searchFcmTokenByUserId(opponentUserId);
+            Message message = Message.builder()
+                    .setToken(fcmToken)
+                    .setNotification(notification)
+                    .putData("type", "chat")
+                    .putData("senderUserName", senderUser.getUserName())
+                    .putData("senderUserType", String.valueOf(senderUser.getUserType()))
+                    .putData("senderUserChildName", childName)
+                    .putData("chatRoomId", String.valueOf(dto.getChatRoomId()))
+                    .build();
+            firebaseMessaging.send(message);
         }
     }
 }
