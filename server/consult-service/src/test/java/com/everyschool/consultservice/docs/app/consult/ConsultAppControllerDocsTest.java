71ed4d6f0cc4b0a2764accadcb8c7250eeddb0b9
Chaos0103 <lyt1228@naver.com>
2023-11-09 17:30:41 +0900

    test(consult): 상담 등록 API docs 테스트 코드 구현
    Adds S09P31C108-481

--- server/consult-service/src/test/java/com/everyschool/consultservice/docs/app/consult/ConsultAppControllerDocsTest.java
+++ server/consult-service/src/test/java/com/everyschool/consultservice/docs/app/consult/ConsultAppControllerDocsTest.java
@@ -1,68 +1,73 @@
-package com.everyschool.consultservice.docs.consult;
+package com.everyschool.consultservice.docs.app.consult;
 
 import com.everyschool.consultservice.api.app.controller.consult.ConsultAppController;
+import com.everyschool.consultservice.api.app.controller.consult.request.CreateConsultRequest;
+import com.everyschool.consultservice.api.app.controller.consult.response.CreateConsultResponse;
 import com.everyschool.consultservice.api.app.service.consult.ConsultAppService;
-import com.everyschool.consultservice.api.controller.consult.request.CreateConsultRequest;
-import com.everyschool.consultservice.api.controller.consult.response.CreateConsultResponse;
 import com.everyschool.consultservice.docs.RestDocsSupport;
+import com.everyschool.consultservice.domain.consult.ConsultType;
 import com.everyschool.consultservice.utils.TokenUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static com.everyschool.consultservice.domain.consult.ConsultType.VISIT;
 import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class ConsultAppControllerDocsTest extends RestDocsSupport {
 
-    private final ConsultAppService consultService = mock(ConsultAppService.class);
+    private final ConsultAppService consultAppService = mock(ConsultAppService.class);
     private final TokenUtils tokenUtils = mock(TokenUtils.class);
-    private final String URL = "/consult-service/v1/app/{schoolYear}/schools/{schoolId}/consults";
+    private static final String BASE_URL = "/consult-service/v1/app/{schoolYear}/schools/{schoolId}/consults";
 
     @Override
     protected Object initController() {
-        return new ConsultAppController(consultService, tokenUtils);
+        return new ConsultAppController(consultAppService, tokenUtils);
     }
 
     @DisplayName("상담 등록 API")
     @Test
     void createConsult() throws Exception {
+        LocalDateTime consultDateTime = LocalDateTime.now();
         CreateConsultRequest request = CreateConsultRequest.builder()
-            .consultDateTime(LocalDateTime.now())
-            .message("리온이가 너무 귀여워요!")
-            .typeId(VISIT.getCode())
-            .teacherKey(generateUUID())
-            .studentKey(generateUUID())
+            .teacherKey(UUID.randomUUID().toString())
+            .studentKey(UUID.randomUUID().toString())
+            .typeId(ConsultType.VISIT.getCode())
+            .consultDateTime(consultDateTime)
+            .message("우리 아이 진로에 대해서 상담하고 싶습니다.")
             .build();
 
         given(tokenUtils.getUserKey())
-            .willReturn(generateUUID());
+            .willReturn(UUID.randomUUID().toString());
 
         CreateConsultResponse response = CreateConsultResponse.builder()
             .consultId(1L)
-            .typeId(VISIT.getCode())
-            .teacher("1학년 3반 임우택 선생님")
-            .applicant("1학년 3반 이리온(모) 이예리")
-            .consultDateTime(LocalDateTime.now())
-            .message("리온이가 너무 귀여워요!")
+            .type(ConsultType.VISIT.getCode())
+            .teacherInfo("2학년 2반 정인재 선생님")
+            .parentInfo("2학년 2반 3번 이리온 어머님")
+            .consultDateTime(consultDateTime)
+            .message("우리 아이 진로에 대해서 상담하고 싶습니다.")
             .build();
 
-//        given(consultService.createConsult(anyString(), anyLong(), any(CreateConsultDto.class)))
-//            .willReturn(response);
+        given(consultAppService.createConsult(anyString(), anyInt(), anyLong(), any()))
+            .willReturn(response);
 
         mockMvc.perform(
-                post(URL, 2023, 21617)
+                post(BASE_URL, 2023, 100000)
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(MediaType.APPLICATION_JSON)
             )
@@ -72,21 +77,21 @@ public class ConsultAppControllerDocsTest extends RestDocsSupport {
                 preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 requestFields(
-                    fieldWithPath("consultDateTime").type(JsonFieldType.ARRAY)
-                        .optional()
-                        .description("상담 일시"),
-                    fieldWithPath("message").type(JsonFieldType.STRING)
-                        .optional()
-                        .description("상담 내용"),
-                    fieldWithPath("typeId").type(JsonFieldType.NUMBER)
-                        .optional()
-                        .description("상담 유형"),
                     fieldWithPath("teacherKey").type(JsonFieldType.STRING)
                         .optional()
                         .description("교직원 식별키"),
                     fieldWithPath("studentKey").type(JsonFieldType.STRING)
                         .optional()
-                        .description("학생(자녀) 식별키")
+                        .description("학생(자녀) 식별키"),
+                    fieldWithPath("typeId").type(JsonFieldType.NUMBER)
+                        .optional()
+                        .description("상담 유형"),
+                    fieldWithPath("consultDateTime").type(JsonFieldType.ARRAY)
+                        .optional()
+                        .description("상담 일시"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .optional()
+                        .description("상담 내용")
                 ),
                 responseFields(
                     fieldWithPath("code").type(JsonFieldType.NUMBER)
@@ -101,19 +106,15 @@ public class ConsultAppControllerDocsTest extends RestDocsSupport {
                         .description("상담 ID"),
                     fieldWithPath("data.type").type(JsonFieldType.STRING)
                         .description("상담 유형"),
-                    fieldWithPath("data.teacher").type(JsonFieldType.STRING)
+                    fieldWithPath("data.teacherInfo").type(JsonFieldType.STRING)
                         .description("교직원 정보"),
-                    fieldWithPath("data.applicant").type(JsonFieldType.STRING)
-                        .description("신청자 정보"),
+                    fieldWithPath("data.parentInfo").type(JsonFieldType.STRING)
+                        .description("학부모 정보"),
                     fieldWithPath("data.consultDateTime").type(JsonFieldType.ARRAY)
                         .description("상담 일시"),
                     fieldWithPath("data.message").type(JsonFieldType.STRING)
                         .description("상담 메세지")
                 )
             ));
-    }
-
-    private String generateUUID() {
-        return UUID.randomUUID().toString();
     }
 }
