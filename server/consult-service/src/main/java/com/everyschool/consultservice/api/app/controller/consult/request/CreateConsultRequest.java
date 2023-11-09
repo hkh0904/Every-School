6ad4e4d1024a4dc44c4a0bebfff08840c997deb8
임우택 <lyt1228@naver.com>
2023-11-09 15:42:31 +0900

    feat(consult): 상담 신청 요청 객체 생성
    Adds S09P31C108-481

---
+++ server/consult-service/src/main/java/com/everyschool/consultservice/api/app/controller/consult/request/CreateConsultRequest.java
@@ -0,0 +1,50 @@
+package com.everyschool.consultservice.api.app.controller.consult.request;
+
+import com.everyschool.consultservice.api.app.service.consult.dto.CreateConsultDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class CreateConsultRequest {
+
+    @NotBlank(message = "교직원 고유키는 필수입니다.")
+    private String teacherKey;
+
+    @NotBlank(message = "자녀 교유키는 필수입니다.")
+    private String studentKey;
+
+    @NotNull(message = "상담 종류는 필수입니다.")
+    private Integer typeId;
+
+    @NotNull(message = "상담 시간을 필수입니다.")
+    private LocalDateTime consultDateTime;
+
+    @NotEmpty(message = "상담 사유는 필수입니다.")
+    @Size(max = 500, message = "상담 사유는 최대 500자입니다.")
+    private String message;
+
+    @Builder
+    private CreateConsultRequest(Integer typeId, LocalDateTime consultDateTime, String message) {
+        this.typeId = typeId;
+        this.consultDateTime = consultDateTime;
+        this.message = message;
+    }
+
+    public CreateConsultDto toDto() {
+        return CreateConsultDto.builder()
+            .teacherKey(this.teacherKey)
+            .studentKey(this.studentKey)
+            .typeId(this.typeId)
+            .consultDateTime(this.consultDateTime)
+            .message(this.message)
+            .build();
+    }
+}
