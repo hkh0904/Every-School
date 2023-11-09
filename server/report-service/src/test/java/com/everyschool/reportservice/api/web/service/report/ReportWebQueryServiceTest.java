460e5658ab5ed19fb14ba0d262e495ebd4929281
임우택 <lyt1228@naver.com>
2023-11-09 10:13:06 +0900

    test(report): 신고 목록 조회 서비스 테스트 코드 구현
    Adds S09P31C108-388

---
+++ server/report-service/src/test/java/com/everyschool/reportservice/api/web/service/report/ReportWebQueryServiceTest.java
@@ -0,0 +1,75 @@
+package com.everyschool.reportservice.api.web.service.report;
+
+import com.everyschool.reportservice.IntegrationTestSupport;
+import com.everyschool.reportservice.api.web.controller.report.response.ReportResponse;
+import com.everyschool.reportservice.domain.report.ProgressStatus;
+import com.everyschool.reportservice.domain.report.Report;
+import com.everyschool.reportservice.domain.report.ReportContent;
+import com.everyschool.reportservice.domain.report.ReportType;
+import com.everyschool.reportservice.domain.report.repository.ReportRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static com.everyschool.reportservice.domain.report.ProgressStatus.*;
+import static org.assertj.core.api.Assertions.*;
+
+class ReportWebQueryServiceTest extends IntegrationTestSupport {
+
+    @Autowired
+    private ReportWebQueryService reportWebQueryService;
+
+    @Autowired
+    private ReportRepository reportRepository;
+
+    @DisplayName("학교 아이디, 학년도, 처리 상태를 입력 받아 신고 목록을 조회한다.")
+    @Test
+    void searchReports() {
+        //given
+        Report report1 = saveReport(100000L, 2023);
+
+        Report report2 = saveReport(1L, 2023);
+
+        Report report3 = saveReport(100000L, 2022);
+
+        Report report4 = saveReport(100000L, 2023);
+        report4.remove();
+
+        Report report5 = saveReport(100000L, 2023);
+        report5.editStatus(FINISH.getCode());
+
+        //when
+        List<ReportResponse> responses = reportWebQueryService.searchReports(100000L, 2023, REGISTER.getCode());
+
+        //then
+        assertThat(responses).hasSize(1)
+            .extracting("status")
+            .containsExactlyInAnyOrder(REGISTER.getText());
+    }
+
+    private Report saveReport(long schoolId, int schoolYear) {
+        ReportContent content = ReportContent.builder()
+            .reportWho("who")
+            .reportWhen("when")
+            .reportWhere("where")
+            .reportWhat("what")
+            .reportHow("how")
+            .reportWhy("why")
+            .build();
+
+        Report report = Report.builder()
+            .title("title")
+            .description("description")
+            .content(content)
+            .schoolYear(schoolYear)
+            .typeId(ReportType.VIOLENCE.getCode())
+            .schoolId(schoolId)
+            .userId(1L)
+            .build();
+
+        return reportRepository.save(report);
+    }
+}
