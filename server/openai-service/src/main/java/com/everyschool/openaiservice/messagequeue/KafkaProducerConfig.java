cb00ff4862ee08dbec285f98fc71c4ae97c1e543
이예리 <leeyr0412@naver.com>
2023-11-10 16:49:06 +0900

    feat(openai): kafka config 설정
    Adds #S09P31C108-407

---
+++ server/openai-service/src/main/java/com/everyschool/openaiservice/messagequeue/KafkaProducerConfig.java
@@ -0,0 +1,41 @@
+package com.everyschool.openaiservice.messagequeue;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+
+    private final Environment env;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+
+        String kafkaIP = env.getProperty("kafka.ip");
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaIP);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
