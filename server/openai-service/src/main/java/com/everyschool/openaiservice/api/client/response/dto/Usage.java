94a5248727e2d983e93a55ab221cc00d1819802e
이예리 <leeyr0412@naver.com>
2023-11-10 14:40:41 +0900

    refactor(openai): GPT 객체 수정
    Adds #S09P31C108-407

--- server/openai-service/src/main/java/com/everyschool/openaiservice/api/client/response/dto/Usage.java
+++ server/openai-service/src/main/java/com/everyschool/openaiservice/api/client/response/dto/Usage.java
@@ -1,19 +1,18 @@
 package com.everyschool.openaiservice.api.client.response.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
 public class Usage {
     private int totalTokens;
     private int completionTokens;
     private int promptTokens;
 
-    public int getTotalTokens() {
-        return totalTokens;
-    }
-
-    public int getCompletionTokens() {
-        return completionTokens;
-    }
-
-    public int getPromptTokens() {
-        return promptTokens;
+    @Builder
+    private Usage(int totalTokens, int completionTokens, int promptTokens) {
+        this.totalTokens = totalTokens;
+        this.completionTokens = completionTokens;
+        this.promptTokens = promptTokens;
     }
 }
