89b121200e74cb650ca20accf5ef46c126d21d3d
이예리 <leeyr0412@naver.com>
2023-11-10 10:58:29 +0900

    feat(openai): 채팅 목록 불러오기 FeignClient 작성
    Adds #S09P31C108-407

--- server/openai-service/src/main/java/com/everyschool/openaiservice/api/client/ChatServiceClient.java
+++ server/openai-service/src/main/java/com/everyschool/openaiservice/api/client/ChatServiceClient.java
@@ -2,6 +2,7 @@ package com.everyschool.openaiservice.api.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
@@ -13,4 +14,7 @@ public interface ChatServiceClient {
     @GetMapping("/chat-service/client/v1/chat-rooms")
     List<Long> searchChatRoomIdByDate(@RequestParam(name = "date") LocalDate date);
 
+    @GetMapping("/chat-service/client/v1/chat-rooms/{chatRoomId}")
+    List<Long> searchChatByDateAndChatRoomId(@RequestParam(name = "date") LocalDate date,
+                                             @PathVariable(name = "chatRoomId") Long chatRoomId);
 }
