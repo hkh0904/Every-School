b2400559f4839219a2e9afa45dfc7c15ff315f3b
신성주 <hans52410537@gmail.com>
2023-11-11 01:08:17 +0900

    test(call): 통화 조회 Repository Test
    Adds #S09P31C108-450

--- server/call-service/src/test/java/com/everyschool/callservice/domain/usercall/UserCallQueryRepositoryTest.java
+++ server/call-service/src/test/java/com/everyschool/callservice/domain/usercall/UserCallQueryRepositoryTest.java
@@ -1,13 +1,16 @@
-package com.everyschool.callservice.domain.usercall.repository;
+package com.everyschool.callservice.domain.usercall;
 
 import com.everyschool.callservice.IntegrationTestSupport;
-import com.everyschool.callservice.api.client.UserServiceClient;
+import com.everyschool.callservice.api.controller.usercall.response.UserCallDetailsResponse;
+import com.everyschool.callservice.api.controller.usercall.response.UserCallReportResponse;
 import com.everyschool.callservice.api.controller.usercall.response.UserCallResponse;
-import com.everyschool.callservice.domain.usercall.UserCall;
+import com.everyschool.callservice.domain.usercall.repository.UserCallQueryRepository;
+import com.everyschool.callservice.domain.usercall.repository.UserCallRepository;
+import com.everyschool.callservice.domain.usercalldetails.UserCallDetails;
+import com.everyschool.callservice.domain.usercalldetails.repository.UserCallDetailsRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,10 +25,11 @@ class UserCallQueryRepositoryTest extends IntegrationTestSupport {
     private UserCallQueryRepository userCallQueryRepository;
 
     @Autowired
+    private UserCallDetailsRepository userCallDetailsRepository;
+
+    @Autowired
     private UserCallRepository userCallRepository;
 
-    @MockBean
-    private UserServiceClient userServiceClient;
 
     @DisplayName("선생님 ID로 선생님 통화 목록 가져오기")
     @Test
@@ -76,8 +80,48 @@ class UserCallQueryRepositoryTest extends IntegrationTestSupport {
                 );
     }
 
+    @DisplayName("통화 전반적인 내용 조회")
+    @Test
+    void findByCallId() {
+        // given
+        UserCall userCall = saveCal(1L, 2L, "T", "신성주", "홍경환", "Y", LocalDateTime.now().minusHours(5), LocalDateTime.now().minusHours(4), "파일명", "파일명", false);
+
+        userCall.updateCallInfo("negative", Float.valueOf("0.000001"), Float.valueOf("0.000001"), Float.valueOf("99.9999"), true);
+
+        // when
+        UserCallReportResponse response = userCallQueryRepository.findByCallId(userCall.getId());
+
+        // then
+        assertThat(response.getOverallSentiment()).isEqualTo("negative");
+        assertThat(response.getIsBad()).isTrue();
+    }
+
+    @DisplayName("통화 상세 내용 조회")
+    @Test
+    void findAllByCallId() {
+        // given
+        UserCall userCall = saveCal(1L, 2L, "T", "신성주", "홍경환", "Y", LocalDateTime.now().minusHours(5), LocalDateTime.now().minusHours(4), "파일명", "파일명", false);
+
+        userCall.updateCallInfo("negative", Float.valueOf("0.000001"), Float.valueOf("0.000001"), Float.valueOf("99.9999"), true);
+        UserCallDetails detail1 = saveDetails(userCall, "야!!! 너 내가 누군지 알아? 내가 임마 어?", 0, 26, "negative", Float.valueOf("0.000001"), Float.valueOf("0.000001"), Float.valueOf("99.9999"));
+        UserCallDetails detail2 = saveDetails(userCall, "느그 서장하고 어? 밥도 묵고 어? 싸우나도 가고 으어?", 0, 26, "positive", Float.valueOf("0.000001"), Float.valueOf("0.000001"), Float.valueOf("99.9999"));
+        UserCallDetails detail3 = saveDetails(userCall, "마 다했어 임마? 으어? 이짜식이 말이야 이거 풀어!", 0, 26, "neutral", Float.valueOf("0.000001"), Float.valueOf("0.000001"), Float.valueOf("99.9999"));
+
+        // when
+        List<UserCallDetailsResponse> response = userCallQueryRepository.findAllByCallId(userCall.getId());
+
+        // then
+        assertThat(response).hasSize(3);
+        assertThat(response).extracting("sentiment", "neutral")
+                .containsExactlyInAnyOrder(
+                        tuple("negative", Float.valueOf("0.000001")),
+                        tuple("positive", Float.valueOf("0.000001")),
+                        tuple("neutral", Float.valueOf("0.000001"))
+                );
+    }
+
     private UserCall saveCal(Long teacherId, Long otherUserId, String sender, String senderName, String receiverName,
-                             String receiveCall,LocalDateTime startDateTime, LocalDateTime endDateTime, String uploadFileName,
+                             String receiveCall, LocalDateTime startDateTime, LocalDateTime endDateTime, String uploadFileName,
                              String storeFileName, Boolean isBad) {
         UserCall userCall = UserCall.builder()
                 .teacherId(teacherId)
@@ -93,5 +137,21 @@ class UserCallQueryRepositoryTest extends IntegrationTestSupport {
                 .isBad(isBad)
                 .build();
         return userCallRepository.save(userCall);
+    }
+
+    private UserCallDetails saveDetails(UserCall userCall, String content, int start, int length, String sentiment, Float neutral,
+                                        Float positive, Float negative) {
+        UserCallDetails detail = UserCallDetails.builder()
+                .userCall(userCall)
+                .content(content)
+                .start(start)
+                .length(length)
+                .sentiment(sentiment)
+                .neutral(neutral)
+                .positive(positive)
+                .negative(negative)
+                .build();
+
+        return userCallDetailsRepository.save(detail);
     }
 }
