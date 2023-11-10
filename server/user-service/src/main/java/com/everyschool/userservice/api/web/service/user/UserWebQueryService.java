71c992f4b92207d47428056b83475da1bc17a621
Chaos0103 <lyt1228@naver.com>
2023-11-10 23:18:42 +0900

    refactor(user): 주석 추가 및 코드 리팩토링
    Adds S09P31C108-483

--- server/user-service/src/main/java/com/everyschool/userservice/api/web/service/user/UserWebQueryService.java
+++ server/user-service/src/main/java/com/everyschool/userservice/api/web/service/user/UserWebQueryService.java
@@ -13,9 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
-import static com.everyschool.userservice.error.ErrorMessage.UNAUTHORIZED_USER;
-import static com.everyschool.userservice.error.ErrorMessage.NO_SUCH_USER;
+import static com.everyschool.userservice.api.web.controller.user.response.UserInfoResponse.*;
+import static com.everyschool.userservice.error.ErrorMessage.*;
 
+/**
+ * 회원 웹 조회 서비스
+ *
+ * @author 임우택
+ */
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
@@ -24,40 +29,57 @@ public class UserWebQueryService {
     private final UserRepository userRepository;
     private final SchoolServiceClient schoolServiceClient;
 
+    /**
+     * 교직원 회원 정보 조회
+     *
+     * @param userKey 회원 고유키
+     * @return 조회된 교직원 회원 정보
+     */
     public UserInfoResponse searchUserInfo(String userKey) {
+        //회원 엔티티 조회
+        User user = getUserByUserKey(userKey);
+
+        //교직원 엔티티로 변환
+        Teacher teacher = convertToTeacher(user);
+
+        //학급 정보 조회
+        SchoolClassInfo schoolClassInfo = schoolServiceClient.searchBySchoolClassId(teacher.getSchoolClassId());
+
+        //학교 정보 생성
+        School school = School.of(teacher.getSchoolId(), schoolClassInfo.getSchoolName());
+
+        //학급 정보 생성
+        SchoolClass schoolClass = SchoolClass.of(teacher.getSchoolClassId(), schoolClassInfo);
+
+        return of(teacher, school, schoolClass);
+    }
+
+    /**
+     * 회원 고유키로 회원 엔티티 조회
+     *
+     * @param userKey 회원 고유키
+     * @return 조회된 회원 엔티티
+     * @throws NoSuchElementException 등록된 회원이 아닌 경우 발생
+     */
+    private User getUserByUserKey(String userKey) {
         Optional<User> findUser = userRepository.findByUserKey(userKey);
         if (findUser.isEmpty()) {
             throw new NoSuchElementException(NO_SUCH_USER.getMessage());
         }
-        User user = findUser.get();
+        return findUser.get();
+    }
 
+    /**
+     * 회원 엔티티를 교직원 엔티티로 변환
+     *
+     * @param user 회원 엔티티
+     * @return 변횐된 교직원 엔티티
+     * @throws IllegalArgumentException 교직원 회원이 아닌 경우 발생
+     */
+    private Teacher convertToTeacher(User user) {
         if (!(user instanceof Teacher)) {
-            throw new IllegalArgumentException(UNAUTHORIZED_USER.getMessage());
+            throw new IllegalArgumentException(NOT_TEACHER_USER.getMessage());
         }
-
-        Teacher teacher = (Teacher) user;
-
-        SchoolClassInfo schoolClassInfo = schoolServiceClient.searchBySchoolClassId(teacher.getSchoolClassId());
-
-        UserInfoResponse.School school = UserInfoResponse.School.builder()
-            .schoolId(teacher.getSchoolId())
-            .name(schoolClassInfo.getSchoolName())
-            .build();
-
-        UserInfoResponse.SchoolClass schoolClass = UserInfoResponse.SchoolClass.builder()
-            .schoolClassId(teacher.getSchoolClassId())
-            .grade(schoolClassInfo.getGrade())
-            .classNum(schoolClassInfo.getClassNum())
-            .build();
-
-        return UserInfoResponse.builder()
-            .userType(teacher.getUserCodeId())
-            .email(teacher.getEmail())
-            .name(teacher.getName())
-            .birth(teacher.getBirth())
-            .school(school)
-            .schoolClass(schoolClass)
-            .joinDate(teacher.getCreatedDate())
-            .build();
+        return (Teacher) user;
     }
 }
