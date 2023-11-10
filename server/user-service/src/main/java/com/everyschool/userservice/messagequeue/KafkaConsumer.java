7e450132bdb31e7f4aac7b6ce483df562564a27b
Chaos0103 <lyt1228@naver.com>
2023-11-10 23:51:02 +0900

    feat(user): 학생 회원 학급 수정 카프카 등록
    Adds S09P31C108-483

---
+++ server/user-service/src/main/java/com/everyschool/userservice/messagequeue/KafkaConsumer.java
@@ -0,0 +1,48 @@
+package com.everyschool.userservice.messagequeue;
+
+import com.everyschool.userservice.api.service.user.StudentService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+@Transactional
+public class KafkaConsumer {
+
+    private final StudentService studentService;
+
+    /**
+     * 학생 회원 학급 수정
+     *
+     * @param kafkaMessage 카프카 큐잉 메세지
+     */
+    @KafkaListener(topics = "edit-student-class-info")
+    public void updateExp(String kafkaMessage) {
+        log.info("Kafka Message: ->" + kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        Long studentId = (Long) map.get("studentId");
+        Long schoolId = (Long) map.get("schoolId");
+        Long schoolClassId = (Long) map.get("schoolClassId");
+
+        studentService.editClassInfo(studentId, schoolId, schoolClassId);
+    }
+}
