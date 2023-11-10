b972150b1f0fc4d729ad2f570ae8ab5f762fc805
신성주 <hans52410537@gmail.com>
2023-11-10 19:33:21 +0900

    style(call): 파일 삭제
    Adds #S09P31C108-450

--- server/call-service/src/main/java/com/everyschool/callservice/domain/callrecord/repository/UserCallRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.everyschool.callservice.domain.callrecord.repository;
-
-import com.everyschool.callservice.domain.callrecord.UserCallDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserCallRecordRepository extends JpaRepository<UserCallDetails, Long> {
-}
