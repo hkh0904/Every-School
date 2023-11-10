248b884d642ddc52ba06406c97949099bc25fffe
Chaos0103 <lyt1228@naver.com>
2023-11-11 00:02:25 +0900

    feat(user): 학부모 학급 신청 메세지 큐 구현
    Adds S09P31C108-483

--- server/user-service/src/main/java/com/everyschool/userservice/messagequeue/KafkaConsumer.java
+++ server/user-service/src/main/java/com/everyschool/userservice/messagequeue/KafkaConsumer.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
