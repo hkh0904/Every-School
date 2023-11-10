4a6192be76428dd73e4b123f4860a96188309a4e
Chaos0103 <lyt1228@naver.com>
2023-11-10 23:53:34 +0900

    feat(user): 카프카 환경 설정
    Adds S09P31C108-483

--- server/user-service/src/main/java/com/everyschool/userservice/config/KafkaConfig.java
+++ server/user-service/src/main/java/com/everyschool/userservice/config/KafkaConfig.java
@@ -1,15 +1,16 @@
-package com.everyschool.userservice.messagequeue;
+package com.everyschool.userservice.config;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,9 +18,22 @@ import java.util.Map;
 @RequiredArgsConstructor
 @EnableKafka
 @Configuration
-public class KafkaConsumerConfig {
+public class KafkaConfig {
 
     private final Environment env;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+
+        String kafkaIP = env.getProperty("kafka.ip");
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaIP);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
@@ -43,5 +57,10 @@ public class KafkaConsumerConfig {
         kafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
 
         return kafkaListenerContainerFactory;
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
