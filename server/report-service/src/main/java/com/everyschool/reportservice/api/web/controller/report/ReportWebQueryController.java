50b6d09d9b1eec667acab491cd32c1d73049706f
Chaos0103 <lyt1228@naver.com>
2023-11-11 22:26:34 +0900

    refactor(report): 코드 리팩토링 및 주석 추가
    Adds S09P31C108-388

--- server/report-service/src/main/java/com/everyschool/reportservice/api/web/controller/report/ReportWebQueryController.java
+++ server/report-service/src/main/java/com/everyschool/reportservice/api/web/controller/report/ReportWebQueryController.java
@@ -5,13 +5,18 @@ import com.everyschool.reportservice.api.Result;
 import com.everyschool.reportservice.api.web.controller.report.response.ReportDetailResponse;
 import com.everyschool.reportservice.api.web.controller.report.response.ReportResponse;
 import com.everyschool.reportservice.api.web.service.report.ReportWebQueryService;
-import com.everyschool.reportservice.domain.report.ProgressStatus;
+import com.everyschool.reportservice.domain.report.ReportType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * 웹 신고 조회용 API 컨트롤러
+ *
+ * @author 임우택
+ */
 @RequiredArgsConstructor
 @RestController
 @Slf4j
@@ -20,19 +25,36 @@ public class ReportWebQueryController {
 
     private final ReportWebQueryService reportWebQueryService;
 
+    /**
+     * 신고 내역 목록 조회 API
+     *
+     * @param schoolYear 학년도
+     * @param schoolId   학교 아이디
+     * @param status     조회할 신고 처리 상태 코드
+     * @return 조회된 신고 내역 목록
+     */
     @GetMapping
     public ApiResponse<Result<ReportResponse>> searchReports(
         @PathVariable Integer schoolYear,
         @PathVariable Long schoolId,
         @RequestParam Integer status
     ) {
-        ProgressStatus.getText(status);
 
-        List<ReportResponse> response = reportWebQueryService.searchReports(schoolId, schoolYear, status);
+        validateReportType(status);
+
+        List<ReportResponse> response = reportWebQueryService.searchReports(schoolYear, schoolId, status);
 
         return ApiResponse.ok(Result.of(response));
     }
 
+    /**
+     * 신고 내역 상세 조회 API
+     *
+     * @param schoolYear 학년도
+     * @param schoolId   학교 아이디
+     * @param reportId   신고 아이디
+     * @return 조회된 신고 내역
+     */
     @GetMapping("/{reportId}")
     public ApiResponse<ReportDetailResponse> searchReport(
         @PathVariable Integer schoolYear,
@@ -43,5 +65,14 @@ public class ReportWebQueryController {
         ReportDetailResponse response = reportWebQueryService.searchReport(reportId);
 
         return ApiResponse.ok(response);
+    }
+
+    /**
+     * 신고 타입 코드 검증
+     *
+     * @param code 신고 타입 코드
+     */
+    private void validateReportType(int code) {
+        ReportType.getText(code);
     }
 }
