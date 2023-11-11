f87798f8a69c752765ee307dbcad90b0e151ca2b
Chaos0103 <lyt1228@naver.com>
2023-11-11 21:53:32 +0900

    style(report): 주석 작성
    Adds S09P31C108-388

--- server/report-service/src/main/java/com/everyschool/reportservice/api/client/SchoolServiceClient.java
+++ server/report-service/src/main/java/com/everyschool/reportservice/api/client/SchoolServiceClient.java
@@ -1,13 +1,24 @@
 package com.everyschool.reportservice.api.client;
 
-import com.everyschool.reportservice.api.client.response.StudentInfo;
+import com.everyschool.reportservice.api.client.response.SchoolUserInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("school-service")
+/**
+ * School microservice 통신
+ *
+ * @author 임우택
+ */
+@FeignClient(name = "school-service", url = "https://every-school.com/api")
 public interface SchoolServiceClient {
 
+    /**
+     * 학급 유저 정보 조회
+     *
+     * @param userId 회원 아이디
+     * @return 학년, 반, 학번(존재하지 않으면 null)
+     */
     @GetMapping("/school-service/client/v1/student-info/{userId}")
-    StudentInfo searchByUserId(@PathVariable String userId);
+    SchoolUserInfo searchByUserId(@PathVariable Long userId);
 }
