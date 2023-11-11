5cbeb420c8111ed41f7246da96e97d385624af1e
Chaos0103 <lyt1228@naver.com>
2023-11-11 19:11:26 +0900

    test(user): 카프카 의존관계 설정
    Adds S09P31C108-483

--- server/user-service/src/test/java/com/everyschool/userservice/IntegrationTestSupport.java
+++ server/user-service/src/test/java/com/everyschool/userservice/IntegrationTestSupport.java
@@ -1,10 +1,23 @@
 package com.everyschool.userservice;
 
+import com.everyschool.userservice.config.KafkaConfig;
+import com.everyschool.userservice.messagequeue.KafkaConsumer;
+import com.everyschool.userservice.messagequeue.KafkaProducer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 public abstract class IntegrationTestSupport {
+
+    @MockBean
+    protected KafkaConfig kafkaConfig;
+
+    @MockBean
+    protected KafkaProducer kafkaProducer;
+
+    @MockBean
+    protected KafkaConsumer kafkaConsumer;
 }
 
