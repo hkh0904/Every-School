5a9ada5f9f38d7c086b537b036fc41a815aaa24e
Chaos0103 <lyt1228@naver.com>
2023-11-12 19:29:27 +0900

    feat(school): 학부모 학교 소속 신청 서비스 구현
    Adds S09P31C108-473

--- server/school-service/src/main/java/com/everyschool/schoolservice/api/app/service/schoolapply/SchoolApplyAppService.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/api/app/service/schoolapply/SchoolApplyAppService.java
@@ -8,6 +8,7 @@ import com.everyschool.schoolservice.domain.schoolapply.SchoolApply;
 import com.everyschool.schoolservice.domain.schoolapply.repository.SchoolApplyRepository;
 import com.everyschool.schoolservice.domain.schoolclass.SchoolClass;
 import com.everyschool.schoolservice.domain.schoolclass.repository.SchoolClassQueryAppRepository;
+import com.everyschool.schoolservice.domain.schoolclass.repository.SchoolClassRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,6 +31,7 @@ import static com.everyschool.schoolservice.error.ErrorMessage.NO_SUCH_SCHOOL_CL
 public class SchoolApplyAppService {
 
     private final SchoolApplyRepository schoolApplyRepository;
+    private final SchoolClassRepository schoolClassRepository;
     private final SchoolClassQueryAppRepository schoolClassAppQueryRepository;
     private final UserServiceClient userServiceClient;
 
@@ -59,11 +61,36 @@ public class SchoolApplyAppService {
     }
 
     /**
+     * 학부모 학교 소속 신청
+     *
+     * @param parentId      학부모 아이디
+     * @param studentId     학생 아이디
+     * @param schoolClassId 학급 아이디
+     */
+    public void createParentSchoolApply(Long parentId, Long studentId, Long schoolClassId) {
+        //학부모 정보 조회
+        UserInfo parentInfo = userServiceClient.searchUserInfoById(parentId);
+
+        //학생(자식) 정보 조회
+        UserInfo studentInfo = userServiceClient.searchUserInfoById(studentId);
+
+        //학급 정보 조회
+        SchoolClass schoolClass = getSchoolClassById(schoolClassId);
+
+        String info = createInformation(schoolClass.getGrade(), schoolClass.getClassNum(), 1, studentInfo.getUserName());
+
+        SchoolApply schoolApply = createSchoolApplyEntity(schoolClass.getSchoolYear(), studentInfo.getUserId(), parentId, schoolClass, info, parentInfo.getUserType());
+
+        SchoolApply savedSchoolApply = schoolApplyRepository.save(schoolApply);
+    }
+
+    /**
      * 학교 신청 엔티티 생성
      *
      * @param schoolYear  학년도
      * @param userId      회원 아이디
      * @param schoolClass 학급 엔티티
+     * @param info        학생 정보
      * @return 생성된 학교 신청 엔티티
      */
     private SchoolApply createSchoolApplyEntity(int schoolYear, Long userId, SchoolClass schoolClass, String info) {
@@ -73,6 +100,28 @@ public class SchoolApplyAppService {
             .codeId(STUDENT.getCode())
             .studentId(userId)
             .parentId(null)
+            .schoolClass(schoolClass)
+            .build();
+    }
+
+    /**
+     * 학교 신청 엔티티 생성
+     *
+     * @param schoolYear  학년도
+     * @param userId      회원 아이디
+     * @param parentId    학부모 아이디
+     * @param schoolClass 학급 아이디
+     * @param info        학생 정보
+     * @param userType    학부모 유형
+     * @return 생성된 학교 신청 엔티티
+     */
+    private SchoolApply createSchoolApplyEntity(int schoolYear, Long userId, Long parentId, SchoolClass schoolClass, String info, char userType) {
+        return SchoolApply.builder()
+            .schoolYear(schoolYear)
+            .studentInfo(info)
+            .codeId(userType == 'M' ? FATHER.getCode() : MOTHER.getCode())
+            .studentId(userId)
+            .parentId(parentId)
             .schoolClass(schoolClass)
             .build();
     }
@@ -88,6 +137,21 @@ public class SchoolApplyAppService {
      */
     private SchoolClass getSchoolClass(int schoolYear, Long schoolId, CreateSchoolApplyDto dto) {
         Optional<SchoolClass> findSchoolClass = schoolClassAppQueryRepository.findByInfo(schoolYear, schoolId, dto.getGrade(), dto.getClassNum());
+        if (findSchoolClass.isEmpty()) {
+            throw new NoSuchElementException(NO_SUCH_SCHOOL_CLASS.getMessage());
+        }
+        return findSchoolClass.get();
+    }
+
+    /**
+     * 학급 아이디로 학급 엔티티 조회
+     *
+     * @param schoolClassId 학급 아이디
+     * @return 조회된 학급 엔티티
+     * @throws NoSuchElementException 정보가 일치하는 학급이 존재하지 않는 경우 발생
+     */
+    private SchoolClass getSchoolClassById(Long schoolClassId) {
+        Optional<SchoolClass> findSchoolClass = schoolClassRepository.findById(schoolClassId);
         if (findSchoolClass.isEmpty()) {
             throw new NoSuchElementException(NO_SUCH_SCHOOL_CLASS.getMessage());
         }
