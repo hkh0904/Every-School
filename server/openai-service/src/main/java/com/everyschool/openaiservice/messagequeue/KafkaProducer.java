a400f866af1bf85aaf734de5d3e809096c4411df
이예리 <leeyr0412@naver.com>
2023-11-12 21:09:34 +0900

    feat(openai): 리뷰 저장 카프카 요청 작성
    Adds #S09P31C108-407

--- server/openai-service/src/main/java/com/everyschool/openaiservice/messagequeue/KafkaProducer.java
+++ server/openai-service/src/main/java/com/everyschool/openaiservice/messagequeue/KafkaProducer.java
@@ -1,5 +1,6 @@
 package com.everyschool.openaiservice.messagequeue;
 
+import com.everyschool.openaiservice.messagequeue.dto.ChatReviewSaveDto;
 import com.everyschool.openaiservice.messagequeue.dto.ChatUpdateDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,6 +28,22 @@ public class KafkaProducer {
 
         kafkaTemplate.send(topic, jsonInString);
         log.info("[카프카] 채팅 업데이트. id = {}, 사유 = {} ", dto.getChatId(), dto.getReason());
+
+        return dto;
+    }
+
+    public ChatReviewSaveDto saveReviewDate(String topic, ChatReviewSaveDto dto) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        String jsonInString = "";
+        try {
+            jsonInString = mapper.writeValueAsString(dto);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        kafkaTemplate.send(topic, jsonInString);
+        log.info("[카프카] 문제 채팅 날짜 기록. 채팅방Id = {}, 제목 = {}", dto.getChatRoomId(), dto.getTitle());
 
         return dto;
     }
