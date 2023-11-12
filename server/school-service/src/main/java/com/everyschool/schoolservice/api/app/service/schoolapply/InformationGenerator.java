d9f3ea52a446767ac016cac603a1c93d26b9b04d
Chaos0103 <lyt1228@naver.com>
2023-11-12 17:46:33 +0900

    style(school): 주석 추가
    Adds S09P31C108-473

--- server/school-service/src/main/java/com/everyschool/schoolservice/api/app/service/schoolapply/InformationGenerator.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/api/app/service/schoolapply/InformationGenerator.java
@@ -4,6 +4,14 @@ public class InformationGenerator {
 
     private static final String STUDENT_INFO_FORMAT = "%d학년 %d반 %s 학생";
 
+    /**
+     * 학생 정보 생성
+     *
+     * @param grade    학년
+     * @param classNum 반
+     * @param name     학생 이름
+     * @return 생성된 학생 정보
+     */
     public static String createInformation(int grade, int classNum, String name) {
         return String.format(STUDENT_INFO_FORMAT, grade, classNum, name);
     }
