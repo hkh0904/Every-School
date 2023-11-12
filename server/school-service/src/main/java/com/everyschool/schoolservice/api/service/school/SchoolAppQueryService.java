2b61e377b407bf2f37112d3f320b5ebd3b5b856c
Chaos0103 <lyt1228@naver.com>
2023-11-12 13:19:22 +0900

    refactor(school): 코드 리팩토링 및 주석 추가
    Adds S09P31C108-473

--- server/school-service/src/main/java/com/everyschool/schoolservice/api/service/school/SchoolAppQueryService.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/api/service/school/SchoolAppQueryService.java
@@ -2,7 +2,7 @@ package com.everyschool.schoolservice.api.service.school;
 
 import com.everyschool.schoolservice.api.controller.school.response.SchoolDetailResponse;
 import com.everyschool.schoolservice.api.controller.school.response.SchoolResponse;
-import com.everyschool.schoolservice.domain.school.repository.SchoolQueryRepository;
+import com.everyschool.schoolservice.domain.school.repository.SchoolAppQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,12 +11,19 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import static com.everyschool.schoolservice.error.ErrorMessage.NO_SUCH_SCHOOL;
+
+/**
+ * 앱 학교 조회용 서비스
+ *
+ * @author 임우택
+ */
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
-public class SchoolQueryService {
+public class SchoolAppQueryService {
 
-    private final SchoolQueryRepository schoolQueryRepository;
+    private final SchoolAppQueryRepository schoolQueryRepository;
 
     /**
      * 쿼리가 포함된 학교 목록 조회
@@ -25,16 +32,20 @@ public class SchoolQueryService {
      * @return 조회된 학교 목록
      */
     public List<SchoolResponse> searchSchools(String query) {
-        return schoolQueryRepository.findByName(query);
+        return schoolQueryRepository.findByNameLike(query);
     }
 
+    /**
+     * 학교 상세 정보 조회
+     *
+     * @param schoolId 학교 아이디
+     * @return 조회된 학교 상세 정보
+     */
     public SchoolDetailResponse searchSchoolInfo(Long schoolId) {
         Optional<SchoolDetailResponse> response = schoolQueryRepository.findById(schoolId);
-
-        if(response.isEmpty()) {
-            throw new NoSuchElementException("학교 정보가 존재하지 않습니다.");
+        if (response.isEmpty()) {
+            throw new NoSuchElementException(NO_SUCH_SCHOOL.getMessage());
         }
-
         return response.get();
     }
 }
