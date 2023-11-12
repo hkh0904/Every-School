9543bd1271a424f5885d56e90c13f3818cf900f0
Chaos0103 <lyt1228@naver.com>
2023-11-12 12:50:31 +0900

    style(school): 주석 추가
    Adds S09P31C108-473

--- server/school-service/src/main/java/com/everyschool/schoolservice/domain/schoolapply/repository/SchoolApplyRepository.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/domain/schoolapply/repository/SchoolApplyRepository.java
@@ -4,6 +4,11 @@ import com.everyschool.schoolservice.domain.schoolapply.SchoolApply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * 학급 신청 저장소
+ *
+ * @author 임우택
+ */
 @Repository
 public interface SchoolApplyRepository extends JpaRepository<SchoolApply, Long> {
 }
