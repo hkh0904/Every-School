984b04e41904600284c5f7794a47c71db22814e8
Chaos0103 <lyt1228@naver.com>
2023-11-12 12:34:47 +0900

    test(school): 학교 소속 신청 API 테스트 코드 구현
    Adds S09P31C108-473

--- server/school-service/src/test/java/com/everyschool/schoolservice/ControllerTestSupport.java
+++ server/school-service/src/test/java/com/everyschool/schoolservice/ControllerTestSupport.java
@@ -1,14 +1,18 @@
 package com.everyschool.schoolservice;
 
+import com.everyschool.schoolservice.api.app.controller.schoolapply.SchoolApplyAppController;
+import com.everyschool.schoolservice.api.app.service.schoolapply.SchoolApplyAppService;
 import com.everyschool.schoolservice.api.controller.school.SchoolQueryController;
 import com.everyschool.schoolservice.api.service.school.SchoolQueryService;
+import com.everyschool.schoolservice.utils.TokenUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {SchoolQueryController.class})
+@WebMvcTest(controllers = {SchoolQueryController.class,
+    SchoolApplyAppController.class})
 public abstract class ControllerTestSupport {
 
     @Autowired
@@ -18,5 +22,11 @@ public abstract class ControllerTestSupport {
     protected ObjectMapper objectMapper;
 
     @MockBean
+    protected TokenUtils tokenUtils;
+
+    @MockBean
     protected SchoolQueryService schoolQueryService;
+
+    @MockBean
+    protected SchoolApplyAppService schoolApplyAppService;
 }
