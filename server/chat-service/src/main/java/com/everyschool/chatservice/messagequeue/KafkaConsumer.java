9850bc6b75b956a3cf0e054dc6e051debc1e036b
이예리 <leeyr0412@naver.com>
2023-11-12 23:28:20 +0900

    feat(openai): 테스트용 코드 작성
    Adds #S09P31C108-407

--- server/chat-service/src/main/java/com/everyschool/chatservice/messagequeue/KafkaConsumer.java
+++ server/chat-service/src/main/java/com/everyschool/chatservice/messagequeue/KafkaConsumer.java
@@ -1,7 +1,11 @@
 package com.everyschool.chatservice.messagequeue;
 
+import com.everyschool.chatservice.api.service.SequenceGeneratorService;
 import com.everyschool.chatservice.api.service.chat.ChatMongoService;
 import com.everyschool.chatservice.api.service.filterword.FilterWordService;
+import com.everyschool.chatservice.domain.chat.Chat;
+import com.everyschool.chatservice.domain.chat.ChatStatus;
+import com.everyschool.chatservice.domain.chat.repository.ChatRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,6 +27,10 @@ public class KafkaConsumer {
     private final ChatMongoService chatMongoService;
     private final FilterWordService filterWordService;
 
+    // TODO: 2023/11/12 아래 2개 확인 후 지우기
+    private final ChatRepository chatRepository;
+    private final SequenceGeneratorService sequenceGeneratorService;
+
     /**
      * 부적절 채팅 상태 업데이트
      *
@@ -31,7 +39,7 @@ public class KafkaConsumer {
     @KafkaListener(topics = "update-chat-topic")
     public void updateExp(String kafkaMessage) {
         log.info("Kafka Message: ->" + kafkaMessage);
-
+        log.debug("[카프카] 부적절 채팅 업데이트 카프가 요청 들어옴");
         Map<Object, Object> map = new HashMap<>();
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -44,12 +52,41 @@ public class KafkaConsumer {
         Long chatId = (Long) map.get("chatId");
         String reason = (String) map.get("reason");
 
+        log.debug("[카프카] 부적절 사유 = {}", reason);
+
         chatMongoService.chatUpdate(chatId);
+        log.debug("[카프카] 부적절 채팅 상태 변경 함");
 
         filterWordService.saveReason(reason, chatId);
+        log.debug("[카프카] 부적절 채팅 사유 저장함");
+    }
 
-        Integer exp = (Integer) map.get("exp");
+    @KafkaListener(topics = "kafka-test")
+    public void test(String kafkaMessage) {
+        log.info("Kafka Message: ->" + kafkaMessage);
 
-        // TODO: 2023-11-10 해야함
+        log.debug("[카프카] 이건 카프카 테스트임");
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        String content = (String) map.get("content");
+
+        log.debug("[카프카] 이거 카프카 요청 옮. content는 지피티가 보낸 내용임 = {}", content);
+        log.debug("[카프카] 그걸 채팅으로 저장해보기");
+
+        Chat savedChat = chatRepository.save(Chat.builder()
+                .id(sequenceGeneratorService.generateSequence(Chat.SEQUENCE_NAME))
+                .userId(-1L)
+                .content(content)
+                .status(ChatStatus.PLANE.getCode())
+                .chatRoomId(-1L)
+                .build());
+        log.debug("[카프카] 저장된 채팅 아이디 = {}", savedChat.getId());
     }
 }
