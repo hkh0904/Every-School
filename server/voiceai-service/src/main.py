d89af1ab543fec84c3c6ed566f4365e61e23f590
신성주 <hans52410537@gmail.com>
2023-11-12 23:16:52 +0900

    feat(voiceai): 통화 분석 기능
    Adds #S09P31C108-451

--- server/voiceai-service/src/main.py
+++ server/voiceai-service/src/main.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 from typing import Union
 from py_eureka_client.eureka_client import EurekaClient
 from pydantic import BaseModel
-# from S3voice import s3_connection
-from pydantic import BaseModel
 from agoraCall import voiceRecording as vr_router
 from S3voice import router as s3_router
+from S3voice import s3
 import os
 import requests
 import httpx
@@ -45,7 +44,6 @@ def index():
     return {"message": "Welcome to voice ai server"}
 
 async def stt(audio_data):
-
     API_URL = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=Kor"
 
     # 요청 헤더 설정
@@ -56,8 +54,12 @@ async def stt(audio_data):
     }
 
     async with httpx.AsyncClient(timeout=60) as client:
-        response = await client.post(API_URL, headers=headers, data=audio_data)
-        
+        with open('audio.wav', 'rb') as f:
+            # 파일 읽기
+            audio_content = f.read()
+
+        response = await client.post(API_URL, headers=headers, data=audio_content)
+
         # 응답 처리
         if response.status_code == 200:
             # 응답이 200 OK인 경우, 응답 내용을 JSON으로 파싱하여 반환
@@ -97,42 +99,44 @@ async def sentiment(msg):
             raise HTTPException(status_code=response.status_code, detail="Failed to request the external API")
 
 
-
+class RecordRequest(BaseModel):
+    tsFileKey: str
 
 @app.post("/v1/record/analysis")
-async def record_analysis(file: UploadFile):
+async def record_analysis(request: RecordRequest):
+    tsFileKey = request.tsFileKey
+    audio_data = s3.get_object(Bucket=os.getenv("S3_BUCKET"),Key=tsFileKey)
 
-    # 업로드된 파일의 내용을 읽어서 바이너리로 저장
-    audio_data = file.file.read()
-
-    # # POST 요청 보내기
-    # result = await stt(audio_data=audio_data)
-
-    # sentiment_result = await sentiment(msg=result["text"])
+    # audio_content를 음성파일로 저장
+    with open('audio.wav', 'wb') as f:
+        f.write(audio_data['Body'].read())
 
     # 병렬로 요청 보내기
-    stt_task = stt(audio_data)
+    stt_task = stt('audio.wav')
     stt_result = await stt_task
 
-    # 요청 결과 기다리기
-    sentiment_task = sentiment(stt_result["text"])
-    sentiment_result = await sentiment_task
-
     detailResult = []
-    for info in sentiment_result["sentences"]:
-        temp = {}
-        temp["content"] = info["content"]
-        temp["offset"] = info["offset"]
-        temp["length"] = info["length"]
-        temp["sentiment"] = info["sentiment"]
-        temp["confidence"] = [round(info["confidence"]["neutral"] * 100, 3), round(info["confidence"]["positive"] * 100, 3), round(info["confidence"]["negative"] * 100, 3)]
-        detailResult.append(temp)
+    response_data = {}
+    # 요청 결과 기다리기
+    if stt_result.get("text"):
+        sentiment_task = sentiment(stt_result["text"])
+        sentiment_result = await sentiment_task
 
-    response_data = {
-        "overallResult": sentiment_result["document"]["sentiment"],
-        "overallPercent": [round(sentiment_result["document"]["confidence"]["neutral"], 3), round(sentiment_result["document"]["confidence"]["positive"], 3), round(sentiment_result["document"]["confidence"]["negative"],3 )],
-        "detailsResult": detailResult
-    }
+        for info in sentiment_result["sentences"]:
+            temp = {}
+            temp["fileName"] = tsFileKey
+            temp["content"] = info["content"]
+            temp["offset"] = info["offset"]
+            temp["length"] = info["length"]
+            temp["sentiment"] = info["sentiment"]
+            temp["confidence"] = [round(info["confidence"]["neutral"] * 100, 3), round(info["confidence"]["positive"] * 100, 3), round(info["confidence"]["negative"] * 100, 3)]
+            detailResult.append(temp)
+
+        response_data = {
+            "overallResult": sentiment_result["document"]["sentiment"],
+            "overallPercent": [round(sentiment_result["document"]["confidence"]["neutral"], 3), round(sentiment_result["document"]["confidence"]["positive"], 3), round(sentiment_result["document"]["confidence"]["negative"],3 )],
+            "detailsResult": detailResult
+        }
 
     return JSONResponse(content=response_data)
 
