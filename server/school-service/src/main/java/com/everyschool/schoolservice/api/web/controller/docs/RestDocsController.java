b55651483462ac5f163f139802b557b65d115211
Chaos0103 <lyt1228@naver.com>
2023-11-12 12:55:26 +0900

    style(school): 주석 추가
    Adds S09P31C108-473

--- server/school-service/src/main/java/com/everyschool/schoolservice/api/web/controller/docs/RestDocsController.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/api/web/controller/docs/RestDocsController.java
@@ -1,11 +1,21 @@
-package com.everyschool.schoolservice.api.controller.restdocs;
+package com.everyschool.schoolservice.api.web.controller.docs;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * API docs 컨트롤러
+ *
+ * @author 임우택
+ */
 @Controller
 public class RestDocsController {
 
+    /**
+     * API docs
+     *
+     * @return index.html
+     */
     @GetMapping("/school-service/v1/index")
     public String restDocs() {
         return "index";
