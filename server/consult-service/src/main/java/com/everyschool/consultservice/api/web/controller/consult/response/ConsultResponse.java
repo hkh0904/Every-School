481fc989b933e62609da294a0c71a9c7a5564ab8
Chaos0103 <lyt1228@naver.com>
2023-11-13 23:29:00 +0900

    refactor(consult): 상담 조회 응답값 수정
    Adds S09P31C108-526

--- server/consult-service/src/main/java/com/everyschool/consultservice/api/web/controller/consult/response/ConsultResponse.java
+++ server/consult-service/src/main/java/com/everyschool/consultservice/api/web/controller/consult/response/ConsultResponse.java
@@ -14,16 +14,16 @@ public class ConsultResponse {
     private String type;
     private String status;
     private String parentInfo;
-    private LocalDateTime consultDate;
+    private LocalDateTime lastModifiedDate;
     private String rejectedReason;
 
     @Builder
-    public ConsultResponse(Long consultId, int type, int status, String parentInfo, LocalDateTime consultDate, String rejectedReason) {
+    public ConsultResponse(Long consultId, int type, int status, String parentInfo, LocalDateTime lastModifiedDate, String rejectedReason) {
         this.consultId = consultId;
         this.type = ConsultType.getText(type);
         this.status = ProgressStatus.getText(status);
         this.parentInfo = parentInfo;
-        this.consultDate = consultDate;
+        this.lastModifiedDate = lastModifiedDate;
         this.rejectedReason = rejectedReason;
     }
 }
