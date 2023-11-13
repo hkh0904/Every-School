bfd82f6154e8e35388f5ae66ddf25e108cddc3c2
이예리 <leeyr0412@naver.com>
2023-11-13 17:55:33 +0900

    feat(chat): 문제 채팅 조회
    Adds #S09P31C108-407

--- server/chat-service/src/main/java/com/everyschool/chatservice/domain/chat/repository/ChatReviewRepository.java
+++ server/chat-service/src/main/java/com/everyschool/chatservice/domain/chat/repository/ChatReviewRepository.java
@@ -4,7 +4,10 @@ import com.everyschool.chatservice.domain.chat.ChatReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChatReviewRepository extends JpaRepository<ChatReview, Long> {
 
+    Optional<ChatReview> findById(Long id);
 }
