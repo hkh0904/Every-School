4a176f8e1317fe17ea5d5c4a006818e56c6f4c97
오연주 <siunaus224@gmail.com>
2023-11-13 17:56:21 +0900

    feat: 메인페이지 추가 Adds #S09P31C108-554

--- app/everyschool/lib/page/home/community_new.dart
+++ app/everyschool/lib/page/home/community_new.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+import 'package:everyschool/api/community_api.dart';
+import 'package:everyschool/store/user_store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
 
 class CommunityNew extends StatefulWidget {
   const CommunityNew({super.key});
@@ -8,66 +14,102 @@ class CommunityNew extends StatefulWidget {
 }
 
 class _CommunityNewState extends State<CommunityNew> {
-  var notiList = [
-    {'title': '짧은커뮤니티글제목', 'date': '2023.10.16'},
-    {'title': '짧은커뮤니티글제목짧은커뮤니티글제목', 'date': '2023.10.16'},
-    {'title': '긴커뮤니티글제목긴커뮤니티글제목긴커뮤니티글제목', 'date': '2023.10.16'},
-    {'title': '짧은커뮤니티글제목', 'date': '2023.10.16'},
-  ];
+  final storage = FlutterSecureStorage();
+
+  getPost() async {
+    var userType = await storage.read(key: 'usertype');
+    var schoolId;
+
+    if (userType == '1002') {
+      final descendantInfo = await storage.read(key: 'descendant') ?? "";
+      var selectDescendant = jsonDecode(descendantInfo);
+      schoolId = selectDescendant['school']['schoolId'];
+    } else {
+      final myInfo = await context.read<UserStore>().userInfo;
+      schoolId = myInfo['school']['schoolId'];
+    }
+    var response = CommunityApi().getNewPostList(schoolId);
+    return response;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.fromLTRB(20, 30, 20, 10),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text(
-          '커뮤니티 최신글',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
-        ),
-        Container(
-            height: 200,
-            margin: EdgeInsets.fromLTRB(0, 15, 0, 10),
-            decoration: BoxDecoration(
-                color: Colors.grey[50],
-                borderRadius: BorderRadius.circular(8),
-                border: Border.all(color: Color(0xffD9D9D9), width: 1)),
-            child: ListView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: notiList.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          width: index == notiList.length - 1 ? 0 : 1,
-                          color: index == notiList.length - 1
-                              ? Colors.transparent
-                              : Color(0xffd9d9d9),
-                        ),
-                      ),
+    return FutureBuilder(
+        future: getPost(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            return Container(
+              margin: EdgeInsets.fromLTRB(20, 30, 20, 10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '커뮤니티 최신글',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
                     ),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.6,
-                            child: Text(
-                              notiList[index]['title'] as String,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(fontSize: 15),
-                            ),
-                          ),
-                          Text(
-                            notiList[index]['date'] as String,
-                            style: TextStyle(color: Color(0xff999999)),
-                          )
-                        ]),
-                  );
-                })),
-      ]),
-    );
+                    Container(
+                        height: 200,
+                        margin: EdgeInsets.fromLTRB(0, 15, 0, 10),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[50],
+                            borderRadius: BorderRadius.circular(8),
+                            border:
+                                Border.all(color: Color(0xffD9D9D9), width: 1)),
+                        child: ListView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                height: 50,
+                                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      width: index == snapshot.data.length - 1
+                                          ? 0
+                                          : 1,
+                                      color: index == snapshot.data.length - 1
+                                          ? Colors.transparent
+                                          : Color(0xffd9d9d9),
+                                    ),
+                                  ),
+                                ),
+                                child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.6,
+                                        child: Text(
+                                          snapshot.data[index]['title']
+                                              as String,
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(fontSize: 15),
+                                        ),
+                                      ),
+                                    ]),
+                              );
+                            })),
+                  ]),
+            );
+          } else if (snapshot.hasError) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Error: ${snapshot.error}',
+                style: TextStyle(fontSize: 15),
+              ),
+            );
+          } else {
+            return Container(
+              height: 800,
+            );
+          }
+        });
   }
 }
