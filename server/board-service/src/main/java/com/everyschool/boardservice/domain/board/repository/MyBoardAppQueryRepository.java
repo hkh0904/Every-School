a0ed4d1f7cf688f5cf79ff1d5005a1c5dd49badd
임우택 <lyt1228@naver.com>
2023-11-14 17:08:00 +0900

    docs(board): 나의 스크랩 목록 등록 및 조회 API 문서 작성
    Adds S09P31C108-525

---
+++ server/board-service/src/main/java/com/everyschool/boardservice/domain/board/repository/MyBoardAppQueryRepository.java
@@ -0,0 +1,68 @@
+package com.everyschool.boardservice.domain.board.repository;
+
+import com.everyschool.boardservice.api.app.controller.board.response.BoardResponse;
+import com.everyschool.boardservice.domain.board.QComment;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.everyschool.boardservice.domain.board.QBoard.board;
+import static com.everyschool.boardservice.domain.board.QComment.*;
+
+@Repository
+public class MyBoardAppQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public MyBoardAppQueryRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+
+    public List<BoardResponse> findMyBoardsByUserId(Long userId, int category) {
+        return queryFactory
+            .select(
+                Projections.constructor(
+                    BoardResponse.class,
+                    board.id,
+                    board.title,
+                    board.content,
+                    board.commentCount,
+                    board.createdDate
+                )
+            )
+            .from(board)
+            .where(
+                board.isDeleted.isFalse(),
+                board.userId.eq(userId),
+                board.categoryId.eq(category)
+            )
+            .orderBy(board.lastModifiedDate.desc())
+            .fetch();
+    }
+
+    public List<BoardResponse> findCommentBoardByUserId(Long userId) {
+        return queryFactory
+            .select(
+                Projections.constructor(
+                    BoardResponse.class,
+                    comment.board.id,
+                    comment.board.title,
+                    comment.board.content,
+                    comment.board.commentCount,
+                    comment.board.createdDate
+                )
+            )
+            .from(comment)
+            .join(comment.board, board)
+            .where(
+                comment.isDeleted.isFalse(),
+                comment.userId.eq(userId)
+            )
+            .orderBy(comment.lastModifiedDate.desc())
+            .fetch();
+    }
+}
