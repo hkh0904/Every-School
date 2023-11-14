c8345cbd4b36a0ea2fb2bfbc1fa1443124df9bd4
이예리 <leeyr0412@naver.com>
2023-11-14 14:52:58 +0900

    refactor(openai): 직렬화 데이터
    Adds #S09P31C108-407

--- server/chat-service/src/main/java/com/everyschool/chatservice/messagequeue/KafkaConsumer.java
+++ server/chat-service/src/main/java/com/everyschool/chatservice/messagequeue/KafkaConsumer.java
@@ -1,18 +1,15 @@
 package com.everyschool.chatservice.messagequeue;
 
-import com.everyschool.chatservice.api.service.SequenceGeneratorService;
 import com.everyschool.chatservice.api.service.chat.ChatMongoService;
 import com.everyschool.chatservice.api.service.filterword.FilterWordService;
-import com.everyschool.chatservice.domain.chat.Chat;
 import com.everyschool.chatservice.domain.chat.ChatReview;
-import com.everyschool.chatservice.domain.chat.ChatStatus;
-import com.everyschool.chatservice.domain.chat.repository.ChatRepository;
 import com.everyschool.chatservice.domain.chat.repository.ChatReviewRepository;
 import com.everyschool.chatservice.domain.chatroom.ChatRoom;
 import com.everyschool.chatservice.domain.chatroom.repository.ChatRoomRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -20,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,9 +32,6 @@ public class KafkaConsumer {
     private final ChatReviewRepository chatReviewRepository;
     private final ChatRoomRepository chatRoomRepository;
 
-    // TODO: 2023/11/12 아래 2개 확인 후 지우기
-    private final ChatRepository chatRepository;
-    private final SequenceGeneratorService sequenceGeneratorService;
 
     /**
      * 부적절 채팅 상태 업데이트
@@ -49,15 +44,15 @@ public class KafkaConsumer {
 
         Map<Object, Object> map = getKafkaRequestDto(kafkaMessage);
 
-        Long chatId = (Long) map.get("chatId");
+        Integer chatId = (Integer) map.get("chatId");
         String reason = (String) map.get("reason");
 
         log.debug("[카프카] 부적절 사유 = {}", reason);
 
-        chatMongoService.chatUpdate(chatId);
+        chatMongoService.chatUpdate(Long.valueOf(chatId));
         log.debug("[카프카] 부적절 채팅 상태 변경 함");
 
-        filterWordService.saveReason(reason, chatId);
+        filterWordService.saveReason(reason, Long.valueOf(chatId));
         log.debug("[카프카] 부적절 채팅 사유 저장함");
     }
 
@@ -67,11 +62,12 @@ public class KafkaConsumer {
 
         Map<Object, Object> map = getKafkaRequestDto(kafkaMessage);
 
-        Long chatRoomId = (Long) map.get("chatRoomId");
-        LocalDate chatDate = (LocalDate) map.get("chatDate");
+        Integer chatRoomId = (Integer) map.get("chatRoomId");
+        ArrayList<Integer> chatDateList = (ArrayList<Integer>) map.get("chatDate");
         String title = (String) map.get("title");
 
-        ChatRoom findChatRoom = chatRoomRepository.findById(chatRoomId).get();
+        LocalDate chatDate = LocalDate.of(chatDateList.get(0), chatDateList.get(1), chatDateList.get(2));
+        ChatRoom findChatRoom = chatRoomRepository.findById(Long.valueOf(chatRoomId)).get();
 
         ChatReview chatReview = ChatReview.builder()
                 .chatDate(chatDate)
@@ -83,34 +79,13 @@ public class KafkaConsumer {
         log.debug("[카프카] 리뷰 제목 = {}", savedChatReview.getTitle());
     }
 
-    @KafkaListener(topics = "kafka-test")
-    public void test(String kafkaMessage) {
-        log.debug("[카프카] 이건 카프카 테스트임");
-
-        Map<Object, Object> map = getKafkaRequestDto(kafkaMessage);
-
-        String content = (String) map.get("content");
-
-        log.debug("[카프카] 이거 카프카 요청 옮. content는 지피티가 보낸 내용임 = {}", content);
-        log.debug("[카프카] 그걸 채팅으로 저장해보기");
-
-        Chat savedChat = chatRepository.save(Chat.builder()
-                .id(sequenceGeneratorService.generateSequence(Chat.SEQUENCE_NAME))
-                .userId(-1L)
-                .content(content)
-                .status(ChatStatus.PLANE.getCode())
-                .chatRoomId(-1L)
-                .build());
-        log.debug("[카프카] 저장된 채팅 아이디 = {}", savedChat.getId());
-    }
-
     private Map<Object, Object> getKafkaRequestDto(String kafkaMessage) {
         Map<Object, Object> map = new HashMap<>();
         log.info("Kafka Message: ->" + kafkaMessage);
 
         ObjectMapper mapper = new ObjectMapper();
         try {
-            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {
+            map = mapper.registerModule(new JavaTimeModule()).readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {
             });
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
