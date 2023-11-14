8d6c771ce71c54757e01c8453fa7f83426ec111b
임우택 <lyt1228@naver.com>
2023-11-14 14:51:55 +0900

    refactor(school): 학급 승인 응답 객체 수정
    Adds S09P31C108-525

--- server/school-service/src/main/java/com/everyschool/schoolservice/api/web/controller/schoolapply/response/SchoolApplyResponse.java
+++ server/school-service/src/main/java/com/everyschool/schoolservice/api/web/controller/schoolapply/response/SchoolApplyResponse.java
@@ -1,5 +1,6 @@
 package com.everyschool.schoolservice.api.web.controller.schoolapply.response;
 
+import com.everyschool.schoolservice.api.client.response.UserResponse;
 import com.everyschool.schoolservice.domain.schoolapply.ApplyType;
 import com.everyschool.schoolservice.domain.schoolapply.SchoolApply;
 import lombok.Builder;
@@ -12,14 +13,20 @@ public class SchoolApplyResponse {
 
     private Long schoolApplyId;
     private String applyType;
+    private String parentName;
+    private String parentBirth;
     private String studentInfo;
+    private String studentBirth;
     private LocalDateTime lastModifiedDate;
 
     @Builder
-    public SchoolApplyResponse(Long schoolApplyId, String applyType, String studentInfo, LocalDateTime lastModifiedDate) {
+    public SchoolApplyResponse(Long schoolApplyId, String applyType, String parentName, String parentBirth, String studentInfo, String studentBirth, LocalDateTime lastModifiedDate) {
         this.schoolApplyId = schoolApplyId;
         this.applyType = applyType;
+        this.parentName = parentName;
+        this.parentBirth = parentBirth;
         this.studentInfo = studentInfo;
+        this.studentBirth = studentBirth;
         this.lastModifiedDate = lastModifiedDate;
     }
 
@@ -28,6 +35,18 @@ public class SchoolApplyResponse {
             .schoolApplyId(schoolApply.getId())
             .applyType(ApplyType.getText(schoolApply.getCodeId()))
             .studentInfo(schoolApply.getStudentInfo())
+            .lastModifiedDate(schoolApply.getLastModifiedDate())
+            .build();
+    }
+
+    public static SchoolApplyResponse of(SchoolApply schoolApply, UserResponse student, UserResponse parent) {
+        return SchoolApplyResponse.builder()
+            .schoolApplyId(schoolApply.getId())
+            .applyType(ApplyType.getText(schoolApply.getCodeId()))
+            .parentName(parent == null ? "" : parent.getName())
+            .parentBirth(parent == null ? "" : parent.getBirth())
+            .studentInfo(schoolApply.getStudentInfo())
+            .studentBirth(student.getBirth())
             .lastModifiedDate(schoolApply.getLastModifiedDate())
             .build();
     }
