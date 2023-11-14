c0881a4655393cbd67e08d64c1174191109528c2
이예리 <leeyr0412@naver.com>
2023-11-15 02:29:37 +0900

    feat(board): 스크랩 취소 Service 구현
    Adds #S09P31C108-601

--- server/board-service/src/main/java/com/everyschool/boardservice/api/app/service/scrap/ScrapAppService.java
+++ server/board-service/src/main/java/com/everyschool/boardservice/api/app/service/scrap/ScrapAppService.java
@@ -5,6 +5,7 @@ import com.everyschool.boardservice.api.client.response.UserInfo;
 import com.everyschool.boardservice.domain.board.Board;
 import com.everyschool.boardservice.domain.board.Scrap;
 import com.everyschool.boardservice.domain.board.repository.BoardRepository;
+import com.everyschool.boardservice.domain.board.repository.ScrapQueryRepository;
 import com.everyschool.boardservice.domain.board.repository.ScrapRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,7 @@ import static com.everyschool.boardservice.error.ErrorMessage.NO_SUCH_BOARD;
 public class ScrapAppService {
 
     private final ScrapRepository scrapRepository;
+    private final ScrapQueryRepository scrapQueryRepository;
     private final BoardRepository boardRepository;
     private final UserServiceClient userServiceClient;
 
@@ -30,12 +32,28 @@ public class ScrapAppService {
         Board board = getBoard(boardId);
 
         Scrap scrap = Scrap.builder()
-            .userId(userInfo.getUserId())
-            .board(board)
-            .build();
+                .userId(userInfo.getUserId())
+                .board(board)
+                .build();
 
         scrapRepository.save(scrap);
         board.increaseScrapCount();
+
+        return true;
+    }
+
+    public boolean unscraps(String userKey, Long boardId) {
+        UserInfo userInfo = userServiceClient.searchUserInfo(userKey);
+
+        Board board = getBoard(boardId);
+
+        Scrap scrap = scrapQueryRepository.findByBoardAndUserId(board.getId(), userInfo.getUserId());
+        if (scrap == null) {
+            return true;
+        }
+
+        scrapRepository.delete(scrap);
+        board.decreaseScrapCount();
 
         return true;
     }
