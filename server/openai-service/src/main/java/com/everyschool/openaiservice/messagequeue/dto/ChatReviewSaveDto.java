6aa1203ef546bd539eb98a824c7c3598edf5f736
이예리 <leeyr0412@naver.com>
2023-11-14 14:50:40 +0900

    refactor(openai): 소통 객체 데이터 타입 변경
    Adds #S09P31C108-407

--- server/openai-service/src/main/java/com/everyschool/openaiservice/messagequeue/dto/ChatReviewSaveDto.java
+++ server/openai-service/src/main/java/com/everyschool/openaiservice/messagequeue/dto/ChatReviewSaveDto.java
@@ -3,17 +3,17 @@ package com.everyschool.openaiservice.messagequeue.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class ChatReviewSaveDto {
 
     private Long chatRoomId;
-    private LocalDate chatDate;
+    private LocalDateTime chatDate;
     private String title;
 
     @Builder
-    private ChatReviewSaveDto(Long chatRoomId, LocalDate chatDate, String title) {
+    private ChatReviewSaveDto(Long chatRoomId, LocalDateTime chatDate, String title) {
         this.chatRoomId = chatRoomId;
         this.chatDate = chatDate;
         this.title = title;
